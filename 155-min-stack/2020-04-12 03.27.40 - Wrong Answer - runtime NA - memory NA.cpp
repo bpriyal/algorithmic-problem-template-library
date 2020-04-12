228740bffdd7d49f1a7f3caffa3e63edc8b53759
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-04-12 03:27:40 -0400

    Attempt: 2020-04-12 03.27.40 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 155-min-stack/2020-04-12 03.27.40 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,61 @@
+class MinStack {
+public:
+    /** initialize your data structure here. */
+    
+    int stack[10000];
+    int end;
+    int min;
+    
+    MinStack() {
+        end=-1;
+    }
+    
+    void push(int x) {
+        ++end;
+        if (end == -1){
+            stack[end]= x;
+            min=x;
+        } else{
+            if (x >= min){
+                stack[end]= x;
+            } else {
+                stack[end] = 2*x-min;
+                min = x;
+            }
+        }
+    }
+    
+    void pop() {
+        if (end != -1){
+            if (stack[end] >= min){
+            } else {
+                min = 2*min-stack[end];
+            }
+            --end;
+        }
+    }
+    
+    int top() {
+        if (end != -1){
+            if (stack[end] >= min){
+                return stack[end];
+            } else {
+                return min;
+            }
+        }
+        return -1;
+    }
+    
+    int getMin() {
+        return min;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(x);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
