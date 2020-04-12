efba7fc70b7f50c3a993d79d9eda9e421d9e206c
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-04-12 06:28:25 -0400

    Attempt: 2020-04-12 06.28.25 - Accepted - runtime 4ms - memory 6MB.cpp

---
+++ 1127-last-stone-weight/2020-04-12 06.28.25 - Accepted - runtime 4ms - memory 6MB.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int size;
+    vector<int>::iterator it;
+    int lastStoneWeight(vector<int>& stones) {
+        while((size=stones.size()) > 1){
+            sort(stones.begin(),stones.end());
+            //cout<<stones[size-1]<<endl;
+            it=stones.end();
+            if (stones[size-2] == stones[size-1]){
+                stones.erase(it-2,it);
+            } else {
+                int newstone;
+                newstone = stones[size-2] > stones[size-1] ? stones[size-2]-stones[size-1] : stones[size-1]-stones[size-2];
+                stones.erase(it-2,it);
+                stones.push_back(newstone);   
+            }
+        }
+        if (size == 0) {
+            return size;
+        } else{
+            return stones[0];
+        }
+    }
+};
