e12263ea983ab53a0d3ce686b712a9e950c78ff3
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-04-12 08:36:24 -0400

    Attempt: 2020-04-12 08.36.24 - Accepted - runtime 16ms - memory 17.2MB.cpp

---
+++ 543-diameter-of-binary-tree/2020-04-12 08.36.24 - Accepted - runtime 16ms - memory 17.2MB.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+private:
+    int max(int a,int b){
+        return a >= b ? a : b;
+    }
+    int height(TreeNode* root){
+        if (root == NULL) return 0;
+        return max(height(root->left),height(root->right))+1;
+    }
+public:
+    int diameter;
+    int diameterOfBinaryTree(TreeNode* root) {
+        if (!root) return 0;
+        int currdiameter = height(root->right) + height(root->left);
+        if (currdiameter > diameter){
+            diameter = currdiameter;
+        }
+        int temp;
+        if(root->left){
+            temp = diameterOfBinaryTree(root->left);
+        }
+        if(root->right){
+            temp = diameterOfBinaryTree(root->right);
+        }
+        return diameter;
+    }
+};
