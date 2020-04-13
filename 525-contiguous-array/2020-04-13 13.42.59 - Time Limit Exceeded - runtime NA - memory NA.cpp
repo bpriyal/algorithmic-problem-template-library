6204f425efa590c2d558650e44faa89085b1f683
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-04-13 13:42:59 -0400

    Attempt: 2020-04-13 13.42.59 - Time Limit Exceeded - runtime NA - memory NA.cpp

---
+++ 525-contiguous-array/2020-04-13 13.42.59 - Time Limit Exceeded - runtime NA - memory NA.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    unsigned int findMaxLength(vector<int>& nums) {
+        if (nums.size()==0) return 0;
+        int sum;
+        unsigned int maxsize=0;
+        for (unsigned int i=0;i<nums.size()-1;++i){
+            sum = (nums[i]==0) ? -1 : 1;
+            for(unsigned int j=i+1;j<nums.size();++j){
+                sum+= (nums[j]==0)? -1 : 1;
+                if (!sum && maxsize < j-i+1) {
+                    maxsize = j-i+1;
+                }
+            }
+        }
+        return maxsize;
+    }
+};
