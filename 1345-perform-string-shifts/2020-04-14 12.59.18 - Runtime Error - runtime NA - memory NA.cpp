be3bc97caad07716621f258ff02d3d50bc518849
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-04-14 12:59:18 -0400

    Attempt: 2020-04-14 12.59.18 - Runtime Error - runtime NA - memory NA.cpp

---
+++ 1345-perform-string-shifts/2020-04-14 12.59.18 - Runtime Error - runtime NA - memory NA.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string stringShift(string s, vector<vector<int>>& shift) {
+        int i,j;
+        string left,right;
+        int total_shifts=0;
+        for(i=0;i<shift.size();i++){
+            total_shifts = total_shifts+ ((shift[i][0]==1) ? shift[i][1] : -shift[i][1]);
+        }
+        if (total_shifts > 0) {
+            left = s.substr(0,s.size()-total_shifts);
+            right = s.substr(s.size()-total_shifts,total_shifts);
+        } else {
+            left = s.substr(0,-total_shifts);
+            right = s.substr(-total_shifts,s.size()+total_shifts);
+        }
+        return right+left;
+    }
+};
