4ffaa50e264e43e7884656c309010f141bd74b61
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-04-16 02:48:06 -0400

    Attempt: 2020-04-16 02.48.06 - Accepted - runtime 40ms - memory 10.5MB.cpp

---
+++ 238-product-of-array-except-self/2020-04-16 02.48.06 - Accepted - runtime 40ms - memory 10.5MB.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int size = nums.size();
+        vector<int> product;
+        for(int i=0;i<size;i++) {
+            product.push_back(1);
+        }
+        int temp=1;
+        for(int i=0;i<size;i++){
+            product[i] = temp;
+            temp*= nums[i];
+        }
+        temp=1;
+        for(int i=size-1;i>=0;--i){
+            product[i] =temp*product[i];
+            temp*= nums[i];
+        }
+        return product;
+        
+    }
+};
