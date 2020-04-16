f4efbc3d3b514470424538b4e2a8adf35d097e04
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-04-16 09:29:15 -0400

    Attempt: 2020-04-16 09.29.15 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 678-valid-parenthesis-string/2020-04-16 09.29.15 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    /*bool checkValidString(string s) {
+        if (s.size()==0) return true;
+        stack<char> bracket;
+        int star_count=0;
+        
+        for(int i=0;i<s.size();++i){
+            if (s[i]=='(') {
+                bracket.push(s[i]);
+            } else if (s[i]==')') {
+                if (bracket.empty()) {
+                    if (!star_count) {
+                        return false;
+                    } else {
+                        --star_count;
+                    }
+                } else {
+                   bracket.pop(); 
+                }
+            } else if (s[i]=='*'){
+                ++star_count;
+            }
+        }
+        if (bracket.size() == 0) return true;
+        if (bracket.size() <= star_count) {
+            cout<<bracket.size()<<endl;
+            cout<<star_count<<endl;
+            return true;
+        }
+        return false;
+    }*/
+    bool checkValidString(string s) {
+        if (s.size()==0) return true;
+        int l=0,h=0;
+        for (int i=0;i<s.size();++i) {
+            if (s[i]=='(') {
+                ++l;
+                ++h;
+            } else if (s[i]==')') {
+                --h;
+                if (l>0) --l;
+            } else if (s[i]=='*') {
+                if(l>0) --l;
+                ++h;
+            }
+        }
+        if (l==0) return true;
+        return false;
+    }
+}; 
