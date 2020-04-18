fe6df3e7119ef8eefe5391f01569e81eb1add1cc
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-04-18 03:05:20 -0400

    Attempt: 2020-04-18 03.05.20 - Accepted - runtime 8ms - memory 8.3MB.cpp

---
+++ 200-number-of-islands/2020-04-18 03.05.20 - Accepted - runtime 8ms - memory 8.3MB.cpp
@@ -0,0 +1,41 @@
+class Solution {
+    void dfsUtil(vector<vector<char>> &grid,const int i,const int j){
+        grid[i][j]='2';
+        if (i!=0 && grid[i-1][j]=='1') {
+            dfsUtil(grid,i-1,j);
+        }
+        if (i!=row-1 && grid[i+1][j]=='1'){
+            dfsUtil(grid,i+1,j);
+        }
+        if (j!=0 && grid[i][j-1]=='1'){
+            dfsUtil(grid,i,j-1);
+        }
+        if (j!=col-1 && grid[i][j+1]=='1'){
+            dfsUtil(grid,i,j+1);
+        }
+    }
+public:
+    //vector<vector<bool>> visited;
+    int row,col;
+    void init(int rows,int cols){
+        row=rows;
+        col=cols;
+        //visited.resize(row,vector<bool>(col, false));
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int rows = grid.size();
+        if (rows==0) return 0;
+        int cols = grid[0].size();
+        init(rows,cols);
+        int i,j,count=0;
+        for (i=0;i<row;++i){
+			for(j=0;j<col;++j){
+                if (grid[i][j]=='1'){
+                    count++;
+                    dfsUtil(grid,i,j);
+                }
+            }
+        }
+        return count;
+    }
+};
