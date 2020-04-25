3040d5a1168c04c5b22c2bc117c58d22969783ce
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-04-25 03:32:29 -0400

    Attempt: 2020-04-25 03.32.29 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 55-jump-game/2020-04-25 03.32.29 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int size =nums.size();
+        if (size == 0) return false;
+        int jump= 1;
+        while (jump<size-1) {
+            if (nums[jump] == 0) return false;
+            jump = jump + nums[jump];
+        }
+        if (jump == size -1) return true;
+        return false;
+    }
+};
