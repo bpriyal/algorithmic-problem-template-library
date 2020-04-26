f0ab0a491c1a97ea50e9122b100cf3d002ffd4b3
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-04-26 00:01:15 -0400

    Attempt: 2020-04-26 00.01.15 - Time Limit Exceeded - runtime NA - memory NA.cpp

---
+++ 1538-maximum-points-you-can-obtain-from-cards/2020-04-26 00.01.15 - Time Limit Exceeded - runtime NA - memory NA.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    long int max(long int a, long int b) {
+        return (a > b)? a:b;
+    }
+    long int util(const vector<int> cardPoints,long int beg, long int end,long int k) {
+        if (k ==0){
+            return 0;
+        }
+        long int start = beg+1;
+        long int finish = end -1;
+        return max(cardPoints[beg]+util(cardPoints,start,end,k-1),cardPoints[end]+util(cardPoints,beg,finish,k-1));
+    }
+    
+    long int maxScore(vector<int>& cardPoints, long int k) {
+        long int size = cardPoints.size();
+        long int sum =0;
+        if (k == size || k== size-1) {
+            if (cardPoints[0] > cardPoints[size-1]){
+                sum+=cardPoints[0];
+            } else sum+=cardPoints[1];
+            for (int i=1;i<size;++i) {
+                sum = sum +cardPoints[i];
+            }
+        }
+        long int beg =0;
+        long int end = size-1;
+        long int k2=k;
+        long int sum1 = cardPoints[beg]+util(cardPoints,beg+1,end,--k);
+        long int sum2 = cardPoints[end]+util(cardPoints,beg,end-1,--k2);
+        return max(sum1,sum2);
+    }
+};
