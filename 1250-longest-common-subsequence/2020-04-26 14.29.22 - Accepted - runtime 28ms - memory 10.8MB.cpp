adf3eb115d320bb4900c04a279f2c5803075e29e
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-04-26 14:29:22 -0400

    Attempt: 2020-04-26 14.29.22 - Accepted - runtime 28ms - memory 10.8MB.cpp

---
+++ 1250-longest-common-subsequence/2020-04-26 14.29.22 - Accepted - runtime 28ms - memory 10.8MB.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int size1=text1.size();
+        int size2=text2.size();
+        int dp[size1+1][size2+1];
+        int i,j;
+        for(i=0;i<=size2;++i) {
+            dp[0][i] = 0;
+        }
+        for(i=1;i<=size1;++i) {
+            dp[i][0] = 0;
+        }
+        
+        for (i=1;i<=size1;++i) {
+            for (j=1;j<=size2;++j) {
+                if (text1[i-1] == text2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                } else {
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[size1][size2];
+    }
+};
