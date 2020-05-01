18b27b64381f66cd48c7e6075d7304cc1c7dfeef
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-05-01 06:55:01 -0400

    Attempt: 2020-05-01 06.55.01 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 278-first-bad-version/2020-05-01 06.55.01 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,31 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+    int index = INT_MAX;
+public:
+    void badversion_util(int l, int r) {
+        if (l>r) return;
+        int mid = (l+r)/2;
+        cout<<mid<<" "<<l<<" "<<r<<endl;
+        if (isBadVersion(mid)) {
+            if (index > mid) {
+                cout<<"hrll"<<endl;
+                index = mid;
+            }
+            if (l == r){
+             return;
+            }
+           badversion_util(l,mid);
+        } else {
+           badversion_util(mid+1,r);
+        }
+    }
+    int firstBadVersion(int n) {
+        if (n == 0) {
+            return 0;
+        } 
+        badversion_util(0,n-1);
+        return index;
+    }
+};
