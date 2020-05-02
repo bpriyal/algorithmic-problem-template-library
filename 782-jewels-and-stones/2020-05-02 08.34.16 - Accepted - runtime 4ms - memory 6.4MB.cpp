e576c52fd27f7b6e338221771facbd933b951fdb
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-05-02 08:34:16 -0400

    Attempt: 2020-05-02 08.34.16 - Accepted - runtime 4ms - memory 6.4MB.cpp

---
+++ 782-jewels-and-stones/2020-05-02 08.34.16 - Accepted - runtime 4ms - memory 6.4MB.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    set<char> cache;
+    int numJewelsInStones(string J, string S) {
+        int size = S.size();
+        if (size == 0 || J.size() == 0) {
+            return 0;
+        }
+        int count = 0;
+        for (auto c : J) {
+            cache.insert(c);
+        }
+        for (auto c : S) {
+            if (cache.find(c) != cache.end()) {
+                ++count;
+            }
+        }
+        return count;
+    }
+};
