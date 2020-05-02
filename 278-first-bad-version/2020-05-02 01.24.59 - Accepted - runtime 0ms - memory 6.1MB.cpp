534966cb2afffa0bc19ef9c88b5aaf3887227a9a
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-05-02 01:24:59 -0400

    Attempt: 2020-05-02 01.24.59 - Accepted - runtime 0ms - memory 6.1MB.cpp

---
+++ 278-first-bad-version/2020-05-02 01.24.59 - Accepted - runtime 0ms - memory 6.1MB.cpp
@@ -0,0 +1,32 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+   long int index = LONG_MAX;
+public:
+    void badversion_util(long int l, long int r) {
+        if (l == r) {
+          if (index > l) {
+              index = l;
+          }
+          return;
+        }
+        if (l>r) return;
+        int mid = l + (r-l)/2;
+        if (isBadVersion(mid)) {
+            if (index > mid) {
+                index = mid;
+            }
+           badversion_util(l,mid);
+        } else {
+           badversion_util(mid+1,r);
+        }
+    }
+    long int firstBadVersion(long int n) {
+        if (n == 0) {
+            return 0;
+        } 
+        badversion_util(1,n);
+        return index;
+    }
+};
