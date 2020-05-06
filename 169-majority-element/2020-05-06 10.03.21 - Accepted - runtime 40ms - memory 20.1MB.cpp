bef0a02912f0ff67b786a989b7e427d5c0bb3df0
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-05-06 10:03:21 -0400

    Attempt: 2020-05-06 10.03.21 - Accepted - runtime 40ms - memory 20.1MB.cpp

---
+++ 169-majority-element/2020-05-06 10.03.21 - Accepted - runtime 40ms - memory 20.1MB.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+        unordered_map<int,int> cache;
+        for (auto x:nums) {
+            ++cache[x];
+        }
+        for (auto it=cache.begin();it!=cache.end();++it) {
+            if (it->second > nums.size()/2) {
+                return it->first;
+            }
+        }
+        return -1;
+    }
+};
