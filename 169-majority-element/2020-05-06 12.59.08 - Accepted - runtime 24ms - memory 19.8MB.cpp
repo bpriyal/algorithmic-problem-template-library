82a956c4d344156169e2bc492904c1402c1c86cd
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-05-06 12:59:08 -0400

    Attempt: 2020-05-06 12.59.08 - Accepted - runtime 24ms - memory 19.8MB.cpp

---
+++ 169-majority-element/2020-05-06 12.59.08 - Accepted - runtime 24ms - memory 19.8MB.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        int majele;
+        int count = 0;
+        for (int num : nums) {
+            if (count == 0) {
+                majele = num;
+                count = 1;
+            } else {
+                if (num == majele) {
+                    ++count;
+                } else {
+                    --count;
+                }
+            }
+        }
+        return majele;
+    }
+};
