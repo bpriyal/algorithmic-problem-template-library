8ede3fdbfbf2e654f5a8d7fb9aaa852c7d776638
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-05-09 09:29:26 -0400

    Attempt: 2020-05-09 09.29.26 - Accepted - runtime 0ms - memory 6.1MB.cpp

---
+++ 367-valid-perfect-square/2020-05-09 09.29.26 - Accepted - runtime 0ms - memory 6.1MB.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if (num <= 0) return false;
+        long int i;
+        for (i=1;i*i<=num;++i) {
+            if ( num%i == 0 && num/i == i) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
