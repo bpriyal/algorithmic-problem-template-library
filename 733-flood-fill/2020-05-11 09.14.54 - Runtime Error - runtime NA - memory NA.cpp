93f5dd9a59af1baa0eaaf5f8073d86832332a698
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-05-11 09:14:54 -0400

    Attempt: 2020-05-11 09.14.54 - Runtime Error - runtime NA - memory NA.cpp

---
+++ 733-flood-fill/2020-05-11 09.14.54 - Runtime Error - runtime NA - memory NA.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int rows,cols;
+    void floodFill_util(vector<vector<int>>& image,const int& sr,const int& sc,const int& oldColor,const int& newColor){
+        image[sr][sc] = newColor;
+        if (sc-1 >= 0 && image[sr][sc-1] == oldColor) floodFill_util(image,sr,sc-1,oldColor,newColor);
+        if (sc+1 < cols && image[sr][sc+1] == oldColor) floodFill_util(image,sr,sc+1,oldColor,newColor);
+        if (sr-1 >= 0 && image[sr-1][sc] == oldColor) floodFill_util(image,sr-1,sc,oldColor,newColor);
+        if (sr+1 < rows && image[sr+1][sc] == oldColor) floodFill_util(image,sr+1,sc,oldColor,newColor);
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        rows = image.size();
+        cols = image[0].size();
+        int oldColor = image[sr][sc];
+        floodFill_util(image,sr,sc,oldColor,newColor);
+        return image;
+    }
+};
