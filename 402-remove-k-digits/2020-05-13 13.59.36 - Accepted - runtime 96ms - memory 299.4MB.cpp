8382914b2dfe7cf47eba568a9ad7db932f0686c2
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-05-13 13:59:36 -0400

    Attempt: 2020-05-13 13.59.36 - Accepted - runtime 96ms - memory 299.4MB.cpp

---
+++ 402-remove-k-digits/2020-05-13 13.59.36 - Accepted - runtime 96ms - memory 299.4MB.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    /*void del_lead_zero(string& num) {
+        int i = 0;
+        while (num[i] == '0') ++i;
+        num.erase(0,i);
+        return;
+    }
+    string removeKdigits(string num, int k) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        int size = num.size();
+        if (size == k) return "0";
+        
+        int count = k;
+        while(count > 0) {
+            bool flag = false;
+            for (auto it = num.begin();it<num.end()-1;++it) {
+                if (*it > *(it+1)) {
+                    num.erase(it);
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag) num.erase(num.begin()+num.size()-1);
+            --count;
+        }
+        del_lead_zero(num);
+        if (num.size() == 0) return "0";
+        return num;
+    }*/
+    string removeKdigits(string num, int k) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        if (num.size() ==0) return "0";
+        string result;
+        for (auto& x:num) {
+            while (result.size() && k > 0 && result.back() > x) {
+                result.pop_back();
+                --k;
+            }
+            if (result.size() || x != '0') {
+                result = result + x;
+            }
+        }
+        while (k && result.size()) {
+            result.pop_back();
+            --k;
+        }
+        if (!result.size()) return "0";
+        return result;
+    }
+};
