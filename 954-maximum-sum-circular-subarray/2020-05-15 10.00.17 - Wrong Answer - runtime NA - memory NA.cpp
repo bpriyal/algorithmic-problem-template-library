4667b529cd570ffa36528709b4eb122e6d1f7612
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-05-15 10:00:17 -0400

    Attempt: 2020-05-15 10.00.17 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 954-maximum-sum-circular-subarray/2020-05-15 10.00.17 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxSubarraySumCircular(vector<int>& A) {
+        int size = A.size();
+        int i,j,x,y;
+        int sum,max = A[0];
+        for (i = 1;i<=size;++i) {
+            sum = 0;
+            for (j = 0;j<i;++j) {
+                sum += A[j];
+            }
+            if (i == size) {
+                if (sum > max) {
+                    max = sum;
+                }
+                return max;
+            }
+            for (x=0,y=i;x<size-1;++x,++y) {
+                if (y>=size) y -= size;
+                sum += A[y] - A[x];
+                if (sum > max) max = sum;
+            }
+        }
+        return -1;
+    }
+};
