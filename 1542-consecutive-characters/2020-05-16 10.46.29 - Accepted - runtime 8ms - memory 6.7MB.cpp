a9c060263a64553b59b21f1feb878e42df6adc65
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-05-16 10:46:29 -0400

    Attempt: 2020-05-16 10.46.29 - Accepted - runtime 8ms - memory 6.7MB.cpp

---
+++ 1542-consecutive-characters/2020-05-16 10.46.29 - Accepted - runtime 8ms - memory 6.7MB.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxPower(string s) {
+        int curr = 1;
+        int power = 1;
+        for(int i =0;i<s.size()-1;++i) {
+            if (s[i] == s[i+1]) {
+                ++curr;
+            } else {
+                if (curr > power) power = curr;
+                curr = 1;
+            }
+        }
+        return curr > power? curr : power;
+        //return power;
+    }
+};
