d0d1c28d75923260884da68cec689feb19aab821
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-05-17 09:22:23 -0400

    Attempt: 2020-05-17 09.22.23 - Runtime Error - runtime NA - memory NA.cpp

---
+++ 438-find-all-anagrams-in-a-string/2020-05-17 09.22.23 - Runtime Error - runtime NA - memory NA.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    bool isEqual(const vector<int>& a,const vector<int>& b) {
+        cout<<"here"<<endl;
+        for (int i=0;i<26;++i) {
+            if (a[i] != b[i]) return false;
+        }
+        return true;
+    }
+    vector<int> findAnagrams(string s, string p) {
+        int s_size = s.size();
+        int p_size = p.size();
+        /*unordered_map<char,int> cache,temp;
+        vector<int> result;
+        if (s_size == 0) return result;
+        bool flag = false;
+        for(auto& x:p) {
+            ++cache[x];
+        }
+        int i = 0;
+        while (i<s_size-p_size+1) {
+            temp = cache;
+            flag = false;
+            for (int j = i;j<i+p_size;++j) {
+                if (temp.find(s[j]) != temp.end() && temp[s[j]] > 0) {
+                    --temp[s[j]];
+                } else {
+                    flag = true;
+                    break;
+                }
+            }
+            if(!flag) {
+                result.push_back(i);
+                i += p_size;
+            } else {
+                ++i;
+            }
+        }
+        return result;*/
+        vector<int> p_cache(26,0);
+        vector<int> s_cache(26,0);
+        int i;
+        vector<int> result;
+        for(i=0;i<p_size;++i) {
+            ++p_cache[p[i]-'a'];
+            ++s_cache[s[i]-'a'];
+        }
+        for(i=p_size;i<s_size;++i) {
+            cout<<"before if:"<<endl;
+            if(isEqual(s_cache,p_cache)) {
+                result.push_back(i-p_size);
+            }
+            cout<<"here2"<<endl;
+            ++s_cache[s[i]-'a'];
+            --s_cache[s[i-p_size]-'a'];
+        }
+        if(isEqual(s_cache,p_cache)) result.push_back(i-p_size);
+        return result;
+    }
+};
