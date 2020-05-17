173d2f5490c4e2cbacf1ca742394749902b3c5d4
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-05-17 00:06:49 -0400

    Attempt: 2020-05-17 00.06.49 - Output Limit Exceeded - runtime NA - memory NA.cpp

---
+++ 1561-rearrange-words-in-a-sentence/2020-05-17 00.06.49 - Output Limit Exceeded - runtime NA - memory NA.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    string arrangeWords(string text) {
+        map<int,string> cache;
+        int i;
+        string output;
+        int first = 0;
+        int size = text.size();
+        string temp = "";
+        for ( i=0;i<size;++i) {
+            if (text[i] == ' ') {
+                if(cache.find(i-first) != cache.end()) {
+                    cache[i-first]=cache[i-first] + " " + temp;
+                } else {
+                    cache[i-first] = temp;
+                }
+                if (first == 0) {
+                    cache[i-first][0] += 'a'-'A';
+                }
+                cout<<cache[i-first]<<endl;
+                first = i+1;
+                temp = "";
+            } else {
+                temp.push_back(text[i]);
+            }
+        }
+        if(cache.find(i-first) != cache.end()) {
+            cache[size-first] = cache[size-first] + " " + temp;
+        } else {
+            cache[size-first] = temp;
+        }
+        cout<<cache[size-first]<<endl;
+        for (auto it=cache.begin();it!=cache.end();++it) {
+            output = output + it->second+ " ";
+        }
+        output.pop_back();
+        cout<<output[0];
+        output[0] -= 'a'-'A';
+        return output;
+    }
+};
