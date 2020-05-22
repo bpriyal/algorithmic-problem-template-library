4507e92db2b72a94f911fded7efd41fd25915f51
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-05-22 10:24:27 -0400

    Attempt: 2020-05-22 10.24.27 - Accepted - runtime 48ms - memory 8.6MB.cpp

---
+++ 451-sort-characters-by-frequency/2020-05-22 10.24.27 - Accepted - runtime 48ms - memory 8.6MB.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    
+    string frequencySort(string s) {
+        /*unordered_map<char,int> cache;
+        vector<pair<char,int>> vec;
+        for(auto &ele:s) {
+            ++cache[ele];
+        }
+        for(auto it=cache.begin();it!=cache.end();++it) {
+            vec.push_back(make_pair(it->first,it->second));
+        }
+        sort(vec.begin(),vec.end(),
+             [](const pair<char,int> &a,const pair<char,int> &b) {
+                 return a.second > b.second;
+             });
+        string result = "";
+        for(auto &ele:vec) {
+            int no_of_char = ele.second;
+            char c = ele.first;
+            while (no_of_char > 0) {
+                result += c;
+                --no_of_char;
+            }
+        }
+        return result;*/
+        unordered_map<char,int> cache;
+        vector<pair<int,char>> vec;
+        for(auto &ele:s) {
+            ++cache[ele];
+        }
+        for(auto it=cache.begin();it!=cache.end();++it) {
+            vec.push_back(make_pair(it->second,it->first));
+        }
+        sort(vec.rbegin(),vec.rend());
+        string result = "";
+        for(auto &ele:vec) {
+            int no_of_char = ele.first;
+            char c = ele.second;
+            while (no_of_char > 0) {
+                result += c;
+                --no_of_char;
+            }
+        }
+        return result;
+    }
+};
