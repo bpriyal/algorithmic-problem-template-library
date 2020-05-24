6f19c6a1e4e67fa216e38dbb6452cf94ca9c8c6b
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-05-24 01:56:59 -0400

    Attempt: 2020-05-24 01.56.59 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/2020-05-24 01.56.59 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,90 @@
+class Trie {
+public:
+    Trie *children[26];
+    bool isEndofWord;
+    int word_number;
+    /** Initialize your data structure here. */
+    Trie() {
+        for (int i=0;i<26;++i) {
+            children[i] = nullptr;
+        }
+        isEndofWord = false;
+        word_number = 0;
+    }
+    
+    /** Inserts a word into the trie. */
+    void insert(string word,int wn) {
+        int size = word.size();
+        Trie *root = this;
+        for(int i=0;i<size;++i) {
+            if(!root->children[word[i]-'a']) {
+                root->children[word[i]-'a'] = new Trie();
+            }
+            if (root->word_number == 0) root->word_number = wn;
+            root = root->children[word[i]-'a'];
+        }
+        root->isEndofWord = true;
+    }
+    
+    /** Returns if the word is in the trie. */
+    bool search(string word) {
+       Trie *root = this;
+        int size = word.size();
+        for (int i=0;i<size;++i) {
+            if (!root->children[word[i]-'a']) return false;
+            root = root->children[word[i]-'a'];
+        }
+        return root->isEndofWord;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    int startsWith(string prefix) {
+        Trie *root = this;
+        int size = prefix.size();
+        for (int i=0;i<size;++i) {
+            if (!root->children[prefix[i]-'a']) return -1;
+            root = root->children[prefix[i]-'a'];
+        }
+        return root->word_number;
+    }
+};
+
+class Solution {
+public:
+    
+    int isPrefixOfWord(string sentence, string searchWord) {
+        Trie *obj = new Trie();
+        int wn = 0;
+        istringstream ss(sentence); 
+        do {
+            string word; 
+            ss >> word;
+            obj->insert(word,++wn);
+        } while (ss); 
+       return obj->startsWith(searchWord);
+        
+        /* int size = sentence.size();
+        int search_size = searchWord.size();
+        if (search_size > size) return -1;
+        int i =0;
+        int flag = false;
+        int spaces =0;
+        for (int k = 0;k<size;++k) {
+            if (sentence[k] == ' ') {
+                ++spaces;
+            }
+            if (sentence[k] == searchWord[i]) {
+                if (i==0  && ((k!=0 && sentence[k-1] == ' ') || k ==0)) {
+                    ++i;
+                    flag = true;
+                } else if (i!=0) {
+                    ++i;
+                }
+            } else if (flag) {
+                i = 0;
+            }
+            if (i == search_size) return spaces+1; 
+        }
+        return -1;*/
+    }
+};
