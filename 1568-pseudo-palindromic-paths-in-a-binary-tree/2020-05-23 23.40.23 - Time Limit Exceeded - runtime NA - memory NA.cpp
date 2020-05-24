3f3ff4a8687d72a7a35c84d6bcc15b12d958de6b
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-05-23 23:40:23 -0400

    Attempt: 2020-05-23 23.40.23 - Time Limit Exceeded - runtime NA - memory NA.cpp

---
+++ 1568-pseudo-palindromic-paths-in-a-binary-tree/2020-05-23 23.40.23 - Time Limit Exceeded - runtime NA - memory NA.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int count = 0;
+    
+    void leafNode(const unordered_map<int,int> &cache) {
+        int n = 0;
+        for(auto it = cache.begin();it != cache.end(); ++it) {
+            if (it->second % 2 != 0) ++n;
+            if (n > 1) return;
+        }
+        ++count;
+    }
+    
+    void util(TreeNode *root,unordered_map<int,int> cache) {
+        if (root == nullptr) return;
+        ++cache[root->val];
+        if (root->left == nullptr && root->right == nullptr) return leafNode(cache);
+        else {
+            util(root->left,cache);
+            util(root->right,cache);
+        }
+    }
+    
+    int pseudoPalindromicPaths (TreeNode* root) {
+        unordered_map<int,int> cache;
+        util(root,cache);
+        return count;
+    }
+};
