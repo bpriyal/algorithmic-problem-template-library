dd078423ac61d0bdad42b0cec3dd6116fdacfb22
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-05-26 07:24:43 -0400

    Attempt: 2020-05-26 07.24.43 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 525-contiguous-array/2020-05-26 07.24.43 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    
+    int findMaxLength(vector<int>& nums) {
+        int size = nums.size();
+        if (size == 0) return 0;
+        vector<int> sumleft(size);
+        sumleft[0] = nums[0] == 0 ? -1:1;
+        int minele = sumleft[0];
+        int maxele = sumleft[0];
+        for (int i = 1;i<size;++i) {
+            sumleft[i] = sumleft[i-1] + ((nums[i] == 0)? -1:1);
+            if (sumleft[i] > maxele) maxele = sumleft[i];
+            if (sumleft[i]< minele) minele = sumleft[i];
+        }
+        vector<int> cache(maxele-minele+1,-1);
+        int result;
+        for(int i =0;i<size;++i) {
+            if (sumleft[i] == 0) result = i+1;
+            if (cache[sumleft[i]-minele] == -1) {
+                cache[sumleft[i]-minele] = i;
+            } else {
+                if (i - cache[sumleft[i]-minele] > result) {
+                    result = i - cache[sumleft[i]-minele];
+                }
+            }
+        }
+        return result;
+    }
+};
