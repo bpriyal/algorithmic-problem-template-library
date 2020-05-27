e6ea7757eb0facd589671c4ab812025fa2550a4b
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-05-27 10:08:20 -0400

    Attempt: 2020-05-27 10.08.20 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 922-possible-bipartition/2020-05-27 10.08.20 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> adjgraph;
+    vector<int> visited;
+    bool dfs(int index, int color) {
+        if (visited[index] == -1) {
+            visited[index] = color==0? 1:0;
+        } else {
+            if (visited[index] == color) return false;
+            else return true;
+        }
+        for (auto &x:adjgraph[index]) {
+            bool result;
+            cout<<x<<endl;
+            result = dfs(x,visited[index]);
+            if (!result) return false;
+        }
+        return true;
+    }
+    bool possibleBipartition(int N, vector<vector<int>>& dislikes) {
+        if (N <= 2) return true;
+        adjgraph.resize(N);
+        visited.resize(N,-1);
+        for (auto &ele:dislikes) {
+            adjgraph[ele[0]-1].push_back(ele[1]-1);
+            adjgraph[ele[1]-1].push_back(ele[0]-1);
+        }
+        cout<<"here"<<endl;
+        return dfs(0,0);
+    }
+};
