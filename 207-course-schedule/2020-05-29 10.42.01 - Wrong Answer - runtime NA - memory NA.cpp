0a6ed5ac80adc070c7df21da98fc8a4e12eb14a1
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-05-29 10:42:01 -0400

    Attempt: 2020-05-29 10.42.01 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 207-course-schedule/2020-05-29 10.42.01 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> adjmat;
+    vector<bool> visited;
+    bool dfs(int i) {
+        if (visited[i]) {
+            cout<<i<<endl;
+            return false;
+        }
+        visited[i] = true;
+        for (auto &x:adjmat[i]) {
+            if(!dfs(x)) return false;
+        }
+        return true;
+    }
+    
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        adjmat.resize(numCourses);
+        unordered_set<int> s;
+        visited.resize(numCourses,false);
+        for (auto &x:prerequisites) {
+            adjmat[x[0]].push_back(x[1]);
+            s.insert(x[1]);
+        }
+        for (auto &x:s) {
+            if (!visited[x]) {
+                if(!dfs(x)) return false;
+            }
+        }
+        return true;
+    }
+};
