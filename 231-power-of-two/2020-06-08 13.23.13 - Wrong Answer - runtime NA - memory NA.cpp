f139b6414925571c3e0f9efa969f337077f47634
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-06-08 13:23:13 -0400

    Attempt: 2020-06-08 13.23.13 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 231-power-of-two/2020-06-08 13.23.13 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPowerOfTwo(long int n) {
+        if (n == 1) return true;
+        while (n) {
+            if (n%2 != 0 && n != 1) return false;
+            n /= 2;
+        }
+        return true;
+    }
+};
