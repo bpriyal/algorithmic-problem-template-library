e54db68259f23781bfa0b655ee0b9e8b1cbd0ecc
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-06-13 07:27:38 -0400

    Attempt: 2020-06-13 07.27.38 - Time Limit Exceeded - runtime NA - memory NA.cpp

---
+++ 368-largest-divisible-subset/2020-06-13 07.27.38 - Time Limit Exceeded - runtime NA - memory NA.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> util(const vector<int> &nums,int mult,int i,const vector<int> &curr) {
+        if (i>=nums.size()) return curr;
+        if (mult%nums[i] ==0 || nums[i]%mult == 0) {
+            vector<int> vec1(curr);
+            vector<int> vec2(curr);
+            int multt = mult;
+            //if(!std::binary_search(vec1.begin(),vec1.end(),nums[i])) {
+                vec1.push_back(nums[i]);
+                if (multt < nums[i]) multt = nums[i];
+            //}
+            vec1 = util(nums,multt,i+1,vec1);
+            
+            vec2 = util(nums,mult,i+1,vec2);
+            return vec1.size() > vec2.size()? vec1:vec2;
+        }
+        return util(nums,mult,i+1,curr);
+    }
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        vector<int> curr;
+        sort(nums.begin(),nums.end());
+        return util(nums,1,0,curr);
+    }
+};
