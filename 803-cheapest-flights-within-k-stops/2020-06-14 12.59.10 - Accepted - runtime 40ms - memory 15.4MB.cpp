8700479c18da3a5cb5d8edb940ea9a1dce335b2d
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-06-14 12:59:10 -0400

    Attempt: 2020-06-14 12.59.10 - Accepted - runtime 40ms - memory 15.4MB.cpp

---
+++ 803-cheapest-flights-within-k-stops/2020-06-14 12.59.10 - Accepted - runtime 40ms - memory 15.4MB.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    int cache[100][100][100] ={0};
+    int dfs(int src,int dst,int k) {
+        if (src == dst) return 0;
+        if (k < 0) return 10000001;
+        int _min = 10000001;
+        if (cache[src][dst][k]) return cache[src][dst][k];
+        for (auto& x: adj[src]) {
+            _min = min(_min,x.second + dfs(x.first,dst,k-1));
+        }
+        cache[src][dst][k] = _min;
+        return _min;
+    }
+public:
+    vector<vector<pair<int,int>>> adj;
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
+        adj.resize(n);
+        for(auto &x:flights) {
+            adj[x[0]].push_back(make_pair(x[1],x[2]));
+        }
+        int result = dfs(src,dst,K);
+        return result >= 10000001? -1:result;
+    }
+};
