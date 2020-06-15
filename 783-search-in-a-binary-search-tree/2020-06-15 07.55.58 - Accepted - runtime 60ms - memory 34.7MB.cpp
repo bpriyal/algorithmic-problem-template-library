c8f5b6da808f89ddef325c3482cd90a4a1b5fa5b
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-06-15 07:55:58 -0400

    Attempt: 2020-06-15 07.55.58 - Accepted - runtime 60ms - memory 34.7MB.cpp

---
+++ 783-search-in-a-binary-search-tree/2020-06-15 07.55.58 - Accepted - runtime 60ms - memory 34.7MB.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+static auto __io_speed_up = [](){
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+  return nullptr;  
+}();
+
+class Solution {
+    TreeNode* searchUtil(TreeNode* root,int val) {
+        if (root == nullptr) return nullptr;
+        if (root->val == val) return root;
+        if (root->val > val) return searchUtil(root->left,val);
+        return searchUtil(root->right,val);
+    }
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        return searchUtil(root,val);
+    }
+};
