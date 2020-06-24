6980d45546d657b1f157ba283c833992ea12e41b
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-06-24 14:14:09 -0400

    Attempt: 2020-06-24 14.14.09 - Accepted - runtime 0ms - memory 6.2MB.cpp

---
+++ 96-unique-binary-search-trees/2020-06-24 14.14.09 - Accepted - runtime 0ms - memory 6.2MB.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> cache(n+1);
+        cache[0] = 1;
+        cache[1] = 1;
+        for(int i = 2;i<=n;++i) {
+            int  sum = 0;
+            for(int j = 0;j<i;++j) {
+                sum += cache[j]*cache[i-j-1];
+            }
+            cache[i] = sum;
+        }
+        return cache[n];
+    }
+};
