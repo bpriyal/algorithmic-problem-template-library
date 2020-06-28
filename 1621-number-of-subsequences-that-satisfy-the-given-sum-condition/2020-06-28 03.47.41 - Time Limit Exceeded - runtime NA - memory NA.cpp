22083deb329f3e4c5d5706f9cb324e0bdcded032
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-06-28 03:47:41 -0400

    Attempt: 2020-06-28 03.47.41 - Time Limit Exceeded - runtime NA - memory NA.cpp

---
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/2020-06-28 03.47.41 - Time Limit Exceeded - runtime NA - memory NA.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numSubseq(vector<int>& nums, int target) {
+        int size = nums.size();
+        long long int sum = 0;
+        int modulo = 1000000007;
+        vector<int> pre;
+        pre.push_back(1);
+        for(int p = 1;p<size;++p) {
+            pre.push_back((pre[p-1]<<1)%modulo);
+        }
+        sort(nums.begin(),nums.end());
+        int i,j;
+        for (i = 0;i<size;++i) {
+            if (2*nums[i] <= target) {
+                for(j = i;j<size;++j) {
+                    if (nums[i] + nums[j] > target) break;
+                }
+                sum = (sum + pre[j-1-i])%modulo;
+            } else {
+                break;
+            }
+        }
+        return sum;
+    }
+};
