faca6c5d6e3e1141dce95623c9f472ba5bc35063
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-07-04 02:50:15 -0400

    Attempt: 2020-07-04 02.50.15 - Accepted - runtime 8ms - memory 11.9MB.cpp

---
+++ 994-prison-cells-after-n-days/2020-07-04 02.50.15 - Accepted - runtime 8ms - memory 11.9MB.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> prisonAfterNDays(vector<int>& cells, int N) { 
+        vector<int> temp(cells.begin(),cells.end());
+        N = (N%14 == 0) ? 14:N%14;
+        for (int i =0;i<N;++i) {
+            for (int j = 1;j<7;++j) {
+                if (temp[j-1] == temp[j+1]) {
+                    cells[j] = 1;
+                } else {
+                    cells[j] = 0;
+                }
+            }
+            if (i == 0) {
+                cells[0] = cells[7] = 0;
+            }
+            temp = cells;
+        }
+        return cells;
+    }
+};
