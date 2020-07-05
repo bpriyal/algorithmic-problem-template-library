38abb27ab9f4fa9de7fe2595e8c967c6e0c8d220
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-07-05 03:04:04 -0400

    Attempt: 2020-07-05 03.04.04 - Runtime Error - runtime NA - memory NA.cpp

---
+++ 1627-last-moment-before-all-ants-fall-out-of-a-plank/2020-07-05 03.04.04 - Runtime Error - runtime NA - memory NA.cpp
@@ -0,0 +1,53 @@
+class Solution {
+   /* void print(vector<int> &a,vector<int> &b,int n) {
+        for (int i = 0;i<=n;++i) {
+            cout<<a[i]<<" ";
+        }
+        cout<<"\n";
+        for (int i = 0;i<=n;++i) {
+            cout<<b[i]<<" ";
+        }
+    }*/
+public:
+    int getLastMoment(int n, vector<int>& left, vector<int>& right) {
+        int ants = left.size()+right.size();
+        if (ants == 0) return 0;
+        vector<int> a(n+1,0);
+        vector<int> b(n+1,0);
+        if (left.size() == 0) {
+            sort(right.begin(),right.end());
+            return n-right[0];
+        } else if (right.size() == 0) {
+            sort(left.begin(),left.end());
+            return left[n];
+        }
+        for(auto &x:right) {
+            a[x] = 1;
+        }
+        for(auto &x:left) {
+            b[x] = 1; 
+        }
+        int time = 0;
+        while(a[n]+b[0] != ants) {
+            time++;
+            //printf("\nBefore\n");
+            //print(a,b,n);
+            for(int i =n-1;i>=0;--i) {
+                if (a[i] > 0) {
+                   // printf("%d\n",i);
+                    ++a[i+1];
+                    --a[i];
+                }
+            }
+            for(int i = 1;i<=n;++i) {
+                if (b[i] > 0) {
+                    ++b[i-1];
+                    --b[i];
+                }
+            }
+            //printf("\nAfter\n");
+            //print(a,b,n);
+        }
+        return time;
+    }
+};
