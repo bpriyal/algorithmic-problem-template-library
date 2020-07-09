1a7b995dcc114bc3ea0e9b3851d7f4433ae28f58
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-07-09 10:55:27 -0400

    Attempt: 2020-07-09 10.55.27 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 662-maximum-width-of-binary-tree/2020-07-09 10.55.27 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,65 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+        if (root ==nullptr) return 0;
+        TreeNode* root1 = root;
+        TreeNode* root2 = root;
+        int l = 0;
+        int r = 0;
+        int max = INT_MIN;
+        while(true) {
+            //cout<<"root1:"<<root1->val<<endl;
+            //cout<<"root2:"<<root2->val<<endl;
+            TreeNode* temp1 = nullptr;
+            TreeNode* temp2 = nullptr;
+            if (root1->left) {
+                temp1 = root1->left;
+                --l;
+            } else if (root1->right) {
+                temp1 = root1->right;
+                ++l;
+            } else {
+                
+            }
+            
+            if (root2->right) {
+                temp2 = root2->right;
+                ++r;
+            } else if (root2->left) {
+                temp2 = root2->left;
+                --r;
+            } else {
+                
+            }
+            if (!temp1 && !temp2) break;
+            if (!temp1) {
+                root1 = root2;
+                l = r = 0;
+                continue;
+            }
+            if (!temp2) {
+                //cout<<"here"<<endl;
+                root2 = root1;
+                r = l = 0;
+                continue;
+            }
+            int run = r-l;
+            if (run > max) max = run;
+            //cout<<max<<endl;
+            root1 = temp1;
+            root2 = temp2;
+        }
+        return max;
+    }
+};
