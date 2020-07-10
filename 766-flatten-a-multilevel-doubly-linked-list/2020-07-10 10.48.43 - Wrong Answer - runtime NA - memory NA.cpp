6f8511a2c2e1e6af5e5e49dc958063da094bdc98
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-07-10 10:48:43 -0400

    Attempt: 2020-07-10 10.48.43 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 766-flatten-a-multilevel-doubly-linked-list/2020-07-10 10.48.43 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,35 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+*/
+
+class Solution {
+public:
+    Node* flatten(Node* head) {
+        Node* root = head;
+        stack<Node*> s;
+        while(root) {
+            if (!root->next && !root->child && s.empty()) {
+                break;
+            }
+            if (!root->next && !root->child) {
+                Node* temp = s.top();
+                s.pop();
+                root->next = temp;
+            } else if (root->child) {
+                if (root->next) {
+                    s.push(root->next);
+                }
+                root->next = root->child;
+            }
+            root = root->next;
+        }
+        return head;
+    }
+};
