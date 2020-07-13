4fb78ce4efe92c6786909ed2a99e42cf352e3729
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-07-13 07:54:33 -0400

    Attempt: 2020-07-13 07.54.33 - Accepted - runtime 4ms - memory 9.8MB.cpp

---
+++ 100-same-tree/2020-07-13 07.54.33 - Accepted - runtime 4ms - memory 9.8MB.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    void preorderWithNull(TreeNode* root,vector<int> &vec) {
+        if (root) {
+            vec.push_back(root->val);
+            preorderWithNull(root->left,vec);
+            preorderWithNull(root->right,vec);
+        } else {
+            vec.push_back(INT_MAX);
+        }
+    }
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        /*vector<int> tree1;
+        vector<int> tree2;
+        preorderWithNull(p,tree1);
+        preorderWithNull(q,tree2);
+        if (tree1 == tree2) return true;
+        return false;*/
+        if (!p && !q) return true;
+        if (!p || !q) return false;
+        if (p->val != q->val) return false;
+        return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+    }
+};
