3319be7fb2c73d3e577d255e77b00c4a1c1555ab
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-07-18 23:31:34 -0400

    Attempt: 2020-07-18 23.31.34 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 1643-number-of-nodes-in-the-sub-tree-with-the-same-label/2020-07-18 23.31.34 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,43 @@
+class Solution {
+    vector<vector<int>> adj;
+    vector<int> result;
+    vector<vector<int>> dp;
+    int dfs(int i,char c,const string &labels) {
+        if (dp[i][c-'a'] != -1) {
+            //cout<<"here"<<endl;
+            return dp[i][c-'a'];
+        }
+        int count = 0;
+        if (labels[i] == c) {
+            ++count;
+        }
+        for(auto &x:adj[i]) {
+            count += dfs(x,c,labels);
+        }
+        dp[i][c-'a'] = count;
+        return count;
+    }
+public:
+    vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
+        adj.resize(n);
+        if (n == 0) return result;
+        vector<bool> hasParent(n,false);
+        for(auto &x:edges) {
+            if (!hasParent[x[1]]) {
+                adj[x[0]].push_back(x[1]);
+                hasParent[x[1]] = true;
+            } else {
+                adj[x[1]].push_back(x[0]);
+                hasParent[x[0]] = true;
+            }
+        }
+        dp.resize(n);
+        for(int i = 0;i<n;++i) {
+            dp[i].resize(26,-1);
+        }
+        for (int i = 0;i<n;++i) {
+            result.push_back(dfs(i,labels[i],labels));
+        }
+        return result;
+    }
+};
