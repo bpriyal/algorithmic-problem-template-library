167b27e30ebd5d1dff82466808cc0300c14b7089
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-07-29 16:42:01 -0400

    Attempt: 2020-07-29 16.42.01 - Accepted - runtime 40ms - memory 12.7MB.cpp

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/2020-07-29 16.42.01 - Accepted - runtime 40ms - memory 12.7MB.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    int size;
+    vector<vector<int>> dp;
+    int util(int buyPrice,int result,const vector<int>& prices,int i) {
+        printf("%d %d %d\n",buyPrice,result,i);
+        if (i >= size) return 0;
+        bool buy = buyPrice != -1;
+        if (dp[i][buy] != -1) {
+            cout<<"**"<<dp[i][buy]<<endl;
+            return dp[i][buy];
+        }
+        if (buy) { /* sell or not sell*/
+           //cout<<result<<" "<<i<<endl;
+            return dp[i][buy] = max(prices[i]+util(-1,result+(prices[i]-buyPrice),prices,i+2),util(buyPrice,result,prices,i+1));
+        } else { /* buy or not buy */
+            return dp[i][buy] = max(-prices[i]+util(prices[i],result,prices,i+1),util(-1,result,prices,i+1));
+        }
+    }
+public:
+    int maxProfit(vector<int>& prices) {
+        size = prices.size();
+        dp.resize(size);
+        for(int i = 0;i<size;++i) {
+            dp[i].resize(2,-1);
+        }
+        return util(-1,0,prices,0);
+    }
+};
