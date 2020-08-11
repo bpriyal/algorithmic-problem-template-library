3e5c3cab9baf1a9f99514bd03811ed76fdb60f6c
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-08-11 09:52:19 -0400

    Attempt: 2020-08-11 09.52.19 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 274-h-index/2020-08-11 09.52.19 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int size = citations.size();
+        sort(citations.begin(),citations.end());
+        for(int i = size-1;i>=0;--i) {
+            if (citations[i] == (size - i)) {
+                return citations[i];
+            }
+        }
+        return -1;
+    }
+};
