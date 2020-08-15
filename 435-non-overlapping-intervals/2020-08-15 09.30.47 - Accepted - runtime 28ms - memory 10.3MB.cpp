b734f963595020f920a91008b6fa8c84a4868ff4
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-08-15 09:30:47 -0400

    Attempt: 2020-08-15 09.30.47 - Accepted - runtime 28ms - memory 10.3MB.cpp

---
+++ 435-non-overlapping-intervals/2020-08-15 09.30.47 - Accepted - runtime 28ms - memory 10.3MB.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    int size;
+    int comp(vector<vector<int>> &intervals,int pos,int n) {
+        //cout<<pos<<endl;
+        if (pos == size-1) {
+            return n;
+        }
+        if (intervals[pos+1][0] < intervals[pos][1]) {
+            //cout<<pos<<endl;
+            if (intervals[pos+1][1] > intervals[pos][1]) {
+                swap(intervals[pos+1],intervals[pos]);
+                return comp(intervals,pos+1,n+1);
+            } else {
+                return comp(intervals,pos+1,n+1);
+            }
+        }
+        return comp(intervals,pos+1,n);
+    }
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        size = intervals.size();
+        if (size == 0) {
+            return 0;
+        }
+        sort(intervals.begin(),intervals.end(),[](vector<int> &a,vector<int> &b) {
+            return a[0] < b[0];
+        });
+        
+        return comp(intervals,0,0);
+    }
+};
