804937f9ada73c8336f60b9d9535125ddeae146a
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-08-30 02:29:31 -0400

    Attempt: 2020-08-30 02.29.31 - Accepted - runtime 8ms - memory 11.4MB.cpp

---
+++ 1009-pancake-sorting/2020-08-30 02.29.31 - Accepted - runtime 8ms - memory 11.4MB.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> pancakeSort(vector<int>& A) {
+        int size = A.size();
+        vector<int> result;
+        while (size > 0) {
+            for(int i = 0;i < size;++i) {
+                if (A[i] == size) {
+                    if (i == 0) {
+                        result.push_back(size);
+                        reverse(A.begin(),A.begin()+size);
+                    } else {
+                        result.push_back(i+1);
+                        reverse(A.begin(),A.begin()+i+1);
+                        result.push_back(size);
+                        reverse(A.begin(),A.begin()+size);
+                    }
+                    --size;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+};
