5fb4a5d767fd54f926773fa284b696bb66720d48
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-08-30 09:39:00 -0400

    Attempt: 2020-08-30 09.39.00 - Time Limit Exceeded - runtime NA - memory NA.cpp

---
+++ 989-largest-component-size-by-common-factor/2020-08-30 09.39.00 - Time Limit Exceeded - runtime NA - memory NA.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    int gcd(int x,int y) {
+        if (y == 0) {
+            return x;
+        }
+        return gcd(y,x%y);
+    }
+public:
+    int largestComponentSize(vector<int>& A) {
+        int size = A.size();
+        vector<bool> visited(size,false);
+        queue<int> q;
+        int result = INT_MIN;
+        
+        for(int i = 0;i < size;++i) {
+            if (!visited[i]) {
+                q.push(A[i]);
+                visited[i] = true;
+                int compSize = 0;
+                while(!q.empty()) {
+                    //cout<<q.size()<<endl;
+                    int element = q.front();
+                    q.pop();
+                    ++compSize;
+                    for(int j = i+1; j < size;++j) {
+                        if (!visited[j] && A[j] != element && gcd(element,A[j]) > 1) {
+                            q.push(A[j]);
+                            visited[j] = true;
+                        }
+                    }
+                }
+                result = max(compSize,result);
+            }
+        }
+        return result;
+    }
+};
