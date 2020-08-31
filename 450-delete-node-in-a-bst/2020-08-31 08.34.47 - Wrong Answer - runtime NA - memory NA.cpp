2eee777033040a0cba7ee2f3e8aaa95094814dcf
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-08-31 08:34:47 -0400

    Attempt: 2020-08-31 08.34.47 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 450-delete-node-in-a-bst/2020-08-31 08.34.47 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    TreeNode* searchAndDelete(TreeNode* root,int key) {
+        if (!root) {
+            return nullptr;
+        }
+        if (root->val == key) {
+            if (root->left) {
+                TreeNode* temp = root;
+                root = root->left;
+                TreeNode* temp2 = root;
+                while(temp2->right != nullptr) {
+                    temp2 = temp2->right;
+                }
+                temp2->right = temp->right;
+                //free(temp);
+            } else if (root->right) {
+                TreeNode* temp = root;
+                root = root->right;
+                TreeNode* temp2 = root;
+                while(temp2->left != nullptr) {
+                    temp2 = temp2->left;
+                }
+                temp2->left = temp->left;
+                //free(temp);
+            }
+        }
+        if (key < root->val) {
+            root->left = searchAndDelete(root->left,key);
+        } 
+        if (key > root->val) {
+            root->right = searchAndDelete(root->right,key);
+        }
+        return root;
+    }
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        return searchAndDelete(root,key);
+    }
+};
