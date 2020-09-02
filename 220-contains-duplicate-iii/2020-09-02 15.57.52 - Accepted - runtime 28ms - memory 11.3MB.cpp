5a999d1fa4f8d0a4af83005a2d7d2940b0ccc632
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-09-02 15:57:52 -0400

    Attempt: 2020-09-02 15.57.52 - Accepted - runtime 28ms - memory 11.3MB.cpp

---
+++ 220-contains-duplicate-iii/2020-09-02 15.57.52 - Accepted - runtime 28ms - memory 11.3MB.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
+      multiset <int> s;
+      int n = nums.size();
+      for(int i = 0; i< n; i++){
+         multiset<int>::iterator it = s.lower_bound(nums[i]);
+         if(it != s.end() && *it <= nums[i] + t ) {
+             return true;
+         }
+         if(it != s.begin()){
+             it = std::next(it, -1);
+             if(*it + t >= nums[i])return true;
+         }
+         s.insert(nums[i]);
+         if(i >= k){
+            s.erase(nums[i - k]);
+         }
+      }
+      return false;
+  }    
+};
