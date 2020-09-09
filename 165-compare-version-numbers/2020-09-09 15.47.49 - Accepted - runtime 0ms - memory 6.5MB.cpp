88a29b596acf1b6962ed444ffa2fd0ace7a17654
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-09-09 15:47:49 -0400

    Attempt: 2020-09-09 15.47.49 - Accepted - runtime 0ms - memory 6.5MB.cpp

---
+++ 165-compare-version-numbers/2020-09-09 15.47.49 - Accepted - runtime 0ms - memory 6.5MB.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        int size1 = version1.size();
+        int size2 = version2.size();
+        
+        vector<int> v1,v2;
+        
+        int in = 0;
+        for(int i = 0;i<size1;++i) {
+            if (version1[i]=='.' ) {
+                v1.push_back(stoi(version1.substr(in,i-in)));
+                in = i+1;
+            } else if (i == size1-1) {
+                v1.push_back(stoi(version1.substr(in)));
+            }
+        }
+        in = 0;
+        for(int i = 0;i<size2;++i) {
+            if (version2[i]=='.') {
+                v2.push_back(stoi(version2.substr(in,i-in).c_str()));
+                in = i+1;
+            } else if (i == size2-1) {
+                v2.push_back(stoi(version2.substr(in)));
+            }
+        }
+        
+        int extra = v1.size()-v2.size();
+        if (extra > 0) {
+            for(int i = 0;i<extra;++i) {
+                v2.push_back(0);
+            }
+        } else if (extra < 0) {
+            for(int i = 0;i<-extra;++i) {
+                v1.push_back(0);
+            }
+        }
+        
+        for(int i=0;i<v1.size();++i) {
+            if (v1[i] > v2[i]) {
+                return 1;
+            } else if (v1[i] < v2[i]) {
+                return -1;
+            }
+        }
+        return 0;
+    }
+};
