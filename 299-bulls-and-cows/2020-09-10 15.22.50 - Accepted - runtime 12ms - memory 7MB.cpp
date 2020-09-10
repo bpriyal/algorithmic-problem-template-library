153d7f27fe165e74574acc27013e8bb14c50f076
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-09-10 15:22:50 -0400

    Attempt: 2020-09-10 15.22.50 - Accepted - runtime 12ms - memory 7MB.cpp

---
+++ 299-bulls-and-cows/2020-09-10 15.22.50 - Accepted - runtime 12ms - memory 7MB.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        int size = secret.size();
+        unordered_map<char,int> cache;
+        int bull=0,cows=0;
+        for(int i = 0;i<size;++i) {
+            if (secret[i]==guess[i]) {
+                guess[i] = 'x';
+                ++bull;
+            } else {
+                ++cache[secret[i]];
+            }
+        }
+        
+        for(auto x:guess) {
+            if (x != 'x') {
+                if (cache.find(x) != cache.end()) {
+                    if (cache[x] > 0) {
+                        ++cows;
+                        --cache[x];
+                    }
+                }
+            }
+        }
+        
+        return (to_string(bull)+'A'+to_string(cows)+'B');
+     }
+};
