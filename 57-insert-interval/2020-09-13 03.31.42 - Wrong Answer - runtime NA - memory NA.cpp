f3e7084528344e697bd10424e9e4f7ed6ebf6e57
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-09-13 03:31:42 -0400

    Attempt: 2020-09-13 03.31.42 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 57-insert-interval/2020-09-13 03.31.42 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        bool open = false;
+        int size = intervals.size();
+        vector<vector<int>> result;
+        if (size == 0) {
+            result.push_back(newInterval);
+            return result;
+        }
+        for(int i = 0;i<size;++i) {
+            if ((intervals[i][0] >= newInterval[0] && intervals[i][0] <= newInterval[1]) ||
+                (intervals[i][1] >= newInterval[0] && intervals[i][1] <= newInterval[1])) {
+                newInterval[0] = min(intervals[i][0],newInterval[0]);
+                newInterval[1] = max(intervals[i][1],newInterval[1]);
+                open = true;
+            } else {
+                if (open) {
+                    open = false;
+                    result.push_back(newInterval);
+                }
+                result.push_back(intervals[i]);
+            }
+        }
+        return result;
+    }
+};
