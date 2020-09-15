75094738bb13dad8219d2147e4c03d3233fa066e
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-09-15 09:26:46 -0400

    Attempt: 2020-09-15 09.26.46 - Accepted - runtime 4ms - memory 6.4MB.cpp

---
+++ 58-length-of-last-word/2020-09-15 09.26.46 - Accepted - runtime 4ms - memory 6.4MB.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int size = s.size();
+        int count = 0;
+        for(int i = size-1;i >= 0;--i) {
+            if (s[i] == ' ') {
+                if (count == 0) {
+                    continue;
+                }
+                return count;
+            }
+            ++count;
+        }
+        return count;
+    }
+};
