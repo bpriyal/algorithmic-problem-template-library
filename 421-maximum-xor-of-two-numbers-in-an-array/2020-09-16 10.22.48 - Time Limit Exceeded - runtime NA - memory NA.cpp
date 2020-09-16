cb27f59abdad9aa5ffd72a0bc10284e9fa31cea0
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-09-16 10:22:48 -0400

    Attempt: 2020-09-16 10.22.48 - Time Limit Exceeded - runtime NA - memory NA.cpp

---
+++ 421-maximum-xor-of-two-numbers-in-an-array/2020-09-16 10.22.48 - Time Limit Exceeded - runtime NA - memory NA.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findMaximumXOR(vector<int>& nums) {
+        int max_ = INT_MIN;
+        if (nums.size()==1) {
+            return 0;
+        }
+        for(int i=0;i<nums.size();++i) {
+            for(int j=i+1;j<nums.size();++j) {
+                max_ = max(max_,nums[i]^nums[j]);
+            }
+        }
+        return max_;
+    }
+};
