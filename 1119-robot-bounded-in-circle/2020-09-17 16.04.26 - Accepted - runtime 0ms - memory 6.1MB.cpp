e661181aa19c2dbdb1286bbc0cc176e42a3bf0bf
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-09-17 16:04:26 -0400

    Attempt: 2020-09-17 16.04.26 - Accepted - runtime 0ms - memory 6.1MB.cpp

---
+++ 1119-robot-bounded-in-circle/2020-09-17 16.04.26 - Accepted - runtime 0ms - memory 6.1MB.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isRobotBounded(string instructions) {
+        vector<pair<int,int>> cache({{0,1},{1,0},{0,-1},{-1,0}});
+        int angle = 0;
+        pair<int,int> position = {0,0};
+        for(auto c:instructions) {
+            switch(c) {
+                case 'L':
+                    angle = (angle+3)%4;
+                    break;
+                case 'R':
+                    angle = (angle+1)%4;
+                    break;
+                case 'G':
+                    position.first += cache[angle].first;
+                    position.second += cache[angle].second;
+            }
+        }
+        return (angle != 0 || (position.first == 0 && position.second ==0));
+    }
+};
