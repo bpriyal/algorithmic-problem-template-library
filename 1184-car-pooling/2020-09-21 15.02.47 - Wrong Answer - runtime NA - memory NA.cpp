6de9a7bc7a82834bd4a1ba53f43146c03e8a2c96
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-09-21 15:02:47 -0400

    Attempt: 2020-09-21 15.02.47 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 1184-car-pooling/2020-09-21 15.02.47 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        unordered_map<int,int> drop;
+        unordered_map<int,int> pickup;
+        int size = trips.size();
+        for(auto trip:trips) {
+            drop[trip[2]] = trip[0];
+            pickup[trip[1]] = trip[0];
+        }
+        
+        sort(trips.begin(),trips.end(),[](vector<int> &a,vector<int> &b) {
+           return a[1] < b[1]; 
+        });
+        
+        int a = trips[0][1];
+        int b = trips[size-1][1];
+        
+        for(int i = a;i<=b;++i) {
+            capacity += drop[i] - pickup[i];
+            //printf("%d||%d\n",i,capacity);
+            if (capacity < 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
