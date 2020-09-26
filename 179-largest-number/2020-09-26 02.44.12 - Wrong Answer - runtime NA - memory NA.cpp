28dffbb26eb72fcbb0b81a7ad406964a31d74ca6
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-09-26 02:44:12 -0400

    Attempt: 2020-09-26 02.44.12 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 179-largest-number/2020-09-26 02.44.12 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        int size = nums.size();
+        sort(nums.begin(),nums.end(),[](const int &x,const int &y) {
+            if (x == y) {
+                return false;
+            }
+           vector<int> av,bv;
+            //printf("%d||%d\n",x,y);
+            if (x == 0) {
+                av.push_back(x);
+            }
+            if (y == 0) {
+                bv.push_back(y);
+            }
+            int a = x;
+            int b = y;
+            while(a != 0) {
+                av.push_back(a%10);
+                a /= 10;
+            }
+            while(b != 0) {
+                bv.push_back(b%10);
+                b /= 10;
+            }
+            int i=av.size()-1,j = bv.size()-1;
+            //cout<<"**"<<endl;
+            while(i >= 0 && j >= 0) {
+                //printf("vector:%d||%d\n",av[i],bv[j]);
+                if (av[i] < bv[j]) {
+                    //cout<<"false"<<endl;
+                    return false;
+                } else if (av[i] > bv[j]) {
+                    //cout<<"true"<<endl;
+                    return true;
+                } else {
+                    if (i > 0) {
+                        --i;
+                    }
+                    if (j > 0) {
+                        --j;
+                    }
+                }
+            }
+           // cout<<"here"<<endl;
+            return false;
+        });
+        string result = "";
+        bool allZero=true;
+        for(int i = 0;i<size;++i) {
+            if (nums[i] != 0) {
+                allZero = false;
+                break;
+            }
+        }
+        if (allZero == true) {
+            return "0";
+        }
+        for(int n:nums) {
+           // cout<<n<<endl;
+            result += to_string(n);
+        }
+        return result;
+    }
+};
