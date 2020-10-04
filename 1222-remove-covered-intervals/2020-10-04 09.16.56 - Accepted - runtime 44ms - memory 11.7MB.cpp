b31c06f634e8d13cbaed6b14b2a3b2c9f1ff5ab9
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-10-04 09:16:56 -0400

    Attempt: 2020-10-04 09.16.56 - Accepted - runtime 44ms - memory 11.7MB.cpp

---
+++ 1222-remove-covered-intervals/2020-10-04 09.16.56 - Accepted - runtime 44ms - memory 11.7MB.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        int size = intervals.size();
+        int count = size;
+        sort(intervals.begin(),intervals.end(),[](const vector<int> &a,const vector<int> &b) {
+           if (a[0] < b[0]) {
+               return true;
+           } else if (a[0] > b[0]) {
+               return false;
+           } else {
+               return a[1] > b[1];
+           }
+        });
+        
+        for(int i = 0;i<size;++i) {
+            int l = intervals[i][0];
+            int r = intervals[i][1];
+            //cout<<l<<endl;
+            //cout<<r<<endl;
+            for(int j = 0;j < i;++j) {
+                if (intervals[j][0] <= l && intervals[j][1] >= r) {
+                    //cout<<"**"<<endl;
+                    --count;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+};
