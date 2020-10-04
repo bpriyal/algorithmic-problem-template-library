c589788b8f6bbef0ee8d67429dd2d69a24f23971
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-10-04 01:49:06 -0400

    Attempt: 2020-10-04 01.49.06 - Runtime Error - runtime NA - memory NA.cpp

---
+++ 532-k-diff-pairs-in-an-array/2020-10-04 01.49.06 - Runtime Error - runtime NA - memory NA.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        int size = nums.size();
+        if (size == 1) {
+            return 0;
+        }
+        
+        int count = 0;
+        int i = 0;
+        unordered_map<int,int> cache;
+        
+        for(int num:nums) {
+            ++cache[num];
+        }
+        
+        if (k == 0) {
+            for(auto it:cache) {
+                if (it.second > 1) {
+                    ++count;
+                }
+            }
+            return count;
+        }
+        
+        sort(nums.begin(),nums.end());
+        
+        while (i < size) {
+            if (cache.find(nums[i]+k) != cache.end()) {
+                ++count;
+            }
+            ++i;
+            while(nums[i] == nums[i-1]) {
+                ++i;
+            }
+        }
+        
+        return count;
+    }
+};
