53e90eb4ca700f5a9dc64f7e89409aebff3f97a2
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-10-05 09:17:34 -0400

    Attempt: 2020-10-05 09.17.34 - Accepted - runtime 0ms - memory 6.4MB.cpp

---
+++ 1054-complement-of-base-10-integer/2020-10-05 09.17.34 - Accepted - runtime 0ms - memory 6.4MB.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int bitwiseComplement(int N) {
+        if (N == 0) {
+            return 1;
+        }
+        
+        vector<int> binary;
+        
+        while(N != 0) {
+            binary.push_back(N%2);
+            N /= 2;
+        }
+        
+        int mult = 1;
+        int result = 0;
+        
+        for(int x:binary) {
+            cout<<x<<" ";
+        }
+        
+        for(int i = 0;i < binary.size();++i) {
+            result += binary[i] ? 0:mult;
+            mult *= 2;
+        }
+        return result;
+    }
+};
