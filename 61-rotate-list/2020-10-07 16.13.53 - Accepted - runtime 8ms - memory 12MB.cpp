3206f07eadfb1bf6979187bf54b87bc7e5ad7f32
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-10-07 16:13:53 -0400

    Attempt: 2020-10-07 16.13.53 - Accepted - runtime 8ms - memory 12MB.cpp

---
+++ 61-rotate-list/2020-10-07 16.13.53 - Accepted - runtime 8ms - memory 12MB.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head==nullptr) {
+            return head;
+        }
+        ListNode* temp = head;
+        int size =0;
+        while (temp) {
+            ++size;
+            temp = temp->next;
+        }
+        if (k%size == 0) {
+            return head;
+        }
+        ListNode *temp1 = head;
+        while(temp1->next) {
+           temp1 = temp1->next;
+        }
+        temp = head;
+        for(int i=0;i<size-(k%size)-1;++i) {
+            temp = temp->next;
+        }
+        temp1->next = head;
+        head = temp->next;
+        temp->next = nullptr;
+        
+        return head;
+    }
+};
