08902308c2d41b56bd3e333126f7440e23fdc8f3
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-10-16 13:09:24 -0400

    Attempt: 2020-10-16 13.09.24 - Accepted - runtime 4ms - memory 9.9MB.cpp

---
+++ 74-search-a-2d-matrix/2020-10-16 13.09.24 - Accepted - runtime 4ms - memory 9.9MB.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = matrix.size();
+        
+        if (row == 0) {
+            return false;
+        }
+        
+        int col = matrix[0].size();
+        if (col == 0) {
+            return false;
+        }
+        
+        int u = 0;
+        int d = row;
+        
+        while(u < d || (u == d && u < row)) {
+            int mid = u + (d-u)/2;
+            //cout<<mid<<endl;
+            if (matrix[mid][col-1] >= target && matrix[mid][0] <= target) {
+                //cout<<"this rw"<<endl;
+                return std::binary_search(matrix[mid].begin(),matrix[mid].end(),target);
+            } else if (matrix[mid][col-1] < target) {
+                u = mid+1;
+            } else {
+                d = mid-1;
+            }
+        }
+        return false;
+    }
+};
