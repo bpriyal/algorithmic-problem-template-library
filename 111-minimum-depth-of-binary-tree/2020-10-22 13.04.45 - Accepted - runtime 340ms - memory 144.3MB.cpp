f0dd11905726fa55673b9ff9953c73cd734f8aab
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-10-22 13:04:45 -0400

    Attempt: 2020-10-22 13.04.45 - Accepted - runtime 340ms - memory 144.3MB.cpp

---
+++ 111-minimum-depth-of-binary-tree/2020-10-22 13.04.45 - Accepted - runtime 340ms - memory 144.3MB.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int result;
+    void util(TreeNode* root,int depth) {
+        if (root == nullptr || depth >= result) {
+            return;
+        }
+        if (!root->left && !root->right) {
+            result = min(result,depth);
+            return;
+        }
+        util(root->left,depth+1);
+        util(root->right,depth+1);
+    }
+public:
+    //Solution():result(INT_MAX) {}
+    int minDepth(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+        result = INT_MAX;
+        util(root,1);
+        return result;
+    }
+};
