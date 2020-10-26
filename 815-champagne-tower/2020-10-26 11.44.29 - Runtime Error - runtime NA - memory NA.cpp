db82526507ee2753ff1bd212024a80245b6e1cc1
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-10-26 11:44:29 -0400

    Attempt: 2020-10-26 11.44.29 - Runtime Error - runtime NA - memory NA.cpp

---
+++ 815-champagne-tower/2020-10-26 11.44.29 - Runtime Error - runtime NA - memory NA.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+        vector<vector<double>> cache(query_row+1,vector<double>(101,0.0));
+        cache[0][0] = poured;
+        for(int i = 0;i <= query_row;++i) {
+            for(int j = 0;j <= i ;++j) {
+                if (cache[i][j] > 1) {
+                    //cout<<"here"<<endl;
+                    cache[i+1][j] += (cache[i][j]-1)/2;
+                    cache[i+1][j+1] += (cache[i][j]-1)/2;
+                    cache[i][j] = 1;
+                }
+            }
+        }
+        return cache[query_row][query_glass];
+    }
+};
