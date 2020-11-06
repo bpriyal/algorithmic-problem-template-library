259f321d52761f135821b8a8521d1052c7c38392
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-11-06 13:08:21 -0500

    Attempt: 2020-11-06 13.08.21 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 1408-find-the-smallest-divisor-given-a-threshold/2020-11-06 13.08.21 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    bool util(int divisor,int threshold,const vector<int> &nums) {
+        int sum = 0;
+        for(int n:nums) {
+            sum+= (ceil)(static_cast<double>(n)/static_cast<double>(divisor));
+        }
+        return sum <=threshold;
+    }
+public:
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int maxElement = *max_element(nums.begin(),nums.end());
+        
+        int minElement = 1;
+        int mid = 0;
+        while(mid <= maxElement) {
+            mid = minElement + (maxElement-minElement)/2;
+            if (util(mid,threshold,nums)) {
+                maxElement = mid - 1;
+            } else {
+                minElement = mid + 1;
+            }
+        }
+        return mid;
+    }
+};
