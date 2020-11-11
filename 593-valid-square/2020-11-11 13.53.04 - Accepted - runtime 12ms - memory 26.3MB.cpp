fd4d5ea11913891bed31c8f06bf460742b977674
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-11-11 13:53:04 -0500

    Attempt: 2020-11-11 13.53.04 - Accepted - runtime 12ms - memory 26.3MB.cpp

---
+++ 593-valid-square/2020-11-11 13.53.04 - Accepted - runtime 12ms - memory 26.3MB.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    int util(vector<int> &p1,vector<int> &p2) {
+        return (p2[0]-p1[0])*(p2[0]-p1[0]) + (p2[1]-p1[1])*(p2[1]-p1[1]);
+    }
+public:
+    bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) {
+        set<int> cache;
+        cache.insert(util(p1,p2));
+        cache.insert(util(p1,p3));
+        cache.insert(util(p1,p4));
+        cache.insert(util(p2,p3));
+        cache.insert(util(p2,p4));
+        cache.insert(util(p3,p4));
+        
+        return cache.size() == 2 && !cache.count(0);
+    }
+};
