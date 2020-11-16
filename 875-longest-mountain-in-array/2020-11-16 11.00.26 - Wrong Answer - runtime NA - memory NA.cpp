ba596f7d1be060dfbdac1c955b5b7eb3694e2c2a
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-11-16 11:00:26 -0500

    Attempt: 2020-11-16 11.00.26 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 875-longest-mountain-in-array/2020-11-16 11.00.26 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int longestMountain(vector<int>& A) {
+        int start = -1,end = 0,_max = 0;
+        int size = A.size();
+        int i = 0;
+        bool flag = false;
+        while(i < size-1) {
+            if (!flag) {
+                if (A[i] < A[i+1]) {
+                    if (start == -1) {
+                        //printf("%d\n",start);
+                        start = i;
+                    }
+                    ++i;
+                    continue;
+                } else if (A[i] > A[i+1]) {
+                    if (start != -1) {
+                        //cout<<"hereeee"<<endl;
+                        flag = true;
+                    } else {
+                        start = i+1;
+                    }
+                    ++i;
+                } else {
+                    ++i;
+                    start = i;
+                }
+            } else {
+                if (A[i] > A[i+1]) {
+                    ++i;
+                    continue;
+                } else {
+                    _max = max(_max,i-start+1);
+                    //printf("%d||%d||%d\n",_max,i,start);
+                    ++i;
+                    start = i;
+                    flag = false;
+                }
+            }
+        }
+        if(flag == true) {
+            _max = max(_max,i-start+1);
+        }
+        return _max;
+    }
+};
