b865cc2a4b51c975104d0893f2997c761befdf04
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-12-04 12:48:26 -0500

    Attempt: 2020-12-04 12.48.26 - Accepted - runtime 0ms - memory 6.2MB.cpp

---
+++ 1585-the-kth-factor-of-n/2020-12-04 12.48.26 - Accepted - runtime 0ms - memory 6.2MB.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+        int size = 0;
+        for(int i = 1;i <= n;++i) {
+            if (n%i == 0 && (++size == k)) {
+                    return i;
+            }
+        }
+        return -1;
+    }
+};
