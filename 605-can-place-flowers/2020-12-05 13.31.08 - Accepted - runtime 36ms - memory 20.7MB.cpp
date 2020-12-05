7d00a680d251ff486bbb0701613e528c9b268ba8
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-12-05 13:31:08 -0500

    Attempt: 2020-12-05 13.31.08 - Accepted - runtime 36ms - memory 20.7MB.cpp

---
+++ 605-can-place-flowers/2020-12-05 13.31.08 - Accepted - runtime 36ms - memory 20.7MB.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        flowerbed.insert(flowerbed.begin(),0);
+        flowerbed.insert(flowerbed.end(),0);
+        for(int i = 1;i < flowerbed.size()-1;++i) {
+            if (flowerbed[i-1]+flowerbed[i]+flowerbed[i+1] == 0) {
+                --n;
+                ++i;
+            }
+        }
+        return n <= 0;
+    }
+};
