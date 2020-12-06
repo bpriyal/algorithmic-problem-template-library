c96b86fd9dd8c112b9ac092435aab7937802f5ab
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-12-06 09:46:57 -0500

    Attempt: 2020-12-06 09.46.57 - Accepted - runtime 12ms - memory 17.9MB.cpp

---
+++ 117-populating-next-right-pointers-in-each-node-ii/2020-12-06 09.46.57 - Accepted - runtime 12ms - memory 17.9MB.cpp
@@ -0,0 +1,47 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        queue<Node*> q;
+        Node* temp = root;
+        if (temp) {
+            q.push(temp);
+        }
+        while(!q.empty()) {
+            int size = q.size();
+            Node *prev = nullptr;
+            for(int i = 0;i < size;++i) {
+                Node* curr = q.front();
+                q.pop();
+                if (curr->left) {
+                    q.push(curr->left);
+                }
+                if (curr->right) {
+                    q.push(curr->right);
+                }
+                if (i != 0) {
+                    prev->next = curr;
+                }
+                prev = curr;        
+            }
+        }
+        return root;
+    }
+};
