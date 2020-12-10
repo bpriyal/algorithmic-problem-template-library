1e0551a7e8d56a4a94e1706e7ea23dcb20f6a2ba
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-12-10 13:34:11 -0500

    Attempt: 2020-12-10 13.34.11 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 978-valid-mountain-array/2020-12-10 13.34.11 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool validMountainArray(vector<int>& arr) {
+        int size = arr.size();
+        if (size < 3) {
+            return false;
+        }
+        bool startDec = false;
+        for(int i = 1;i < size;++i) {
+            if (!startDec) {
+                if (arr[i] == arr[i-1]) {
+                    return false;
+                }
+                if (arr[i] < arr[i-1]) {
+                    startDec = true;
+                }
+            } else {
+                if (arr[i] == arr[i-1]) {
+                    return false;
+                }
+                if (arr[i] > arr[i-1]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
