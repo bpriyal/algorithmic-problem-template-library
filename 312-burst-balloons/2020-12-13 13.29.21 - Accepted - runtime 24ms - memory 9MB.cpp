a402b48bdcc8f5903fc6053abc85b25ee06870f2
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-12-13 13:29:21 -0500

    Attempt: 2020-12-13 13.29.21 - Accepted - runtime 24ms - memory 9MB.cpp

---
+++ 312-burst-balloons/2020-12-13 13.29.21 - Accepted - runtime 24ms - memory 9MB.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) {
+       vector<int> a(nums.size() + 2, 1);
+        for (int i = 0; i < nums.size(); i ++)
+            a[i + 1] = nums[i];
+        int n = a.size();
+        
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for (int len = 2; len < n; len ++){
+            for (int l = 0; l < n - len; l ++){
+                int r = l + len, tmp = INT_MIN;
+                for (int k = l + 1; k < r; k ++)
+                    tmp = max(tmp, a[l] * a[k] * a[r] + dp[l][k] + dp[k][r]);
+                dp[l][r] = tmp;
+            }
+        }
+        return dp[0][n - 1];
+    }
+};
