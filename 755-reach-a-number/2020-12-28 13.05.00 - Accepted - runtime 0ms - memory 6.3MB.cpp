1dad31ec0e550674eecd18e2c86def74f377cb61
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2020-12-28 13:05:00 -0500

    Attempt: 2020-12-28 13.05.00 - Accepted - runtime 0ms - memory 6.3MB.cpp

---
+++ 755-reach-a-number/2020-12-28 13.05.00 - Accepted - runtime 0ms - memory 6.3MB.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int reachNumber(int target) {
+        int pos = 0;
+        int n = 0;
+        target = abs(target);
+        
+        while (pos < target) {
+            ++n;
+            pos += n;
+        }
+        
+        while((pos-target)%2 != 0) {
+            ++n;
+            pos += n;
+        }
+        
+        return n;
+    }
+};
