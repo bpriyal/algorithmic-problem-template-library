350103087c80b727c32091c799320aa377cb4e1e
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2021-01-07 13:10:52 -0500

    Attempt: 2021-01-07 13.10.52 - Accepted - runtime 4ms - memory 8.8MB.cpp

---
+++ 3-longest-substring-without-repeating-characters/2021-01-07 13.10.52 - Accepted - runtime 4ms - memory 8.8MB.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int size = s.size();
+        int result = 0;
+        int start = -1;
+        vector<int> cache(256,-1);
+        for(int i = 0;i < size;++i) {
+            if (cache[s[i]] > start) {
+                start = cache[s[i]];
+            }
+            cache[s[i]] = i;
+            result = max(result,i - start);
+        }
+        return result;
+    }
+};
