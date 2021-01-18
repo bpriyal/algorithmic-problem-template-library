eb30db8d1cfc5063e17939bc59151d1134f4c768
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2021-01-18 13:08:46 -0500

    Attempt: 2021-01-18 13.08.46 - Accepted - runtime 332ms - memory 72.8MB.cpp

---
+++ 1798-max-number-of-k-sum-pairs/2021-01-18 13.08.46 - Accepted - runtime 332ms - memory 72.8MB.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        unordered_map<int,int> cache;
+        for(int num:nums) {
+            ++cache[num];
+        }
+        int result = 0;
+        for(int num:nums) {
+            if (2*num == k) {
+                result += floor(cache[num]/2);
+                cache[num] = 0;
+            } else {
+                //cout<<num<<endl;
+                result += min(cache[num],cache[k-num]);
+                cache[num] = 0;
+            }
+        }
+        return result;
+    }
+};
