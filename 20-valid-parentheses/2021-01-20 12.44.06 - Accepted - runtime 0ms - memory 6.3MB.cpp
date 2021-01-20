080051513e0a7ce00d2134bf7415aecb641070be
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2021-01-20 12:44:06 -0500

    Attempt: 2021-01-20 12.44.06 - Accepted - runtime 0ms - memory 6.3MB.cpp

---
+++ 20-valid-parentheses/2021-01-20 12.44.06 - Accepted - runtime 0ms - memory 6.3MB.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    bool checkTopOfStack(stack<char> &st,char typeOfBracket) {
+        if (st.empty()) {
+            return false;
+        }
+        char stackTop = st.top();
+        st.pop();
+        return stackTop == typeOfBracket;
+    }
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        
+        for(char c:s) {
+            bool topMatches;
+            if (c == ')') {
+                topMatches = checkTopOfStack(st,'(');
+                if (!topMatches) {
+                    return false;
+                }
+            } else if (c == '}') {
+                topMatches = checkTopOfStack(st,'{');
+                if (!topMatches) {
+                    return false;
+                }
+            } else if (c == ']') {
+                topMatches = checkTopOfStack(st,'[');
+                if (!topMatches) {
+                    return false;
+                }
+            } else {
+                st.push(c);
+            }
+        }
+        return st.empty();
+    }
+};
