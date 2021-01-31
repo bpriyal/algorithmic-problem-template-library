dde980d638e32132c8f5c3761f7ce83002512d57
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2021-01-31 12:33:47 -0500

    Attempt: 2021-01-31 12.33.47 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 31-next-permutation/2021-01-31 12.33.47 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int size = nums.size();
+        int i;
+        for(i = size - 2;i > 0;i--) {
+            if (nums[i] < nums[i+1]) {
+                break;
+            }
+        }
+        
+        if (i < 0) {
+            reverse(nums.begin(),nums.end());
+        } else {
+            int j;
+            for(j = size-1;j > i;--j) {
+                if (nums[j] > nums[i]) {
+                    break;
+                }
+            }
+            swap(nums[i],nums[j]);
+            reverse(nums.begin()+i+1,nums.end());
+        }
+    }
+};
