5be5bf248cd5f4c3180023d89247d4fd4d8f4285
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2021-02-09 13:03:23 -0500

    Attempt: 2021-02-09 13.03.23 - Accepted - runtime 36ms - memory 33.4MB.cpp

---
+++ 538-convert-bst-to-greater-tree/2021-02-09 13.03.23 - Accepted - runtime 36ms - memory 33.4MB.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int temp = 0;
+public:
+    TreeNode* convertBST(TreeNode* root) {
+        if (root == nullptr) {
+            return root;
+        }
+        if (root->right) {
+            convertBST(root->right);
+        }
+        temp = root->val = temp + root->val;
+        if (root->left) {
+            convertBST(root->left);
+        }
+        return root;
+    }
+};
