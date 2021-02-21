14c1f401a4356bf0605f7611d2873b69263946d3
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2021-02-21 11:34:35 -0500

    Attempt: 2021-02-21 11.34.35 - Accepted - runtime 0ms - memory 6MB.cpp

---
+++ 1033-broken-calculator/2021-02-21 11.34.35 - Accepted - runtime 0ms - memory 6MB.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int brokenCalc(int X, int Y) {
+        int result = 0;
+        while (Y > X) {
+            ++result;
+            if (Y%2 == 0) {
+                Y /= 2;
+            } else {
+                ++Y;
+            }
+        }
+        
+        return result + X-Y;
+    }
+};
