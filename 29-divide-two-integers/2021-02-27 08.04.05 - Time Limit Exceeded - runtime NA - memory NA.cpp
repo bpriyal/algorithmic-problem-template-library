7e66d1175e9ac3fb4aec94c222e7e9948baf95a2
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2021-02-27 08:04:05 -0500

    Attempt: 2021-02-27 08.04.05 - Time Limit Exceeded - runtime NA - memory NA.cpp

---
+++ 29-divide-two-integers/2021-02-27 08.04.05 - Time Limit Exceeded - runtime NA - memory NA.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        int result = 0;
+        if (dividend == INT_MIN && divisor == -1) {
+            return INT_MAX;
+        }
+        if (dividend == INT_MIN && divisor == 1) {
+            return INT_MIN;
+        }
+        bool isOneNegative = (dividend < 0 && divisor > 0) || (divisor < 0 && dividend >= 0);
+        if (dividend > 0) {
+            dividend = -dividend;
+        }
+        if (divisor > 0) {
+            divisor = -divisor;
+        }
+        while (dividend <= divisor) {
+            dividend -= divisor;
+            ++result;
+        }
+        if (isOneNegative) {
+            return -result;
+        } else {
+            return result;
+        }
+    }
+};
