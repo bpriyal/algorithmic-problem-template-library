59c371c3091c2d1aee64c197e0533cd8cd4925f7
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2021-03-13 01:48:03 -0500

    Attempt: 2021-03-13 01.48.03 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/2021-03-13 01.48.03 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool hasAllCodes(string s, int k) {
+        unordered_map<string,int> cache;
+        
+        if (s.size() < k) {
+            return false;
+        }
+        
+        for(int i = 0;i <= s.size() - k;++i) {
+            ++cache[s.substr(i,k)];
+        }
+        
+        for(int i = 0;i < pow(2,k)-1;++i) {
+            string temp = "";
+            int num = i;
+            //cout<<"number"<<i<<endl;
+            while(temp.size() != k) {
+                temp += to_string(num%2);
+                //cout<<temp<<endl;
+                num = num >> 1;
+            }
+            
+            if (cache.find(temp) == cache.end()) {
+                //cout<<temp<<endl;
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
