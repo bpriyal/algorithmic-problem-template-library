fd8de642a12e6ee7ca2065fac463395db0e105f9
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2021-03-13 09:19:55 -0500

    Attempt: 2021-03-13 09.19.55 - Accepted - runtime 112ms - memory 16.8MB.cpp

---
+++ 843-binary-trees-with-factors/2021-03-13 09.19.55 - Accepted - runtime 112ms - memory 16.8MB.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numFactoredBinaryTrees(vector<int>& arr) {
+        int div = pow(10,9) + 7;
+        unordered_map<int,long> dp;
+        sort(begin(arr),end(arr));
+        int res = 0;
+        // dp[i] means number of trees with root as i
+        for(int i = 0;i < arr.size();++i) {
+            dp[arr[i]] = 1;
+            for(int j = 0;j < i;++j) {
+                if (arr[i]%arr[j] == 0) {
+                    dp[arr[i]] = (dp[arr[i]] + dp[arr[j]] * dp[arr[i]/arr[j]]) % div;
+                }
+            }
+            res = (res + dp[arr[i]]) % div;
+        }
+        
+        return res;
+    }
+};
