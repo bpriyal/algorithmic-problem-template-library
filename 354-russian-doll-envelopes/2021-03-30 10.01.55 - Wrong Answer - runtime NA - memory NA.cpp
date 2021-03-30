fbe08f7e033a29033a7eab21576d86016a88d305
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2021-03-30 10:01:55 -0400

    Attempt: 2021-03-30 10.01.55 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 354-russian-doll-envelopes/2021-03-30 10.01.55 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        sort(begin(envelopes),end(envelopes),[](const vector<int> &a,const vector<int> &b) {
+           if (a[0] < b[0]) {
+               return true;
+           } else if (a[0] == b[0]) {
+               return a[1] < b[1];
+           } else {
+               return false;
+           }
+        });
+        
+        int size = envelopes.size();
+        vector<int> dp(size+1,1);
+        
+        for(int i = 1;i < size;++i) {
+            for(int j = i-1;j >= 0;--j) {
+                if (envelopes[i][0] > envelopes[j][0] && envelopes[i][1] > envelopes[j][1]) {
+                    dp[i] = dp[j] + 1;
+                    break;
+                }
+            }
+        }
+        
+        return dp[size-1];
+    }
+};
