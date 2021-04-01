bc3469b3e8b26bfbc411f2b6c2b603e9544ebc55
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2021-04-01 06:56:53 -0400

    Attempt: 2021-04-01 06.56.53 - Accepted - runtime 168ms - memory 110.3MB.cpp

---
+++ 234-palindrome-linked-list/2021-04-01 06.56.53 - Accepted - runtime 168ms - memory 110.3MB.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if (head == nullptr) {
+            return true;
+        }
+        int size = 0;
+        ListNode *temp = head;
+        
+        while(temp != nullptr) {
+            ++size;
+            temp = temp->next;
+        }
+        
+        int i = 0;
+        ListNode *curr = nullptr;
+        ListNode *ahead = head;
+        ListNode* ahead2 = nullptr;
+        
+        while (i < size/2) {
+            ahead2 = ahead->next;
+            ahead->next = curr;
+            curr = ahead;
+            ahead = ahead2;
+            ++i;
+        }
+        
+        if (size%2 == 0) {
+        } else {
+            ahead = ahead->next;
+        }
+        
+        //printf("curr:%d||ahead:%d\n",curr->val,ahead->val);
+        
+        while(curr && ahead) {
+            if (curr->val != ahead->val) {
+                return false;
+            }
+            curr = curr->next;
+            ahead = ahead->next;
+        }
+        
+        if (!curr && !ahead) {
+            return true;
+        }
+        return false;
+    }
+};
