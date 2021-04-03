e4434227b5ec33b8ffffb7f8a84c85cc5e7815ec
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2021-04-03 13:10:45 -0400

    Attempt: 2021-04-03 13.10.45 - Time Limit Exceeded - runtime NA - memory NA.cpp

---
+++ 32-longest-valid-parentheses/2021-04-03 13.10.45 - Time Limit Exceeded - runtime NA - memory NA.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    bool isValid(const string &s) {
+        stack<int> st;
+        
+        for(char c:s) {
+            if (c == '(') {
+                st.push(c);
+            } else {
+                if (st.empty()) {
+                    return false;
+                } else {
+                    st.pop();
+                }
+            }
+        }
+        return st.empty();
+    }
+public:
+    int longestValidParentheses(string s) {
+        int size = s.size();
+        int result = 0;
+        for(int i = 0;i < size;++i) {
+            for(int j = 2;i+j <= size;j+=2) {
+                printf("i:%d||j:%d\n",i,j);
+                if (isValid(s.substr(i,j))) {
+                    cout<<s.substr(i,j)<<endl;
+                    result = max(result,j);
+                }
+            }
+        }
+        
+        return result;
+    }
+};
