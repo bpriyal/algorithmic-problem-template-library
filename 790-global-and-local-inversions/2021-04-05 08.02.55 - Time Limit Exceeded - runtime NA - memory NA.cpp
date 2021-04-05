4ebf41da489712682c9309a8e9ba4e94dba2e9da
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2021-04-05 08:02:55 -0400

    Attempt: 2021-04-05 08.02.55 - Time Limit Exceeded - runtime NA - memory NA.cpp

---
+++ 790-global-and-local-inversions/2021-04-05 08.02.55 - Time Limit Exceeded - runtime NA - memory NA.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isIdealPermutation(vector<int>& A) {
+        int size = A.size();
+        for(int i = 0;i < size - 2;++i) {
+            for(int j = i+2;j < size;++j) {
+                if (A[j] < A[i]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
