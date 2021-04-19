d8d8b24348e28935f0b7a9b684b3c5b635117cc5
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2021-04-19 02:56:07 -0400

    Attempt: 2021-04-19 02.56.07 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 19-remove-nth-node-from-end-of-list/2021-04-19 02.56.07 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* ptr1 = head,*ptr2 = head;
+        
+        if (n == 1) {
+            if (ptr1->next == nullptr) {
+                return nullptr;
+            }
+            
+            while(ptr1->next->next != nullptr) {
+                ptr1 = ptr1->next;
+            }
+            
+            ptr1->next = nullptr;
+            return head;
+        }
+        
+        int counter = 0;
+        
+        while(ptr1->next != nullptr) {
+            ptr1 = ptr1->next;
+            ++counter;
+            if (counter%n==0) {
+                cout<<"kkk"<<endl;
+                ptr2 = ptr2->next;
+                counter = 0;
+            }
+        }
+        cout<<"he;;"<<endl;
+        ptr2->next = ptr1;
+        
+        return head;
+    }
+};
