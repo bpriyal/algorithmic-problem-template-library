6915d5336b161e85e0a23ae6dd1a88964b319afa
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2021-04-22 13:52:46 -0400

    Attempt: 2021-04-22 13.52.46 - Accepted - runtime 48ms - memory 21.7MB.cpp

---
+++ 554-brick-wall/2021-04-22 13.52.46 - Accepted - runtime 48ms - memory 21.7MB.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int leastBricks(vector<vector<int>>& wall) {
+        unordered_map<int,int> cache;
+        int rows = wall.size();
+        
+        for(int i =0;i < rows;++i) {
+            int temp = 0;
+            for(int j = 0;j < wall[i].size() - 1;++j) {
+                ++cache[temp+wall[i][j]];
+                temp += wall[i][j];
+            } 
+        }
+        
+        
+        int result = INT_MIN;
+        for(auto it = cache.begin();it != cache.end();++it) {
+            result = max(result,it->second);
+        }
+        
+        return result == INT_MIN? rows:rows-result;
+    }
+};
