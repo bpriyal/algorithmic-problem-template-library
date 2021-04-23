b0ab4701c673ffb9f3ae5a17ed4eac7b56d97580
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2021-04-23 13:29:27 -0400

    Attempt: 2021-04-23 13.29.27 - Accepted - runtime 16ms - memory 14MB.cpp

---
+++ 696-count-binary-substrings/2021-04-23 13.29.27 - Accepted - runtime 16ms - memory 14MB.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int countBinarySubstrings(string s) {
+        vector<int> counter;
+        
+        int count = 1;
+        int result = 0;
+        
+        for(int i = 1;i < s.size();++i) {
+            if (s[i] != s[i-1]) {
+                if (count == 0) {
+                    ++count;
+                } else {
+                    counter.push_back(count);
+                    count = 0;
+                }
+            }
+            ++count;
+        }
+        counter.push_back(count);
+
+        for(int i = 0;i < counter.size()-1;++i) {
+            result += min(counter[i],counter[i+1]);
+        }
+        
+        return result;
+    }
+};
