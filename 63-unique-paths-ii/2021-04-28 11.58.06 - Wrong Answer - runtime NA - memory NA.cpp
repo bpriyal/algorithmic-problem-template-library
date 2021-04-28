6b8f7b435b17bff14e1a6736b5e955c4ee9a8667
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2021-04-28 11:58:06 -0400

    Attempt: 2021-04-28 11.58.06 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 63-unique-paths-ii/2021-04-28 11.58.06 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    int row,col;
+    int result;
+    vector<vector<int>> dp;
+    int util(const vector<vector<int>>& obstacleGrid,int i,int j) {
+        if (i >= row || j >= col || obstacleGrid[i][j] == 1) {
+            return 0;
+        }
+        
+        if (i == row - 1 && j == col - 1) {
+            return 1;
+        }
+        
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        
+        dp[i][j] = util(obstacleGrid,i+1,j) + util(obstacleGrid,i,j+1);
+        
+        return dp[i][j];
+    }
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        row = obstacleGrid.size();
+        col = obstacleGrid[0].size();
+        result = 0;
+        
+        dp.resize(row,vector<int>(col,-1));
+        
+        util(obstacleGrid,0,0);
+        
+        return dp[0][0];
+    }
+};
