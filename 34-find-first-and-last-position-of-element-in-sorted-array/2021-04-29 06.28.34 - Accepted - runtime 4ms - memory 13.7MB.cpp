c430f57307d2d9a10d6e976be57ab03868abae30
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2021-04-29 06:28:34 -0400

    Attempt: 2021-04-29 06.28.34 - Accepted - runtime 4ms - memory 13.7MB.cpp

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/2021-04-29 06.28.34 - Accepted - runtime 4ms - memory 13.7MB.cpp
@@ -0,0 +1,39 @@
+class Solution {
+    int minI;
+    int maxI;
+    void binary(vector<int>& nums,int target,int l,int r) {
+        if (r < l) {
+            return;
+        }
+        
+        int mid = l + (r-l)/2;
+        
+        if (nums[mid] == target) {
+            minI = min(minI,mid);
+            maxI = max(maxI,mid);
+            
+            binary(nums,target,l,mid-1);
+            binary(nums,target,mid+1,r);
+        } else if (nums[mid] < target) {
+            binary(nums,target,mid+1,r);
+        } else {
+            binary(nums,target,l,mid-1);
+        }
+    }
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int size = nums.size();
+        int l = 0;
+        int r = size - 1;
+        minI = INT_MAX;
+        maxI = INT_MIN;
+        
+        binary(nums,target,l,r);
+        
+        if (minI == INT_MAX) {
+            minI = maxI = -1;
+        }
+        
+        return vector<int>({minI,maxI});
+    }
+};
