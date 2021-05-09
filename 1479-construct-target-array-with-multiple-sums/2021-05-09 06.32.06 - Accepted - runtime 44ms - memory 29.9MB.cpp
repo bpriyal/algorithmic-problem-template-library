b73a72e2f16c9a1fa09a267fdeb6ab7d79b5cfcd
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2021-05-09 06:32:06 -0400

    Attempt: 2021-05-09 06.32.06 - Accepted - runtime 44ms - memory 29.9MB.cpp

---
+++ 1479-construct-target-array-with-multiple-sums/2021-05-09 06.32.06 - Accepted - runtime 44ms - memory 29.9MB.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& target) {
+        int size = target.size();
+        priority_queue<int> pq;
+        long sum = 0;
+        for(int i = 0;i < size;++i) {
+            pq.push(target[i]);
+            sum += target[i];
+        }
+        
+        while (true) {
+            int max = pq.top();
+            pq.pop();
+            sum -= max;
+            
+            if (max == 1 || sum == 1) {
+                return true;
+            }
+            if (max < sum || sum == 0 || max%sum == 0) {
+                return false;
+            }
+            
+            max = max%sum;
+            pq.push(max);
+            sum += max;
+        }
+        
+        return true;
+    }
+};
