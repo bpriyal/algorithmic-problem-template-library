b9e70c7e4a17ee9497b9a7a2144845b7f36d99c5
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2021-05-12 14:30:59 -0400

    Attempt: 2021-05-12 14.30.59 - Accepted - runtime 20ms - memory 15.2MB.cpp

---
+++ 304-range-sum-query-2d-immutable/2021-05-12 14.30.59 - Accepted - runtime 20ms - memory 15.2MB.cpp
@@ -0,0 +1,34 @@
+class NumMatrix {
+    vector<vector<int>> cache;
+    int row,col;
+public:
+    NumMatrix(vector<vector<int>>& matrix) {
+        row = matrix.size();
+        col = matrix[0].size();
+        cache.resize(row,vector<int>(col,0));
+        
+        for(int i = 0;i < row;++i) {
+            int sum = 0;
+            for(int j = 0;j < col;++j) {
+                cache[i][j] = sum + matrix[i][j];
+                sum = cache[i][j];
+            }
+        }
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        int sum = 0;
+        
+        for(int i = row1;i <=row2;++i) {
+            sum += cache[i][col2]-(col1 > 0? cache[i][col1-1]:0);
+        }
+        
+        return sum;
+    }
+};
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
