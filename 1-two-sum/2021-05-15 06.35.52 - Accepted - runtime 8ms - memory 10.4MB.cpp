a10af7cd43c0f6fbeab5abbb1ed4a2f374ee6c8e
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2021-05-15 06:35:52 -0400

    Attempt: 2021-05-15 06.35.52 - Accepted - runtime 8ms - memory 10.4MB.cpp

---
+++ 1-two-sum/2021-05-15 06.35.52 - Accepted - runtime 8ms - memory 10.4MB.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,pair<int,int>> cache;
+        int size = nums.size();
+        
+        for(int i = 0;i < size;++i) {
+            cache[nums[i]].first = i;
+            ++cache[nums[i]].second;
+        }
+        
+        for(int i = 0;i < size;++i) {
+            /*if (nums[i] >= target && target != 0) {
+                continue;
+            }*/
+            int rem = target - nums[i];
+            
+            if (cache.find(rem) != cache.end()) {
+                //cout<<"hereeee"<<endl;
+                if (rem == nums[i] && cache[rem].second > 1) {
+                    //cout<<"here"<<endl;
+                    return {i,cache[rem].first};
+                } 
+                
+                if (rem != nums[i]) {
+                    return {i,cache[rem].first};
+                }
+            }
+        }
+        
+        return {0,0};
+    }
+};
