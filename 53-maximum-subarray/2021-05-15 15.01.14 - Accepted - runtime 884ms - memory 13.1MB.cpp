5e1f885cf8460a69197b93c531486d004059fcd0
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2021-05-15 15:01:14 -0400

    Attempt: 2021-05-15 15.01.14 - Accepted - runtime 884ms - memory 13.1MB.cpp

---
+++ 53-maximum-subarray/2021-05-15 15.01.14 - Accepted - runtime 884ms - memory 13.1MB.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int size = nums.size();
+        
+        int result = nums[0];
+        
+        for(int i = 1;i < size;++i) {
+            nums[i] = nums[i] + nums[i-1];
+        }
+        
+        for(int i = 0;i < size;++i) {
+            result = max(result,nums[i]);
+            for(int j = i+1;j < size;++j) {
+                result = max(result,nums[j]-nums[i]);
+            }
+        }
+        
+        return result;
+    }
+};
