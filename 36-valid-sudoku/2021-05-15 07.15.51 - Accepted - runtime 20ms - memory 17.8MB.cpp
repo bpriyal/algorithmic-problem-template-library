e89533c8b6484ed2784a58045bc3dc2ca8de0c01
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2021-05-15 07:15:51 -0400

    Attempt: 2021-05-15 07.15.51 - Accepted - runtime 20ms - memory 17.8MB.cpp

---
+++ 36-valid-sudoku/2021-05-15 07.15.51 - Accepted - runtime 20ms - memory 17.8MB.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        int row[9][9] = {0};
+        int col[9][9] = {0};
+        int block[9][9] = {0};
+        
+        for(int i = 0;i < 9;++i) {
+            for(int j = 0;j < 9;++j) {
+                if (board[i][j] != '.') {
+                    int x = 3*(i/3)+j/3;
+                    int y = board[i][j]-'0' - 1;
+                    ++row[i][y];
+                    ++col[j][y];
+                    ++block[x][y];
+                    
+                    if (row[i][y] > 1 || col[j][y] > 1 || block[x][y] > 1) {
+                        return false;
+                    }
+                }
+            }
+        }
+        
+        return true;
+    }
+};
