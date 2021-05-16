a793016acee047e55c1ffb031051471ba572321c
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2021-05-16 04:16:36 -0400

    Attempt: 2021-05-16 04.16.36 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 1008-binary-tree-cameras/2021-05-16 04.16.36 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int util(TreeNode* root,bool flag) {
+        if (root == nullptr) {
+            return 0;
+        }
+        
+        int result = INT_MAX;
+        // put a camera at this node
+        result = min(result,util(root->left,true)+util(root->right,true)+1);
+        
+        //not put a camera
+        if (flag) {
+            result = min(result,util(root->left,false)+util(root->right,false));
+        }
+        
+        return result;
+    }
+public:
+    int minCameraCover(TreeNode* root) {
+        return util(root,true);
+    }
+};
