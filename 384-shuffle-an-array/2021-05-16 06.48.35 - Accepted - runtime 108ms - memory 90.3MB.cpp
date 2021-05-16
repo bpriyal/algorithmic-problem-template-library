f88ea045e0f7524fd47f817cc607376888b6ed59
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2021-05-16 06:48:35 -0400

    Attempt: 2021-05-16 06.48.35 - Accepted - runtime 108ms - memory 90.3MB.cpp

---
+++ 384-shuffle-an-array/2021-05-16 06.48.35 - Accepted - runtime 108ms - memory 90.3MB.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    vector<int> original;
+    vector<int> shuf;
+public:
+    Solution(vector<int>& nums) {
+        original = shuf = nums;
+    }
+    
+    /** Resets the array to its original configuration and return it. */
+    vector<int> reset() {
+        return original;
+    }
+    
+    /** Returns a random shuffling of the array. */
+    vector<int> shuffle() {
+        random_shuffle(shuf.begin(),shuf.end());
+        return shuf;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(nums);
+ * vector<int> param_1 = obj->reset();
+ * vector<int> param_2 = obj->shuffle();
+ */
