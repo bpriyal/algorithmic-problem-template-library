fe5bd32d1eb588dfc5cb69ae93a3beb4cefac07f
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2021-05-16 06:45:41 -0400

    Attempt: 2021-05-16 06.45.41 - Accepted - runtime 0ms - memory 6.5MB.cpp

---
+++ 118-pascals-triangle/2021-05-16 06.45.41 - Accepted - runtime 0ms - memory 6.5MB.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> result;
+        result.push_back({1});
+        
+        if (numRows == 1) {
+            return result;
+        }
+        
+        result.push_back({1,1});
+        
+        if (numRows == 2) {
+            return result;
+        }
+        
+        for(int i = 3;i <= numRows;++i) {
+            vector<int> temp(i,1);
+            
+            for(int j = 1;j < i-1;++j) {
+                temp[j] = result[i-2][j-1]+result[i-2][j];
+                //cout<<"here"<<endl;
+            }
+            
+            result.push_back(temp);
+        }
+        
+        return result;
+    }
+};
