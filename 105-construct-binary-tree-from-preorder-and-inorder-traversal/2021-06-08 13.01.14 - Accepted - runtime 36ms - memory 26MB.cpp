5444a5e2f41947b7d02754a0faed10987a0c6552
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2021-06-08 13:01:14 -0400

    Attempt: 2021-06-08 13.01.14 - Accepted - runtime 36ms - memory 26MB.cpp

---
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/2021-06-08 13.01.14 - Accepted - runtime 36ms - memory 26MB.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int size;
+    TreeNode* util(const vector<int> &preorder,const vector<int> &inorder,int i,int l,int r) {
+        if (l > r || i >= size) {
+            return nullptr;
+        }
+        int index;
+        for(int x = l;x <= r;++x) {
+            if (preorder[i] == inorder[x]) {
+                index = x;
+                break;
+            }
+        }
+        
+        TreeNode* node = new TreeNode(preorder[i]);
+        node->left = util(preorder,inorder,i+1,l,index-1);
+        node->right = util(preorder,inorder,i+1+index-l,index+1,r);
+        
+        return node;
+    }
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        size = preorder.size();
+        
+        return util(preorder,inorder,0,0,size-1);
+    }
+};
