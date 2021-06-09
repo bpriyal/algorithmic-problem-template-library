4085eeb08bff7460c36d14ddf80dc981b7b05751
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2021-06-09 13:10:22 -0400

    Attempt: 2021-06-09 13.10.22 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 1814-jump-game-vi/2021-06-09 13.10.22 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxResult(vector<int>& nums, int k) {
+        int size = nums.size();
+        vector<int> dp(size,0);
+        
+        for(int i = size-1;i >= 0;--i) {
+            dp[i] = nums[i];
+            int _max = 0;
+            for(int j = i+1;j <= min(size-1,i+k);++j) {
+                _max = max(_max,dp[j]);
+            }
+            
+            dp[i] += _max;
+        }
+        
+        /*for(int x:dp) {
+            cout<<x<<"\t";
+        }*/
+        
+        return dp[0];
+    }
+};
