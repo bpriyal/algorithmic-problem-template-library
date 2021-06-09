71d1fc3175f433e591217d33ef64b464b76754f7
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2021-06-09 13:46:22 -0400

    Attempt: 2021-06-09 13.46.22 - Accepted - runtime 188ms - memory 78.4MB.cpp

---
+++ 1814-jump-game-vi/2021-06-09 13.46.22 - Accepted - runtime 188ms - memory 78.4MB.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxResult(vector<int>& nums, int k) {
+        int size = nums.size();
+        long curr = 0;
+        deque<int> q;
+        
+        for(int i = size-1;i >= 0;--i) {
+            curr = nums[i] + (q.empty()? 0:nums[q.front()]);
+            
+            while (!q.empty() && curr > nums[q.back()]) {
+                q.pop_back();
+            }
+            
+            q.push_back(i);
+            
+            if (i+k <= q.front()) {
+                q.pop_front();
+            }
+            
+            nums[i] = curr;
+        }
+        
+        return curr;
+    }
+};
