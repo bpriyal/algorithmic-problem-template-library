55d851f81fe3044e66ab8b0ca37f8caca2cc7b39
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2021-06-13 04:05:22 -0400

    Attempt: 2021-06-13 04.05.22 - Time Limit Exceeded - runtime NA - memory NA.cpp

---
+++ 336-palindrome-pairs/2021-06-13 04.05.22 - Time Limit Exceeded - runtime NA - memory NA.cpp
@@ -0,0 +1,77 @@
+class Solution {
+    bool util(const string &a,const string &b) {
+        int sizea = a.size();
+        int sizeb = b.size();
+        
+        if (sizea > sizeb) {
+            int i = 0;
+            int j = sizeb-1;
+            
+            while(j >= 0) {
+                if (a[i] != b[j]) {
+                    return false;
+                } else {
+                    ++i;
+                    --j;
+                }
+            }
+            j = sizea-1;
+            while (i < j) {
+                if (a[i] != a[j]) {
+                    return false;
+                } else {
+                    ++j;
+                    --j;
+                }
+            }
+        } else if (sizea < sizeb) {
+            //cout<<a<<" "<<b<<endl;
+            int i = 0;
+            int j = sizeb-1;
+            
+            while (i < sizea) {
+                if (a[i] != b[j]) {
+                    return false;
+                } else {
+                    ++i;
+                    --j;
+                }
+            }
+            i = 0;
+          //cout<<"reached here"<<i<<" "<<j<<endl;  
+            
+            while (i < j) {
+                if (b[i] != b[j]) {
+                    return false;
+                } else {
+                    ++i;
+                    --j;
+                }
+            }
+        } else {
+            for(int i = 0;i < sizea;++i) {
+                if (a[i] != b[sizeb-1-i]) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+public:
+    vector<vector<int>> palindromePairs(vector<string>& words) {
+        int size = words.size();
+        vector<vector<int>> result;
+        
+        for(int i = 0;i < size;++i) {
+            for(int j = 0;j < size;++j) {
+                if (i != j && util(words[i],words[j])) {
+                    //cout<<i<<" "<<j<<endl;
+                    result.push_back({i,j});
+                }
+            }
+        }
+        
+        return result;
+    }
+};
