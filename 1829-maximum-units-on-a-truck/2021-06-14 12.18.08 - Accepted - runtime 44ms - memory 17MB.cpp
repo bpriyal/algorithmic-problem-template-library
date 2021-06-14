072482af6c67ee22f7bc2cca9d1eb6b79e3eabc8
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2021-06-14 12:18:08 -0400

    Attempt: 2021-06-14 12.18.08 - Accepted - runtime 44ms - memory 17MB.cpp

---
+++ 1829-maximum-units-on-a-truck/2021-06-14 12.18.08 - Accepted - runtime 44ms - memory 17MB.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+        sort(begin(boxTypes),end(boxTypes),[](const vector<int> &a,const vector<int> &b) {
+           return a[1] > b[1]; 
+        });
+        
+        int units = 0;
+        
+        for(auto boxType:boxTypes) {
+            if (truckSize == 0) {
+                return units;
+            }
+            int temp = min(boxType[0],truckSize);
+            units += temp*boxType[1]; 
+            truckSize -= temp;
+        }
+        
+        return units;
+    }
+};
