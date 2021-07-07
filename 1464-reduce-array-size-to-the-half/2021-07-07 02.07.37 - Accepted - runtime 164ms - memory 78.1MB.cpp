253dfa480bf8930e9f6fb1c60037cba487080c5f
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2021-07-07 02:07:37 -0400

    Attempt: 2021-07-07 02.07.37 - Accepted - runtime 164ms - memory 78.1MB.cpp

---
+++ 1464-reduce-array-size-to-the-half/2021-07-07 02.07.37 - Accepted - runtime 164ms - memory 78.1MB.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        unordered_map<int,int> cache;
+        
+        for(int num:arr) {
+            ++cache[num];
+        }
+        
+        vector<int> frequency;
+        
+        for(auto it=cache.begin();it != cache.end();++it) {
+            frequency.push_back(it->second);
+        }
+        sort(frequency.begin(),frequency.end(),greater<int>());
+        
+        int size = arr.size();
+        int run = 0;
+        int result = 0;
+        int i = 0;
+        while (run < size/2) {
+            run += frequency[i];
+            ++i;
+            ++result;
+        }
+        
+        return result;
+    }
+};
