1869b7304d0546c3f4361c3429d2303e3068f418
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-01-20 13:32:21 -0500

    Attempt: 2022-01-20 13.32.21 - Accepted - runtime 0ms - memory 6MB.cpp

---
+++ 9-palindrome-number/2022-01-20 13.32.21 - Accepted - runtime 0ms - memory 6MB.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        string s = "";
+        
+        while (x != 0) {
+            s += x%10;
+            x /= 10;
+        }
+        
+        int size = s.size();
+        
+        for (int i = 0; i < size/2; ++i) {
+            if (s[i] != s[size-1-i]) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
