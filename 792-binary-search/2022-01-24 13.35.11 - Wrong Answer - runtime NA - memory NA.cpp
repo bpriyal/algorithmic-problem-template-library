2d3df59f8bdeeb23d560e917056e8f12ca8af5b7
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-01-24 13:35:11 -0500

    Attempt: 2022-01-24 13.35.11 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 792-binary-search/2022-01-24 13.35.11 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    int bst(const vector<int>& nums, int l, int h, int target) {
+        int mid = l + (h-l)/2;
+        
+        if (h <= l) return -1;
+        
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            return bst(nums, mid + 1, h, target);
+        } else {
+            return bst(nums, l, mid - 1, target);
+        }
+    } 
+public:
+    int search(vector<int>& nums, int target) {
+        return bst(nums, 0, nums.size(), target);
+    }
+};
