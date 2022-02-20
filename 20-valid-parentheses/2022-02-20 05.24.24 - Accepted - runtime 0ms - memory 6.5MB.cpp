4f17e098cd9bc516fea5d57ceb29d5e17271b735
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-02-20 05:24:24 -0500

    Attempt: 2022-02-20 05.24.24 - Accepted - runtime 0ms - memory 6.5MB.cpp

---
+++ 20-valid-parentheses/2022-02-20 05.24.24 - Accepted - runtime 0ms - memory 6.5MB.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        unordered_map<char,char> masterList({
+            {'(', ')'},
+            {'{', '}'},
+            {'[', ']'}
+        });
+        
+        for (char c : s) {
+            if ( c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } else {
+                if (st.empty() || masterList[st.top()] != c) return false;
+                st.pop();
+            }
+        }
+        
+        return st.empty()? true:false;
+    }
+};
