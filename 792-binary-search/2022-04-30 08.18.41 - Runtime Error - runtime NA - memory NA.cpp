0eefae2633aae6e67224d469d1a7a74c226b0750
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-04-30 08:18:41 -0400

    Attempt: 2022-04-30 08.18.41 - Runtime Error - runtime NA - memory NA.cpp

---
+++ 792-binary-search/2022-04-30 08.18.41 - Runtime Error - runtime NA - memory NA.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int size = nums.size();
+        
+        int l = 0, mid = 0;
+        int h = nums.size();
+        
+        while (l <= h) {
+            mid = l + (h-l)/2;
+            
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                h = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        
+        return -1;
+    }
+};
