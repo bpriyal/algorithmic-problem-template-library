53e09a8bf4005f6de108c79f38afb3893d1c4c4a
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-05-01 14:17:16 -0400

    Attempt: 2022-05-01 14.17.16 - Runtime Error - runtime NA - memory NA.cpp

---
+++ 189-rotate-array/2022-05-01 14.17.16 - Runtime Error - runtime NA - memory NA.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        // [4,3,2,1] [7,6,5]
+        
+        int size = nums.size();
+        
+        reverse(nums.begin(), nums.begin() + size - k);
+        reverse(nums.end() - k, nums.end());
+        
+        reverse(nums.begin(), nums.end());
+    }
+};
