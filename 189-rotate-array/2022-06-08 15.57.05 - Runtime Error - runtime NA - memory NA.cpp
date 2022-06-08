0a7b02f7d0fe3cbbbaaeb1df88562f447719a8a6
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-06-08 15:57:05 -0400

    Attempt: 2022-06-08 15.57.05 - Runtime Error - runtime NA - memory NA.cpp

---
+++ 189-rotate-array/2022-06-08 15.57.05 - Runtime Error - runtime NA - memory NA.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    void revers(vector<int> &nums, int l, int r) {
+        for (int i = l;i < (l + r)/2;++i) {
+            int temp = nums[i];
+            nums[i] = nums[(r+l)-i-1];
+            nums[(r+l)-i-1] = temp;
+        }
+    }
+    
+    void printvec(vector<int> &nums) {
+        for(int i = 0;i < nums.size();++i) {
+            cout<<nums[i]<<"\t";
+        }
+        cout<<"\n";
+    }
+public:
+    void rotate(vector<int>& nums, int k) {
+        int size = nums.size();
+        revers(nums, 0, size);
+        printvec(nums);
+        revers(nums, 0, k);
+        printvec(nums);
+        revers(nums, k, size);
+        printvec(nums);
+    }
+};
