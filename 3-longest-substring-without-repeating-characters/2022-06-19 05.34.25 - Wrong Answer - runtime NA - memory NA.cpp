7d58800685c7b4a82a4ff47c1d4beac9e5ba3233
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-06-19 05:34:25 -0400

    Attempt: 2022-06-19 05.34.25 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 3-longest-substring-without-repeating-characters/2022-06-19 05.34.25 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int size = s.size();
+        vector<int> dp(size + 1);
+        unordered_map<char, int> cache;
+        int result = 0;
+        
+        if (size < 2) return size;
+        
+        dp[size] = 0;
+        
+        for(int i = size - 1;i >= 0;--i) {
+            if (cache.find(s[i]) != cache.end()) {
+                dp[i] = cache[s[i]] - i;
+            } else {
+                dp[i] = dp[i+1] + 1;
+            }
+            result = max(result,dp[i]);
+            cache[s[i]] = i;
+        }
+        
+        return result;
+    }
+};
