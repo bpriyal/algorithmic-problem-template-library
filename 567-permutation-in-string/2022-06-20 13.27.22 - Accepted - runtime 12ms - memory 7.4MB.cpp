d8837172f4f61bda8525beac8a47292c7035f521
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-06-20 13:27:22 -0400

    Attempt: 2022-06-20 13.27.22 - Accepted - runtime 12ms - memory 7.4MB.cpp

---
+++ 567-permutation-in-string/2022-06-20 13.27.22 - Accepted - runtime 12ms - memory 7.4MB.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    bool checkCache(vector<int> &cache) {
+        for(int i = 0;i < 256;++i) {
+            if (cache[i] != 0) return false;
+        }
+        
+        return true;
+    }
+public:
+    bool checkInclusion(string s1, string s2) {
+        int size1 = s1.size();
+        int size2 = s2.size();
+        
+        if (size2 < size1) return false;
+        
+        vector<int> cache(256, 0);
+        
+        for(int i = 0;i < size1;++i) ++cache[s1[i]-'a'];
+        
+        for(int i = 0;i < size1;++i) {
+            --cache[s2[i]-'a'];
+        }
+        
+        if (checkCache(cache)) return true;
+        
+        for(int i = size1;i < size2;++i) {
+            ++cache[s2[i-size1]-'a'];
+            --cache[s2[i]-'a'];
+            if (checkCache(cache)) return true;
+        }
+        
+        return false;
+    }
+};
