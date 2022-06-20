26b67b84b4b0de6d080af675253cb437df04b2ce
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-06-20 14:37:37 -0400

    Attempt: 2022-06-20 14.37.37 - Accepted - runtime 15ms - memory 14MB.cpp

---
+++ 733-flood-fill/2022-06-20 14.37.37 - Accepted - runtime 15ms - memory 14MB.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    int row, col;
+    void utility(vector<vector<int>>& image, int sr, int sc, int color, int currColor) {
+        cout<<sr<<"\t"<<sc<<"\n";
+        if (sr < 0 || sr >= row || sc < 0 || sc >= col || image[sr][sc] == color || image[sr][sc] != currColor) {
+            return;
+        }
+        
+        currColor = image[sr][sc];
+        image[sr][sc] = color;
+        
+        utility(image, sr - 1, sc, color, currColor);
+        utility(image, sr + 1, sc, color, currColor);
+        utility(image, sr, sc - 1, color, currColor);
+        utility(image, sr, sc + 1, color, currColor);
+    }
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        row = image.size();
+        col = image[0].size();
+        
+        if (image[sr][sc] != color)
+            utility(image, sr, sc, color, image[sr][sc]);
+        
+        return image;
+    }
+};
