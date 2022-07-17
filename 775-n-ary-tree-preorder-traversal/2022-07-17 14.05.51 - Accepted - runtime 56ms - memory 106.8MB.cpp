a8d64db9a055aaa2a7bd01aa73faee7b6017bd4a
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-07-17 14:05:51 -0400

    Attempt: 2022-07-17 14.05.51 - Accepted - runtime 56ms - memory 106.8MB.cpp

---
+++ 775-n-ary-tree-preorder-traversal/2022-07-17 14.05.51 - Accepted - runtime 56ms - memory 106.8MB.cpp
@@ -0,0 +1,33 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    vector<int> result;
+public:
+    vector<int> preorder(Node* root) {
+        if (!root) return result;
+        
+        result.push_back(root->val);
+        
+        for(auto &it : root->children) preorder(it);
+        
+        return result;
+    }
+};
