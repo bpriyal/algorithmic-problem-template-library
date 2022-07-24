0ffda3c275b19d358b46a7ffa78684e02b29a1b2
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-07-24 04:26:39 -0400

    Attempt: 2022-07-24 04.26.39 - Accepted - runtime 21ms - memory 7.1MB.cpp

---
+++ 424-longest-repeating-character-replacement/2022-07-24 04.26.39 - Accepted - runtime 21ms - memory 7.1MB.cpp
@@ -0,0 +1,39 @@
+class Solution {
+    bool util(const vector<int> &cache, int k) {
+        int highestFrequency = 0;
+        int sum = 0;
+        
+        for(int i = 0; i < 26; ++i) {
+            if (cache[i] != 0) {
+                highestFrequency = max(highestFrequency, cache[i]);
+                sum += cache[i];
+            }
+        }
+        
+        if (sum - highestFrequency > k) return false;
+         
+        return true;
+    }
+public:
+    int characterReplacement(string s, int k) {
+        int size = s.size();
+        int result = k;
+        
+        vector<int> cache(26, 0);
+        
+        for(int i = 0; i < k; ++i) ++cache[s[i] - 'A'];
+        
+        for(int i = k; i < size; ++i) {
+            ++cache[s[i] - 'A'];
+            
+            if (util(cache, k)) {
+                ++result;
+            }
+            else {
+                --cache[s[i - result] - 'A'];
+            }
+        }
+        
+        return result;
+    }
+};
