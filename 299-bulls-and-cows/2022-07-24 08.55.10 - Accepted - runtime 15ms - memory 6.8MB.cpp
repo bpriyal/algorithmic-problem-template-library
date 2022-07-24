50223e9b0c574e5f82238a7738424df21965ba5e
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-07-24 08:55:10 -0400

    Attempt: 2022-07-24 08.55.10 - Accepted - runtime 15ms - memory 6.8MB.cpp

---
+++ 299-bulls-and-cows/2022-07-24 08.55.10 - Accepted - runtime 15ms - memory 6.8MB.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        unordered_map<char, int> cache;
+        unordered_set<int> indicesToSkip;
+        int bulls = 0;
+        int cows = 0;
+        
+        for(int i = 0;i < secret.size(); ++i) {
+            if (secret[i] == guess[i]) {
+                indicesToSkip.insert(i);
+                ++bulls;
+            }
+            else {
+                if (cache.find(secret[i]) == cache.end()) {
+                    cache[secret[i]] = 1;
+                } else {
+                    ++cache[secret[i]];
+                }
+            }
+        }
+        
+        for(int i = 0; i < guess.size(); ++i) {
+            if (indicesToSkip.find(i) != indicesToSkip.end()) {
+                continue;
+            }
+            if (cache.find(guess[i]) != cache.end() && cache[guess[i]] > 0) {
+                ++cows;
+                --cache[guess[i]];
+            }
+        }
+        
+        string result = "";
+        result += to_string(bulls) + 'A' + to_string(cows) + 'B';
+        
+        return result;
+    }
+};
