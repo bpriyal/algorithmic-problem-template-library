ec7bae667c8bfda4ed843279ae437688e0a4b945
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-07-24 06:50:18 -0400

    Attempt: 2022-07-24 06.50.18 - Accepted - runtime 0ms - memory 6.7MB.cpp

---
+++ 394-decode-string/2022-07-24 06.50.18 - Accepted - runtime 0ms - memory 6.7MB.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<string> st;
+        int size = s.size();
+        
+        for(char c:s) {
+            if (c != ']') {
+                string s(1,c);
+                st.push(s);
+            } else {
+                string temp = "";
+                while(!st.empty() && st.top() != "[") {
+                    temp = st.top() + temp;
+                    st.pop();
+                }
+                st.pop();
+                string num = "";
+                while(!st.empty() && st.top() >= "0" && st.top() <="9") {
+                    num = st.top() + num;
+                    st.pop();
+                }
+                if (num.empty()) {
+                    num = "1";
+                }
+                int frequency = stoi(num);
+                string temp2 = "";
+                while(frequency) {
+                    temp2 += temp;
+                    --frequency;
+                }
+                //cout<<temp2<<endl;
+                st.push(temp2);
+            }
+        }
+        string result = "";
+        while(!st.empty()) {
+            result = st.top() + result;
+            st.pop();
+        }
+        return result;
+    }
+};
