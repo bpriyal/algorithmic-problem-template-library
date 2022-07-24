8ac5e5f3c9e431a7e799a19f48b80fe33391034a
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-07-24 04:55:23 -0400

    Attempt: 2022-07-24 04.55.23 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 1127-last-stone-weight/2022-07-24 04.55.23 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        int size = stones.size();
+        
+        std::priority_queue<int> cache;
+        
+        for(const int &stone : stones) {
+            cache.push(stone);
+        }
+        
+        while(cache.size() > 1) {
+            int w1 = cache.top();
+            cache.pop();
+            
+            int w2 = cache.top();
+            cache.pop();
+            
+            if (w1 != w2) cache.push(abs(w1-w2));
+        }
+        
+        return cache.top();
+    }
+};
