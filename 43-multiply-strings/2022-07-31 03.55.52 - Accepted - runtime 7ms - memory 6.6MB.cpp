069f5abd233a39e73b0a6d1aeb9e3e6eaa935c09
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-07-31 03:55:52 -0400

    Attempt: 2022-07-31 03.55.52 - Accepted - runtime 7ms - memory 6.6MB.cpp

---
+++ 43-multiply-strings/2022-07-31 03.55.52 - Accepted - runtime 7ms - memory 6.6MB.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+//         int size1 = num1.size();
+//         int size2 = num2.size();
+        
+//         if(size1 < size2) {
+//             cout<<"swappping..."<<endl;
+//             string temp = num1;
+//             num1 = num2;
+//             num2 = temp;
+            
+//             int intTemp = size1;
+//             size1 = size2;
+//             size2 = intTemp;
+//         }
+        
+//         int j = 0;
+//         long int result = 0;
+        
+//         while(j < size2) {
+//             long int run = 0;
+//             int rec = 0;
+//             int i = 0;
+//             while(i < size1) {
+//                 long int mult = (num1[size1 - i - 1] - '0') * (num2[size2 - j - 1] - '0') + rec;
+//                 run += (mult%10)*pow(10,i);
+//                 rec = mult/10;
+//                 ++i;
+//             }
+            
+//             run = rec*pow(10,i) + run;
+//             long int te = run*pow(10,j);
+//             cout<<"Adding: "<<result<<"+"<<te;
+//             result += run*pow(10,j);
+//             cout<<"="<<result<<endl;
+//             ++j;
+//         }
+        
+//         return to_string(result);
+        string sum(num1.size() + num2.size(), '0');
+    
+    for (int i = num1.size() - 1; 0 <= i; --i) {
+        int carry = 0;
+        for (int j = num2.size() - 1; 0 <= j; --j) {
+            int tmp = (sum[i + j + 1] - '0') + (num1[i] - '0') * (num2[j] - '0') + carry;
+            sum[i + j + 1] = tmp % 10 + '0';
+            carry = tmp / 10;
+        }
+        sum[i] += carry;
+    }
+    
+    size_t startpos = sum.find_first_not_of("0");
+    if (string::npos != startpos) {
+        return sum.substr(startpos);
+    }
+    return "0";
+    }
+};
