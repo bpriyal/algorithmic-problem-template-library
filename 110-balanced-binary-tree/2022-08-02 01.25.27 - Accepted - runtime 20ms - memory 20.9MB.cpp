67509ede9ac401b620d37b5cbcc266ca1a6ac814
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-08-02 01:25:27 -0400

    Attempt: 2022-08-02 01.25.27 - Accepted - runtime 20ms - memory 20.9MB.cpp

---
+++ 110-balanced-binary-tree/2022-08-02 01.25.27 - Accepted - runtime 20ms - memory 20.9MB.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    bool result;
+    int util(TreeNode* root) {
+        if (!root || !result) return 0;
+        
+        int left = util(root->left);
+        int right = util(root->right);
+        
+        result = result && abs(left-right) <= 1;
+        
+        return max(left, right) + 1;
+    }
+public:
+    bool isBalanced(TreeNode* root) {
+        result = true;
+        util(root);
+        return result;
+    }
+};
