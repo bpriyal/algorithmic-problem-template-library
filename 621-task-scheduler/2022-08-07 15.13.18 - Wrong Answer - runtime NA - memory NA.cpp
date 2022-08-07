f4192b7838443a35cdf1b9bb54395a70640d54b2
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-08-07 15:13:18 -0400

    Attempt: 2022-08-07 15.13.18 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 621-task-scheduler/2022-08-07 15.13.18 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        int size = tasks.size();
+        int result = 0;
+        
+        vector<pair<bool, int>> cache(26, {true, 0});
+        vector<char> wait(n+1, 'i');
+        int index = -1;
+        
+        for(auto &it : tasks) {
+            cache[it - 'A'] = { cache[it - 'A'].first, ++cache[it - 'A'].second };
+        }
+        
+        // cout<<"aaaks"<<endl;
+        
+        while (size > 0) {
+            
+            ++result;
+            
+            index = (index + 1) % (n + 1);
+            if (wait[index] != 'i') cache[wait[index] - 'A'].first = true;
+            
+            int flag = false;
+            for(int i = 0; i < 26; ++i) {
+                if (cache[i].first && cache[i].second > 0) {
+                    wait[index] = i + 'A';
+                    --cache[i].second;
+                    --size;
+                    cache[i].first = false;
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag) {
+                wait[index] = 'i';
+            }
+        }
+        
+        return result;
+    }
+};
