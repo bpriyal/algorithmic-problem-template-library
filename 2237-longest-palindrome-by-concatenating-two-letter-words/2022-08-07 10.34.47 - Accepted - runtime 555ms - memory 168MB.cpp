baf1783d0896c980276ff9a16333e65ae7d05cb1
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-08-07 10:34:47 -0400

    Attempt: 2022-08-07 10.34.47 - Accepted - runtime 555ms - memory 168MB.cpp

---
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/2022-08-07 10.34.47 - Accepted - runtime 555ms - memory 168MB.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int longestPalindrome(vector<string>& words) {
+        int size = words.size();
+        unordered_map<string, int> cache;
+        
+        int result = 0, unpaired = 0;
+        
+        for(auto& word : words) {
+            
+            if (word[0] == word[1]) {
+                if (cache[word] > 0) {
+                    --cache[word];
+                    --unpaired;
+                    result += 4;
+                } else {
+                    ++cache[word];
+                    ++unpaired;
+                }
+            } else {
+                string reverse = {word[1], word[0]};
+                
+                if (cache[reverse] > 0) {
+                    result += 4;
+                    --cache[reverse];
+                }
+                else ++cache[word];
+            }
+        }
+        
+        result += unpaired? 2 : 0;
+        
+        return result;
+    }
+};
