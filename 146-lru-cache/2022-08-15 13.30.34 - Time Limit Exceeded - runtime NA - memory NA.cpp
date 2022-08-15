bf78f1b757649e6ab3a2cbd244d69ed2b8a141bd
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-08-15 13:30:34 -0400

    Attempt: 2022-08-15 13.30.34 - Time Limit Exceeded - runtime NA - memory NA.cpp

---
+++ 146-lru-cache/2022-08-15 13.30.34 - Time Limit Exceeded - runtime NA - memory NA.cpp
@@ -0,0 +1,33 @@
+class LRUCache {
+public:
+    list<int> lru;
+    map<int,int> cache;
+    int size;
+    
+    LRUCache(int capacity) {
+        size = capacity;
+    }
+    
+    int get(int key) {
+        if (cache.find(key) == cache.end()) {
+            return -1;
+        }
+        lru.remove(key);
+        lru.push_front(key);
+        return cache[key];
+    }
+    
+    void put(int key, int value) {
+        if (cache.find(key) == cache.end()) {
+            if (lru.size() == size) {
+                int last = lru.back();
+                lru.pop_back();
+                cache.erase(last);
+            }
+        } else {
+            lru.remove(key);
+        }
+        cache[key] = value;
+        lru.push_front(key);
+    }
+};
