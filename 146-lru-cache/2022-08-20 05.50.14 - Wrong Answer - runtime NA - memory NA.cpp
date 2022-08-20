ca14b70c715d01f062563407b6c98bba64601786
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-08-20 05:50:14 -0400

    Attempt: 2022-08-20 05.50.14 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 146-lru-cache/2022-08-20 05.50.14 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,51 @@
+class LRUCache {
+    list<pair<int, int>> lru;
+    unordered_map<int, list<pair<int, int>>::iterator> cache;
+    int size;
+    int capacity;
+public:
+    LRUCache(int capacity) {
+        this->size = 0;
+        this->capacity = capacity;
+    }
+    
+    int get(int key) {
+        if (auto cit = cache.find(key); cit != cache.end()) {
+            int val = cit->second->second;
+            // lru.erase(cit->second);
+            // lru.emplace_back(std::make_pair(key, val));
+            
+            lru.splice(lru.begin(), lru, cit->second);
+            
+            // cache[key] = --lru.end();
+            
+            return val;
+        }
+        return -1;
+    }
+    
+    void put(int key, int value) {
+        if (auto cit = cache.find(key); cit != cache.end()) {
+            lru.splice(lru.begin(), lru, cit->second);
+            cit->second->second = value;
+        } else {
+            if (size == capacity) {
+                cache.erase(lru.back().first);
+                lru.pop_back();
+            } else {
+                ++size;
+            }
+        }
+        
+        lru.emplace_front(std::make_pair(key, value));
+        cache[key] = lru.begin();
+        // cout<<cache[key]->second<<endl;
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
