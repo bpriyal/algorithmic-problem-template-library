501d4b7bfc6d41a390240dd54d220b1d45c3585e
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-08-20 05:30:02 -0400

    Attempt: 2022-08-20 05.30.02 - Time Limit Exceeded - runtime NA - memory NA.cpp

---
+++ 146-lru-cache/2022-08-20 05.30.02 - Time Limit Exceeded - runtime NA - memory NA.cpp
@@ -0,0 +1,49 @@
+class LRUCache {
+    list<pair<int, int>> lru;
+    unordered_map<int, list<pair<int, int>>::iterator> cache;
+    int size;
+    int capacity;
+public:
+    LRUCache(int capacity) {
+        this->size = 0;
+        this->capacity = capacity;
+    }
+    
+    int get(int key) {
+        cout<<"in get"<<endl;
+        if (auto cit = cache.find(key); cit != cache.end()) {
+            int val = cit->second->second;
+            lru.erase(cit->second);
+            lru.emplace_back(std::make_pair(key, val));
+            cache[key] = --lru.end();
+            
+            return val;
+        }
+        return -1;
+    }
+    
+    void put(int key, int value) {
+        cout<<"in put"<<endl;
+        if (auto cit = cache.find(key); cit != cache.end()) {
+            lru.erase(cit->second);
+        } else {
+            if (size == capacity) {
+                cache.erase(lru.front().first);
+                lru.pop_front();
+            } else {
+                ++size;
+            }
+        }
+        
+        lru.emplace_back(std::make_pair(key, value));
+        cache[key] = --lru.end();
+        cout<<cache[key]->second<<endl;
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
