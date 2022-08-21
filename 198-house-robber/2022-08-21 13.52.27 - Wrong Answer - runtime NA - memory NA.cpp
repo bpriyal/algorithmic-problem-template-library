a3eadc4fb46e07fdae7a35e0e5d940118bfc979d
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-08-21 13:52:27 -0400

    Attempt: 2022-08-21 13.52.27 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 198-house-robber/2022-08-21 13.52.27 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    int size;
+    vector<vector<int>> dp;
+    int util(const vector<int>& nums, int i, int run, bool isLastUsed) {
+        if (i == size) {
+            return run;
+        }
+        
+        if (dp[i][isLastUsed] != -1) return dp[i][isLastUsed];
+        
+        int res = INT_MIN;
+        if (!isLastUsed) {
+            res = max(res, util(nums, i + 1, run + nums[i], true));
+        }
+        
+        res = max(res, util(nums, i + 1, run, false));
+        dp[i][isLastUsed] = res;
+        return res;
+    }
+public:
+    int rob(vector<int>& nums) {
+        size = nums.size();
+        dp.resize(size, vector<int>(2, -1));
+        
+        return util(nums, 0, 0, false);
+    }
+};
