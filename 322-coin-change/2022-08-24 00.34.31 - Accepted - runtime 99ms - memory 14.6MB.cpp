b7063d93e63bb78dee52ef8d3c995959e78a2a53
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-08-24 00:34:31 -0400

    Attempt: 2022-08-24 00.34.31 - Accepted - runtime 99ms - memory 14.6MB.cpp

---
+++ 322-coin-change/2022-08-24 00.34.31 - Accepted - runtime 99ms - memory 14.6MB.cpp
@@ -0,0 +1,47 @@
+class Solution {
+    int size;
+    vector<int> dp;
+    int util(vector<int>& coins, int amount, int run) {
+        if (amount == 0) return run;
+        if (amount < 0) return INT_MAX;
+        
+        if (dp[amount] != -1) return dp[amount];
+        
+        int res = INT_MAX;
+        
+        for(int i = size - 1; i >= 0; --i) {
+            if (coins[i] <= amount) {
+                res = min(res, util(coins,amount - coins[i], run + 1));   
+            }
+        }
+        
+        dp[amount] = res;
+        
+        return res;
+    }
+public:
+    int coinChange(vector<int>& coins, int amount) {
+//         size = coins.size();
+//         dp.resize(amount + 1, -1);
+        
+//         sort(coins.begin(), coins.end());
+        
+//         int result = util(coins, amount, 0);
+        
+//         return result == INT_MAX? -1 : result;
+        
+        vector<int> cache(amount+1, INT_MAX);
+        cache[0] = 0;
+        
+        sort(coins.begin(), coins.end());
+        
+        for(int i = 1; i <= amount; ++i) {
+            for(auto coin : coins) {
+                if (i - coin < 0) break;
+                cache[i] = min(cache[i], cache[i - coin] ==INT_MAX? cache[i - coin] : cache[i - coin] + 1);
+            }
+        }
+        
+        return cache[amount] == INT_MAX? -1 : cache[amount];
+    }
+};
