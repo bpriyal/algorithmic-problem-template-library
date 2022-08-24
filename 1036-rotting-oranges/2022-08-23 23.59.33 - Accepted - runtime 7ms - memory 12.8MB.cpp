b676d2d76faa7286ab35aea146bf59856413bf69
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-08-23 23:59:33 -0400

    Attempt: 2022-08-23 23.59.33 - Accepted - runtime 7ms - memory 12.8MB.cpp

---
+++ 1036-rotting-oranges/2022-08-23 23.59.33 - Accepted - runtime 7ms - memory 12.8MB.cpp
@@ -0,0 +1,41 @@
+class Solution {
+    int rows, cols;
+    int util(vector<vector<int>>& grid, int i, int j, int run) {
+        if (i >= rows || j >= cols || i < 0 || j < 0 || grid[i][j] == 0 || grid[i][j] == -1) return INT_MAX;
+        
+        if (grid[i][j] == 2) {
+            return run;
+        }
+        
+        grid[i][j] = -1;
+        
+        int a = util(grid, i + 1, j, run + 1);
+        int b = util(grid, i - 1, j, run + 1);
+        int c = util(grid, i, j + 1, run + 1);
+        int d = util(grid, i, j - 1, run + 1);
+        
+        int res = min({a, b, c ,d});
+        
+        grid[i][j] = 1;
+        
+        return res;
+    }
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        rows = grid.size();
+        cols = grid[0].size();
+        
+        int result = 0;
+        
+        for(int i = 0; i < rows; ++i) {
+            for(int j = 0; j < cols; ++j) {
+                if (grid[i][j] == 1) {
+                    result = max(result, util(grid, i, j, 0));
+                    if (result == INT_MAX) return -1;
+                }
+            }
+        }
+        
+        return result;
+    }
+};
