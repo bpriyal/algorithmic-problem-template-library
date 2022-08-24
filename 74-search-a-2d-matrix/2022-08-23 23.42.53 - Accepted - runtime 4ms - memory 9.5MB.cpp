95b514d546ad1048dd05e0cd2889fd0f02953e99
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-08-23 23:42:53 -0400

    Attempt: 2022-08-23 23.42.53 - Accepted - runtime 4ms - memory 9.5MB.cpp

---
+++ 74-search-a-2d-matrix/2022-08-23 23.42.53 - Accepted - runtime 4ms - memory 9.5MB.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    bool searchRow(const vector<int>& num, int size, int target) {
+        int l = 0;
+        int r = size - 1;
+        
+        while (l <= r) {
+            int mid = l + (r-l)/2;
+            
+            if (num[mid] == target) return true;
+            else if (num[mid] < target) l = mid + 1;
+            else r = mid - 1;
+        }
+        
+        return false;
+    }
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        
+        int r = 0;
+        int c = col - 1;
+        
+        while (r < row) {
+            int pivotVal = matrix[r][c];
+            
+            if (pivotVal == target) return true;
+            else if (pivotVal < target) ++r;
+            else return searchRow(matrix[r], col, target);
+        }
+        
+        return false;
+    }
+};
