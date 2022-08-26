6f25067930317686aa7bcf192d866aaee8154228
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-08-26 13:06:38 -0400

    Attempt: 2022-08-26 13.06.38 - Accepted - runtime 109ms - memory 13.7MB.cpp

---
+++ 152-maximum-product-subarray/2022-08-26 13.06.38 - Accepted - runtime 109ms - memory 13.7MB.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int size = nums.size();
+        int result = nums[0];
+        
+        int _max = result;
+        int _min = result;
+        
+        for(int i = 1; i < size; ++i) {
+            
+            if (nums[i] < 0) swap(_min, _max);
+            
+            _max = max(_max * nums[i], nums[i]);
+            _min = min(_min * nums[i], nums[i]);
+            
+            result = max({_max, _min, result});
+            cout<<i<<","<<result<<endl;
+        }
+        
+        return result;
+    }
+};
