8a7ea5193a3439565e2a0542a3ecade4f0327390
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-08-27 12:38:33 -0400

    Attempt: 2022-08-27 12.38.33 - Accepted - runtime 214ms - memory 16.4MB.cpp

---
+++ 16-3sum-closest/2022-08-27 12.38.33 - Accepted - runtime 214ms - memory 16.4MB.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        int size = nums.size();
+        
+        sort(nums.begin(), nums.end());
+        
+        int result = nums[0] + nums[1] + nums[2];
+        
+        for(int i = 0; i < size - 1; ++i) {
+            int newTarget = target - nums[i];
+            
+            int l = i + 1;
+            int r = size - 1;
+            
+            while (l < r) {
+                int sum = nums[l] + nums[r];
+                
+                if (abs(sum + nums[i] - target) < abs(result - target)) {
+                    result = sum + nums[i];
+                }
+                
+                if (sum == newTarget) {
+                    return target;
+                } else if (sum > newTarget) {
+                    --r;
+                } else {
+                    ++l;
+                }
+            }
+        }
+        
+        return result;
+    }
+};
