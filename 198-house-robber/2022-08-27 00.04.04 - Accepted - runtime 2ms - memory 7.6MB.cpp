f6c76d0e7f97177b3fb614166b4bf9c09c02bb87
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-08-27 00:04:04 -0400

    Attempt: 2022-08-27 00.04.04 - Accepted - runtime 2ms - memory 7.6MB.cpp

---
+++ 198-house-robber/2022-08-27 00.04.04 - Accepted - runtime 2ms - memory 7.6MB.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int size = nums.size();
+        
+        if (size == 0) return 0;
+        if (size == 1) return nums[0];
+        
+        nums[1] = max(nums[1], nums[0]);
+        
+        for(int i = 2; i < size; ++i) {
+            nums[i] = max(nums[i] + nums[i - 2], nums[i-1]);
+        }
+        
+        return nums[size-1];
+    }
+};
