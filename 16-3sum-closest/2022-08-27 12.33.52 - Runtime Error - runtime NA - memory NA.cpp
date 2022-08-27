d00e058553e688ffc8c8683f0adb3ffd2a273296
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-08-27 12:33:52 -0400

    Attempt: 2022-08-27 12.33.52 - Runtime Error - runtime NA - memory NA.cpp

---
+++ 16-3sum-closest/2022-08-27 12.33.52 - Runtime Error - runtime NA - memory NA.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        int size = nums.size();
+        
+        sort(nums.begin(), nums.end());
+        
+        int result = INT_MAX;
+        
+        for(int i = 0; i < size - 1; ++i) {
+            int newTarget = target - nums[i];
+            
+            int l = i + 1;
+            int r = size - 1;
+            
+            while (l < r) {
+                int sum = nums[l] + nums[r];
+                
+                if (abs(sum + nums[i] - target) < abs(result - target)) {
+                    cout<<"l:"<<nums[l]<<", r:"<<nums[r]<<", i: "<<nums[i]<<endl;
+                    result = sum + nums[i];
+                }
+                
+                if (sum == newTarget) {
+                    return target;
+                } else if (sum > target) {
+                    --r;
+                } else {
+                    ++l;
+                }
+            }
+        }
+        
+        return result;
+    }
+};
