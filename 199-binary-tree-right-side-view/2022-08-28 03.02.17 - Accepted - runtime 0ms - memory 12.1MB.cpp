dfb5df9219c2fe56aaabbdf1b0bca94308fe86f1
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-08-28 03:02:17 -0400

    Attempt: 2022-08-28 03.02.17 - Accepted - runtime 0ms - memory 12.1MB.cpp

---
+++ 199-binary-tree-right-side-view/2022-08-28 03.02.17 - Accepted - runtime 0ms - memory 12.1MB.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> result;
+        queue<TreeNode*> q;
+        
+        if (root) q.push(root);
+        
+        while(!q.empty()) {
+            int size = q.size();
+            TreeNode* temp = nullptr;
+            
+            while(size) {
+                --size;
+                
+                temp = q.front();
+                q.pop();
+                
+                if (temp->left) q.push(temp->left);
+                if (temp->right) q.push(temp->right);
+            }
+            
+            result.push_back(temp->val);
+        }
+        
+        return result;
+    }
+};
