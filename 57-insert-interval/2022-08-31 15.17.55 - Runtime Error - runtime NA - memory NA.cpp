b7dc542c942ed40bfbf7202760e9068151c16c78
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-08-31 15:17:55 -0400

    Attempt: 2022-08-31 15.17.55 - Runtime Error - runtime NA - memory NA.cpp

---
+++ 57-insert-interval/2022-08-31 15.17.55 - Runtime Error - runtime NA - memory NA.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int size = intervals.size();
+        
+        int i = 0;
+        
+        while (i < size) {
+            if (newInterval[0] < intervals[i][0]) {
+                intervals.insert(intervals.begin() + i, newInterval);
+                break;
+            }
+            ++i;
+        }
+        
+        ++size;
+        
+        vector<vector<int>> result;
+        result.push_back(intervals[0]);
+        
+        for (int i = 1; i < size; ++i) {
+            auto curr = result.back();
+            if (intervals[i][0] <= curr[1]) {
+                result.pop_back();
+                result.push_back({ curr[0], max(curr[1], intervals[i][1]) });
+            } else result.push_back(intervals[i]);
+        }
+        
+        return result;
+    }
+};
