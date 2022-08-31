26929aebc2f50dabe91c8b91b7bc5c5cb7ca14f9
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-08-31 15:22:07 -0400

    Attempt: 2022-08-31 15.22.07 - Accepted - runtime 43ms - memory 18.1MB.cpp

---
+++ 57-insert-interval/2022-08-31 15.22.07 - Accepted - runtime 43ms - memory 18.1MB.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int size = intervals.size();
+        vector<vector<int>> result;
+        
+        int i = 0;
+        
+        while (i < size) {
+            if (newInterval[0] < intervals[i][0]) {
+                intervals.insert(intervals.begin() + i, newInterval);
+                break;
+            }
+            ++i;
+        }
+        
+        if (size == intervals.size()) intervals.push_back(newInterval);
+        
+        ++size;
+        
+        result.push_back(intervals[0]);
+        
+        for (int i = 1; i < size; ++i) {
+            auto curr = result.back();
+            if (intervals[i][0] <= curr[1]) {
+                result.pop_back();
+                result.push_back({ curr[0], max(curr[1], intervals[i][1]) });
+            } else result.push_back(intervals[i]);
+        }
+        
+        return result;
+    }
+};
