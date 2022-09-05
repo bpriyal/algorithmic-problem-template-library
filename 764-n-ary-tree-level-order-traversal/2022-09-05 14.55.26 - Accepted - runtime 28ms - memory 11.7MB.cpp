548a9f32a94c8043afbad206351622a2659b98d9
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-09-05 14:55:26 -0400

    Attempt: 2022-09-05 14.55.26 - Accepted - runtime 28ms - memory 11.7MB.cpp

---
+++ 764-n-ary-tree-level-order-traversal/2022-09-05 14.55.26 - Accepted - runtime 28ms - memory 11.7MB.cpp
@@ -0,0 +1,47 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(Node* root) {
+        vector<vector<int>> result;
+        
+        if (!root) return result;
+        
+        queue<Node*> q;
+        q.push(root);
+        
+        while(!q.empty()) {
+            int size = q.size();
+            vector<int> run;
+            for (int i = 0; i < size; ++i) {
+                run.push_back(q.front()->val);
+                
+                for (auto ele : q.front()->children) q.push(ele);
+                
+                q.pop();
+            }
+            
+            result.emplace_back(run);
+        }
+        
+        return result;
+    }
+};
