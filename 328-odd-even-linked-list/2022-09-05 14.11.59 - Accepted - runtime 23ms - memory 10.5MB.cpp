de9def4d254ef16906b009ac5c4f03c732b600a8
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-09-05 14:11:59 -0400

    Attempt: 2022-09-05 14.11.59 - Accepted - runtime 23ms - memory 10.5MB.cpp

---
+++ 328-odd-even-linked-list/2022-09-05 14.11.59 - Accepted - runtime 23ms - memory 10.5MB.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (!head || !head->next) return head;
+        
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        ListNode* oldEven = head->next;
+        
+        while (even && odd) {
+            
+            if (even->next) {
+                odd->next = even->next;
+                odd = odd->next;
+            } else break;
+            
+            if (odd->next) {
+                even->next = odd->next;
+                even = even->next;
+            } else break;
+        }
+        
+        even->next = nullptr;
+        odd->next = oldEven;
+        
+        return head;
+    }
+};
