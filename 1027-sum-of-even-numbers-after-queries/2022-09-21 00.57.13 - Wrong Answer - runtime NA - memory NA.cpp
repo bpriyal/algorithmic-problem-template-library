0d084d406c3a8562c9f59f16e0b647c3da3e76c2
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-09-21 00:57:13 -0400

    Attempt: 2022-09-21 00.57.13 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 1027-sum-of-even-numbers-after-queries/2022-09-21 00.57.13 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    int sumOfEven(const vector<int>& nums) {
+        int sum = 0;
+        
+        for (int num : nums) {
+            cout<<num<<"\t";
+            // sum += num % 2? 0 : num;
+        }
+        // cout<<endl;
+        return sum;
+    }
+public:
+    vector<int> sumEvenAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
+        int size = nums.size();
+        vector<int> result;
+        
+        for (auto query : queries) {
+            nums[query[1]] += query[0];
+            
+            result.push_back(sumOfEven(nums));
+        }
+        
+        return result;
+    }
+};
