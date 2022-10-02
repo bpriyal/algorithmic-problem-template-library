a9b89dd81923a67f5346b76fc49672f875653789
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-10-02 07:10:07 -0400

    Attempt: 2022-10-02 07.10.07 - Accepted - runtime 362ms - memory 114.5MB.cpp

---
+++ 1072-next-greater-node-in-linked-list/2022-10-02 07.10.07 - Accepted - runtime 362ms - memory 114.5MB.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> nextLargerNodes(ListNode* head) {
+        stack<int> st;
+        unordered_map<int, queue<int>> map;
+        ListNode* temp = head;
+        vector<int> result;
+        
+        while (temp) {
+            while (!st.empty() && st.top() < temp->val) {
+                map[st.top()].push(temp->val);
+                st.pop();
+            }
+            
+            st.push(temp->val);
+            temp = temp->next;
+        }
+        
+        temp = head;
+        
+        while (temp) {
+            if (map.find(temp->val) != map.end() && !map[temp->val].empty()) {
+                result.push_back(map[temp->val].front());
+                map[temp->val].pop();
+            } else result.push_back(0);
+            temp = temp->next;
+        }
+        
+        return result;
+    }
+};
