7e97d821df5667bf3674fade600db3965a7e61c0
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-10-02 04:17:02 -0400

    Attempt: 2022-10-02 04.17.02 - Accepted - runtime 21ms - memory 10.9MB.cpp

---
+++ 71-simplify-path/2022-10-02 04.17.02 - Accepted - runtime 21ms - memory 10.9MB.cpp
@@ -0,0 +1,54 @@
+class Solution {
+    int size;
+//     string getFileOrDirName(string& path, int& i) {
+//         if (i >= size) return "";
+//         string result = "";
+        
+//         while (i < size && path[i] != '/') {
+//             result += path[t];
+//             ++i;
+//         }
+        
+//         return result;
+//     }
+    // string findSlashes()
+public:
+    string simplifyPath(string path) {
+        path += "/";
+        size = path.size();
+        stack<string> st;
+        int i = 0;
+        int first = -1, second = -1;
+        
+        while (i < size) {
+            if (path[i] == '/') {
+                if (first == -1) first = i;
+                else second = i;
+            }
+            if (first != -1 && second != -1) {
+                string temp = path.substr(first + 1, second - first - 1);
+                
+                if (temp == "..") {
+                    if (!st.empty()) {
+                        st.pop();
+                    }
+                }
+                else if (!temp.empty() && temp != ".") {
+                    st.push(temp);
+                }
+                
+                first = second;
+                second = -1;
+            }
+            ++i;
+        }
+        
+        string result = "";
+        
+        while (!st.empty()) {
+            result = "/" + st.top() + result;
+            st.pop();
+        }
+        return result.empty()? "/" : result;
+    }
+};
