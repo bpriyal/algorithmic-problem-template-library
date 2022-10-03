b98dfe3e7fa8645818111c0735d3e413ab306cc7
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-10-03 16:17:16 -0400

    Attempt: 2022-10-03 16.17.16 - Accepted - runtime 170ms - memory 96.8MB.cpp

---
+++ 1700-minimum-time-to-make-rope-colorful/2022-10-03 16.17.16 - Accepted - runtime 170ms - memory 96.8MB.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minCost(string colors, vector<int>& neededTime) {
+        colors += ".";
+        neededTime.push_back(0);
+        int size = colors.size();
+        int result = 0;
+        
+        int _sum = 0;
+        int _max = 0;
+        for (int i = 0; i < size; ++i) {
+            _sum += neededTime[i];
+            _max = max(_max, neededTime[i]);
+            
+            if (colors[i] == colors[i+1]) {
+            } else {
+                if (_sum != neededTime[i]) {
+                    result += _sum - _max;
+                }
+                _sum = 0;
+                _max = 0;
+            }
+        }
+                
+        return result;
+    }
+};
