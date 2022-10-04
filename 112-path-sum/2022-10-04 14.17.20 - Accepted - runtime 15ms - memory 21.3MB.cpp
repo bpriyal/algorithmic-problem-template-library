36a2f1e0885a044fb698373f3ce743fe652b80dc
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-10-04 14:17:20 -0400

    Attempt: 2022-10-04 14.17.20 - Accepted - runtime 15ms - memory 21.3MB.cpp

---
+++ 112-path-sum/2022-10-04 14.17.20 - Accepted - runtime 15ms - memory 21.3MB.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    bool util(TreeNode* root, int target, int sum) {
+        if (!root) return false;
+        
+        sum += root->val;
+        if (!root->left && !root->right) return sum == target;
+        bool temp = util(root->left, target, sum) || util(root->right, target, sum);
+        sum -= root->val;
+        
+        return temp;
+    }
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (!root) return false;
+        return util(root, targetSum, 0);
+    }
+};
