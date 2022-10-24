b8d729cd70ba0520566714d26b063d575a759983
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-10-24 01:41:08 -0400

    Attempt: 2022-10-24 01.41.08 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/2022-10-24 01.41.08 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,56 @@
+class Solution {
+    int result;
+    int size;
+    
+    int getCacheSize(const vector<int>& cache) {
+        int result = 0;
+        
+        for (int frequency : cache) result += frequency;
+        
+        return result;
+    }
+    
+    pair<bool, vector<int>> uniqueStrings(const string& s, vector<int>& cache) {
+        vector<int> temp = cache;
+        
+        for (char c : s) {
+            ++temp[c - 'a'];
+        }
+        
+        for (int frequency : temp) {
+            if (frequency > 1) return { false, cache };
+        }
+        
+        return { true, temp };
+    }
+    
+    vector<int> getNewCache(const string& s) {
+        vector<int> cache(26);
+        for (char c : s) ++cache[c - 'a'];
+        
+        return cache;
+    }
+    
+    void util(const vector<string>& arr, int i, vector<int>& cache) {
+        if (i >= size) {
+            result = max(result, getCacheSize(cache));
+            return;
+        }
+        vector<int> newCache = getNewCache(arr[i]);
+        util(arr, i + 1, newCache); // start afresh
+        
+        if (auto [isUnique, freqVector] = uniqueStrings(arr[i], cache); isUnique) {
+            util(arr, i + 1, freqVector);
+        }
+    }
+public:
+    int maxLength(vector<string>& arr) {
+        size = arr.size();
+        result = 0;
+        
+        vector<int> newCache(26);
+        util(arr, 0, newCache);
+        
+        return result;
+    }
+};
