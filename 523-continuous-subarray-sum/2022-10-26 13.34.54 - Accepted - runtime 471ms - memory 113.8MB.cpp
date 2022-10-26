89ad66828ed5c4e5f436b147c44fe26c36c5ea17
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-10-26 13:34:54 -0400

    Attempt: 2022-10-26 13.34.54 - Accepted - runtime 471ms - memory 113.8MB.cpp

---
+++ 523-continuous-subarray-sum/2022-10-26 13.34.54 - Accepted - runtime 471ms - memory 113.8MB.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        int size = nums.size();
+        
+        unordered_map<int, int> cache;
+        cache[0] = -1;
+        int sum = 0;
+        
+        for (int i = 0; i < size; ++i) {
+            sum = (sum + nums[i]) % k;
+            
+            if (cache.find(sum) != cache.end()) {
+                if (i - cache[sum] > 1) return true;
+            } else {
+                cache[sum] = i;
+            }
+        }
+        
+        return false;
+    }
+};
