90c2ece0bd4b64d0f885baac73913205bb29f26c
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-11-27 03:21:28 -0500

    Attempt: 2022-11-27 03.21.28 - Time Limit Exceeded - runtime NA - memory NA.cpp

---
+++ 789-kth-largest-element-in-a-stream/2022-11-27 03.21.28 - Time Limit Exceeded - runtime NA - memory NA.cpp
@@ -0,0 +1,31 @@
+class KthLargest {
+    priority_queue<int> cache;
+    int n;
+public:
+    KthLargest(int k, vector<int>& nums) {
+        for (int num : nums) cache.emplace(num);
+        n = k;
+    }
+    
+    int add(int val) {
+        cache.emplace(val);
+
+        vector<int> temp(n);
+        cout<<"********"<<endl;
+        for (int i = 0; i < n; ++i) {
+            cout<<cache.top()<<endl;
+            temp[i] = cache.top();
+            cache.pop();
+        }
+        // cout<<temp[n-1]<<endl;
+        for (int num : temp) cache.emplace(num);
+
+        return temp[n - 1];
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
