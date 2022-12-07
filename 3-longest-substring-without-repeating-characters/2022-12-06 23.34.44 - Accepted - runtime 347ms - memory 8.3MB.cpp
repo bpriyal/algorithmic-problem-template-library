34dc898b9d857414a962109ec075459a3fa95dd3
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-12-06 23:34:44 -0500

    Attempt: 2022-12-06 23.34.44 - Accepted - runtime 347ms - memory 8.3MB.cpp

---
+++ 3-longest-substring-without-repeating-characters/2022-12-06 23.34.44 - Accepted - runtime 347ms - memory 8.3MB.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int size = s.size();
+        int result = 0;
+        int left = 0;
+        int right = 0;
+
+        unordered_map<char, int> cache;
+
+        for (right = 0; right < size; ++right) {
+            char c = s[right];
+
+            if (cache.find(c) != cache.end()) {
+                left = max(left, cache[c]);
+            }
+
+            cache[c] = right + 1;
+            result = max(result, right - left + 1);
+            cout<<"right: "<<right<<", result:"<<result<<endl;
+        }
+
+        return result;
+
+        // int n = int(s.length()), res = 0;
+        // unordered_map<char, int> mp;
+
+        // for (int j = 0, i = 0; j < n; j++){
+        //     if(mp[s[j]] > 0) {
+        //         i = max(mp[s[j]], i);
+        //     }
+        //     res = max(res, j - i + 1);
+        //     mp[s[j]] = j + 1;
+        // }
+        // return res;
+    }
+};
