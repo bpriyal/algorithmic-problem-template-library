bf97e1a698b4d1f804abc8161b9c6113de0dd332
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-12-06 23:27:02 -0500

    Attempt: 2022-12-06 23.27.02 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 3-longest-substring-without-repeating-characters/2022-12-06 23.27.02 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int size = s.size();
+        int result = 0;
+        int left = 0;
+        int right = 0;
+
+        unordered_map<char, int> cache;
+
+        for (right = 0; right < size; ++right) {
+            char c = s[right];
+
+            if (cache.find(c) != cache.end()) {
+                left = max(right, cache[c]);
+            }
+
+            cache.emplace(c, right);
+            result = max(result, right - left + 1);
+            cout<<"right: "<<right<<", result:"<<result<<endl;
+        }
+
+        return result;
+    }
+};
