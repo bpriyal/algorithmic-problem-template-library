2f5bdd25224a6e3b4fc55e435097108ea4e8da35
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-12-12 00:20:22 -0500

    Attempt: 2022-12-12 00.20.22 - Accepted - runtime 46ms - memory 12.3MB.cpp

---
+++ 200-number-of-islands/2022-12-12 00.20.22 - Accepted - runtime 46ms - memory 12.3MB.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    int rows, cols;
+
+    void util(vector<vector<char>>& grid, int row, int col) {
+        if (row < 0 || row >= rows || col < 0 || col >= cols || grid[row][col] == '0') return;
+
+        grid[row][col] = '0';
+
+        util(grid, row - 1, col);
+        util(grid, row + 1, col);
+        util(grid, row, col - 1);
+        util(grid, row, col + 1);
+    }
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        rows = grid.size();
+        cols = grid[0].size();
+        int islands = 0;
+
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (grid[i][j] == '1') {
+                    ++islands;
+                    util(grid, i , j);
+                }
+            }
+        }
+
+        return islands;
+    }
+};
