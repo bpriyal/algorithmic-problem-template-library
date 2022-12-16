7daea5fb9a07d2dff4ceca452af13028f0a3cb69
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-12-16 14:38:42 -0500

    Attempt: 2022-12-16 14.38.42 - Accepted - runtime 19ms - memory 13.2MB.cpp

---
+++ 73-set-matrix-zeroes/2022-12-16 14.38.42 - Accepted - runtime 19ms - memory 13.2MB.cpp
@@ -0,0 +1,39 @@
+class Solution {
+    int rows, cols;
+    int flag = INT_MAX - 5;
+    void util(vector<vector<int>>& matrix, int i, int j) {
+        // set cols as -1
+        for (int k = 0; k < rows; ++k) {
+            if (matrix[k][j] != 0) {
+                matrix[k][j] = flag;
+            }
+        }
+
+        for (int k = 0; k < cols; ++k) {
+            if (matrix[i][k] != 0) {
+                matrix[i][k] = flag;
+            }
+        }
+    }
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        rows = matrix.size();
+        cols = matrix[0].size();
+
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (matrix[i][j] == 0) {
+                    util(matrix, i, j);
+                }
+            }
+        }
+
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (matrix[i][j] == flag) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
