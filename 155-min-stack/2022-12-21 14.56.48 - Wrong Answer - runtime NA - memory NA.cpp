f213675d1fc2bf7f1a94e975115f9cd79fab6ecf
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2022-12-21 14:56:48 -0500

    Attempt: 2022-12-21 14.56.48 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 155-min-stack/2022-12-21 14.56.48 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,33 @@
+class MinStack {
+    stack<pair<int, int>> cache;
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if (cache.empty()) cache.emplace(val, val);
+        cache.emplace(val, min(cache.top().second, val));
+    }
+    
+    void pop() {
+        cache.pop();
+    }
+    
+    int top() {
+        return cache.top().first;
+    }
+    
+    int getMin() {
+        return cache.top().second;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
