c6e3801d1209a2f05f61f3dfd940072cd950b577
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2023-01-02 01:20:44 -0500

    Attempt: 2023-01-02 01.20.44 - Accepted - runtime 388ms - memory 27.5MB.cpp

---
+++ 1017-odd-even-jump/2023-01-02 01.20.44 - Accepted - runtime 388ms - memory 27.5MB.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int oddEvenJumps(vector<int>& arr) {
+        int size = arr.size();
+        vector<int> higher(size); vector<int> lower(size);
+        int result = 1;
+        higher[size - 1] = lower[size - 1] = 1;
+        map<int, int> cache;
+        cache[arr[size - 1]] = size - 1;
+
+        for (int i = size - 2; i >= 0; --i) {
+            auto hi = cache.lower_bound(arr[i]);
+            auto lo = cache.upper_bound(arr[i]);
+            cout<<arr[i]<<", "<<lo->first<<", "<<lo->second<<endl;
+
+            if (hi != cache.end()) higher[i] = lower[hi->second];
+            if (lo != cache.begin()) lower[i] = higher[(--lo)->second];
+            if (higher[i]) ++result;
+            cache[arr[i]] = i;
+        }
+
+        return result;
+    }
+};
