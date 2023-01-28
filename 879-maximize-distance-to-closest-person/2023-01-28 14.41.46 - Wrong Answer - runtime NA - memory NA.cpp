8bdd1073b1fc3f5f4ce6d67a8210d7a44b5f94d6
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2023-01-28 14:41:46 -0500

    Attempt: 2023-01-28 14.41.46 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 879-maximize-distance-to-closest-person/2023-01-28 14.41.46 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxDistToClosest(vector<int>& seats) {
+        int size = seats.size();
+        int p1 = 0, p2 = 0;
+        int result = 0;
+        bool firstEleZero = !seats[0];
+        int i = 0;
+
+        while (i < size) {
+            if (seats[i] == 1) {
+                if (!firstEleZero) p2 = i;
+                result = max(result, firstEleZero? p2 - p1 : (p2 - p1)/2);
+                p1 = i;
+                firstEleZero = false;
+            } 
+            ++i;
+        }
+
+        return p2? result : max({ result, abs(size - 1 - p1), p1 });
+    }
+};
