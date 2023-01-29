dc512895a0e6606f5beb1c793101e7546c3ea7e1
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2023-01-29 01:39:17 -0500

    Attempt: 2023-01-29 01.39.17 - Accepted - runtime 16ms - memory 11.8MB.cpp

---
+++ 253-meeting-rooms-ii/2023-01-29 01.39.17 - Accepted - runtime 16ms - memory 11.8MB.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[0] < b[0];
+        });
+
+        priority_queue<int, vector<int>, greater<int>> q;
+        int maxConferenceRooms = 0;
+
+        for (int i = 0; i < intervals.size(); ++i) {
+            if (!q.empty() && q.top() <= intervals[i][0]) {
+                q.pop();
+            } else {
+                ++maxConferenceRooms;
+            }
+            q.push(intervals[i][1]);
+        }
+
+        return maxConferenceRooms;
+    }
+};
