2f2e17ee5e60d1beebdd26d0afa9c32f274d391f
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2023-04-01 04:36:21 -0400

    Attempt: 2023-04-01 04.36.21 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 158-read-n-characters-given-read4-ii-call-multiple-times/2023-04-01 04.36.21 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,45 @@
+/**
+ * The read4 API is defined in the parent class Reader4.
+ *     int read4(char *buf4);
+ */
+
+class Solution {
+    char buf4[4];
+    int numberUnusedChars = 0;
+    int charsRead = 4;
+public:
+    /**
+     * @param buf Destination buffer
+     * @param n   Number of characters to read
+     * @return    The number of actual characters read
+     */
+    int read(char *buf, int n) {
+        cout<<"entering"<<endl;
+        int temp = n;
+        int number = 0;
+        while (temp) {
+            int charactersToCopy = INT_MAX;
+            if (numberUnusedChars) {
+                cout<<"if"<<numberUnusedChars<<endl;
+                charactersToCopy = min(temp, numberUnusedChars);
+                for (int i = 0; i < charactersToCopy; ++i) {
+                    *(buf + number + i) = *(buf4 + (charsRead - numberUnusedChars) + i);
+                }
+                numberUnusedChars -= charactersToCopy;
+                number += charactersToCopy;
+                temp -= charactersToCopy;
+            } else {
+                cout<<"else"<<endl;
+                buf4[0] = buf4[1] = buf4[2] = buf4[3] = '\0';
+                if (charsRead < 4) return number;
+                numberUnusedChars = charsRead = read4(buf4);
+                if (!numberUnusedChars) return 0;
+                // charactersToCopy = min(temp, charactersRead);
+                // for (int i = 0; i < charactersToCopy; ++i) {
+                //     *(buf + number + i) = *(buf4 + i);
+                // }
+            }
+        }
+        return number;
+    }
+};
