5314f3a6a4dbc1cc5b533c43f40a7ccba2f207a4
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2023-04-23 09:16:33 -0400

    Attempt: 2023-04-23 09.16.33 - Time Limit Exceeded - runtime NA - memory NA.cpp

---
+++ 49-group-anagrams/2023-04-23 09.16.33 - Time Limit Exceeded - runtime NA - memory NA.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    vector<int> getCount(const string& str) {
+        vector<int> counts(26, 0);
+        for (char c : str) ++counts[c - 'a'];
+        return counts;
+    }
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        int size = strs.size();
+        vector<bool> visited(size, false);
+        vector<vector<string>> anagrams;
+
+        for (int i = 0; i < size; ++i) {
+            if (visited[i]) continue;
+            vector<string> anagram;
+            anagram.emplace_back(strs[i]);
+            visited[i] = true;
+            vector<int> primaryStrCount = getCount(strs[i]);
+            for (int j = i + 1; j < size; ++j) {
+                cout<<i<<", "<<j<<endl;
+                if (visited[j]) continue;
+                if (getCount(strs[j]) == primaryStrCount) {
+                    anagram.emplace_back(strs[j]);
+                    visited[j] = true;
+                }
+            }
+            anagrams.emplace_back(anagram);
+        }
+
+        return anagrams;
+    }
+};
