17a4cab05d027de4fd2b38b0b037d639d32eb9c7
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2023-04-23 09:03:11 -0400

    Attempt: 2023-04-23 09.03.11 - Accepted - runtime 0ms - memory 7MB.cpp

---
+++ 54-spiral-matrix/2023-04-23 09.03.11 - Accepted - runtime 0ms - memory 7MB.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        
+        int fr = 0;
+        int lc = col - 1;
+        int lr = row - 1;
+        int fc = 0;
+        
+        vector<int> result;
+        
+        while (true) {
+            for(int i = fc; i <= lc; ++i) {
+                result.push_back(matrix[fr][i]);
+            }
+            
+            if (fc > lc) break;
+            ++fr;
+            
+            for(int i = fr; i <= lr; ++i) {
+                result.push_back(matrix[i][lc]);
+            }
+            if (fr > lr) break;
+            --lc;
+            
+            for(int i = lc; i >= fc; --i) {
+                result.push_back(matrix[lr][i]);
+            }
+            if (lc < fc) break;
+            --lr;
+            
+            for(int i = lr; i >= fr; --i) {
+                result.push_back(matrix[i][fc]);
+            }
+            if (lr < fr) break;
+            ++fc;
+        }
+        
+        return result;
+    }
+};
