eaad2d64cb23fe4afc0a45a7df03b8fa91cf6d51
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2023-04-23 09:34:47 -0400

    Attempt: 2023-04-23 09.34.47 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 49-group-anagrams/2023-04-23 09.34.47 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    int getHash(const string& str) {
+        int hash = 0;
+        for (char c : str) hash += (c - 'a' + 1);
+        return hash;
+    }
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<int, vector<string>> cache;
+        for (const auto& str : strs) {
+            cache[getHash(str)].emplace_back(str);
+        }
+        vector<vector<string>> anagrams;
+        for (const auto& [hash, anagram] : cache) anagrams.emplace_back(anagram);
+        return anagrams;
+    }
+};
