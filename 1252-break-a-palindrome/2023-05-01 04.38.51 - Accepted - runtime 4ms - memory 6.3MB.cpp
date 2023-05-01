237a08bb4179e1b22bd8c41be477a3e0f29b4733
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2023-05-01 04:38:51 -0400

    Attempt: 2023-05-01 04.38.51 - Accepted - runtime 4ms - memory 6.3MB.cpp

---
+++ 1252-break-a-palindrome/2023-05-01 04.38.51 - Accepted - runtime 4ms - memory 6.3MB.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string breakPalindrome(string palindrome) {
+        int size = palindrome.size();
+        if (size == 1) return "";
+        int i;
+
+        for (i = 0; i < size; ++i) {
+            if (palindrome[i] != 'a') {
+                if (i == size / 2) {
+                    palindrome[size - 1] = 'b';
+                } else palindrome[i] = 'a';
+                break;
+            }
+        }
+        if (i == size) {
+            palindrome[size - 1] = 'b';
+        }
+        return palindrome;
+    }
+};
