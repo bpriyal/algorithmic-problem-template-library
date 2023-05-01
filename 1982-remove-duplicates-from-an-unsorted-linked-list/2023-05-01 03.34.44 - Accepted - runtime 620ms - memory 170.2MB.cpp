9891b55d0571764ac68d5948deae24288b5d1fef
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2023-05-01 03:34:44 -0400

    Attempt: 2023-05-01 03.34.44 - Accepted - runtime 620ms - memory 170.2MB.cpp

---
+++ 1982-remove-duplicates-from-an-unsorted-linked-list/2023-05-01 03.34.44 - Accepted - runtime 620ms - memory 170.2MB.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicatesUnsorted(ListNode* head) {
+        unordered_map<int, int> cache;
+        ListNode* tempIterator = head;
+
+        while (tempIterator) {
+            ++cache[tempIterator->val];
+            tempIterator = tempIterator->next;
+        }
+
+        // fix head first
+        while (head && cache[head->val] > 1) {
+            head = head->next;
+        }
+        if (!head) return nullptr;
+
+        tempIterator = head->next;
+        ListNode* prev = head;
+        while (tempIterator) {
+            if (cache[tempIterator->val] > 1) {
+                prev->next = tempIterator->next;
+                ListNode* tempToDelete = tempIterator;
+                tempIterator = tempIterator->next;
+                delete tempToDelete;
+                tempToDelete = nullptr;
+            } else {
+                prev = tempIterator;
+                tempIterator = tempIterator->next;
+            }
+
+        }
+        return head;
+    }
+};
