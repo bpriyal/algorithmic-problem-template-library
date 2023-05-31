cab8017396622244e19090b6ab743482e6d60480
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2023-05-31 02:13:19 -0400

    Attempt: 2023-05-31 02.13.19 - Wrong Answer - runtime NA - memory NA.java

---
+++ 605-can-place-flowers/2023-05-31 02.13.19 - Wrong Answer - runtime NA - memory NA.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int canBePlanted = 0;
+        int len = flowerbed.length;
+
+        for(int i = 0; i < len - 1; ++i){
+            if(/*flowerbed[i] == 1 &&*/ flowerbed[i+1] == 0){
+                int contiguousZeroes = 0;
+                for(int j = i; flowerbed[j] == 0; ++j, ++i){
+                    if(flowerbed[j] == 0){
+                        ++contiguousZeroes;
+                    }                    
+                }
+                canBePlanted += (contiguousZeroes - 1)/2;
+                if(canBePlanted >= n){
+                    return true;
+                } 
+            }    
+        }
+
+        if(canBePlanted >= n){
+            return true;
+        }
+        return false;
+    }
+}
+
+/*
+1 - 3. 2
+2 - 5. 3
+3 - 7. 4
+2n+1 no of zeroes required
+*/
