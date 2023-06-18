e4d018fca0acbf268168fc7595919db16df9dfba
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2023-06-18 15:47:01 -0400

    Attempt: 2023-06-18 15.47.01 - Accepted - runtime 44ms - memory 44.8MB.java

---
+++ 2470-removing-stars-from-a-string/2023-06-18 15.47.01 - Accepted - runtime 44ms - memory 44.8MB.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String removeStars(String s) {
+        StringBuilder newS = new StringBuilder();
+        for (int i = 0; i < s.length(); ++i){
+            char c = s.charAt(i);
+            if (c != '*'){
+                // System.out.println(c);
+                newS.append(c);
+            }
+            else { //when * encountered
+                // if(i > 0 && s.charAt(i-1) != '*'){
+                    // System.out.println(i);
+                    // System.out.println(newS.charAt(i));
+                    newS.deleteCharAt(newS.length() - 1);
+                // }
+            }
+        }
+        return newS.toString();
+    }
+}
