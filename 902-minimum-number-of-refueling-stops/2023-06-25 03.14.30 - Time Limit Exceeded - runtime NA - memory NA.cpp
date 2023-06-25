c6d47a45b809fe519cf7d8cf85ba1bfedc1ccdb3
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2023-06-25 03:14:30 -0400

    Attempt: 2023-06-25 03.14.30 - Time Limit Exceeded - runtime NA - memory NA.cpp

---
+++ 902-minimum-number-of-refueling-stops/2023-06-25 03.14.30 - Time Limit Exceeded - runtime NA - memory NA.cpp
@@ -0,0 +1,89 @@
+/********************* First Version, no memoization, TLE *********************/
+// class Solution {
+//     int beyondReach = std::numeric_limits<int>::max();
+//     int numberOfStations;
+//     int util(int target, int currentFuel, const vector<vector<int>>& stations, int stationToReach, int currentPosition) {
+//         int maxReach = currentPosition + currentFuel;
+//         if (maxReach >= target) return 0;
+//         // Either stations are exhausted or you can't reach nearest station
+//         if (stationToReach >= numberOfStations || maxReach < stations[stationToReach][0]) return beyondReach;
+
+//         /* make a choice */
+//         int fuelToReachNearestStation = stations[stationToReach][0] - currentPosition;
+//         // Fuel
+//         int withFuel = util(target, currentFuel - fuelToReachNearestStation + stations[stationToReach][1], stations, stationToReach + 1, stations[stationToReach][0]);
+//         int stops = withFuel == beyondReach? beyondReach : withFuel + 1;
+//         // Don't Fuel
+//         stops = min(stops, util(target, currentFuel - fuelToReachNearestStation, stations, stationToReach + 1, stations[stationToReach][0]));
+        
+//         return stops;
+//     }
+// public:
+//     int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
+//         numberOfStations = stations.size();
+//         int stops = util(target, startFuel, stations, 0, 0);
+//         return stops == beyondReach? -1 : stops;
+//     }
+// };
+
+/********************* Memory limit exceeds because of target being = 1000000000 *********************/
+// class Solution {
+//     int beyondReach = std::numeric_limits<int>::max();
+//     int numberOfStations;
+//     vector<vector<int>> dp;
+//     int util(int target, int currentFuel, const vector<vector<int>>& stations, int stationToReach, int currentPosition) {
+//         int maxReach = currentPosition + currentFuel;
+//         if (maxReach >= target) return 0;
+//         // Either stations are exhausted or you can't reach nearest station
+//         if (stationToReach >= numberOfStations || maxReach < stations[stationToReach][0]) return beyondReach;
+
+//         if (dp[currentPosition][currentFuel] != -1) return dp[currentPosition][currentFuel];
+
+//         /* make a choice */
+//         int fuelToReachNearestStation = stations[stationToReach][0] - currentPosition;
+//         // Fuel
+//         int withFuel = util(target, currentFuel - fuelToReachNearestStation + stations[stationToReach][1], stations, stationToReach + 1, stations[stationToReach][0]);
+//         int stops = withFuel == beyondReach? beyondReach : withFuel + 1;
+//         // Don't Fuel
+//         stops = min(stops, util(target, currentFuel - fuelToReachNearestStation, stations, stationToReach + 1, stations[stationToReach][0]));
+        
+//         return dp[currentPosition][currentFuel] = stops;
+//     }
+// public:
+//     int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
+//         dp.resize(target + 1, vector<int>(target + 1, -1));
+//         numberOfStations = stations.size();
+//         int stops = util(target, startFuel, stations, 0, 0);
+//         return stops == beyondReach? -1 : stops;
+//     }
+// };
+
+class Solution {
+    int beyondReach = std::numeric_limits<int>::max();
+    int numberOfStations;
+    map<pair<int, int>, int> dp;
+    int util(int target, int currentFuel, const vector<vector<int>>& stations, int stationToReach, int currentPosition) {
+        int maxReach = currentPosition + currentFuel;
+        if (maxReach >= target) return 0;
+        // Either stations are exhausted or you can't reach nearest station
+        if (stationToReach >= numberOfStations || maxReach < stations[stationToReach][0]) return beyondReach;
+
+        if (dp.find({currentPosition, currentFuel}) != dp.end()) return dp[{currentPosition, currentFuel}];
+
+        /* make a choice */
+        int fuelToReachNearestStation = stations[stationToReach][0] - currentPosition;
+        // Fuel
+        int withFuel = util(target, currentFuel - fuelToReachNearestStation + stations[stationToReach][1], stations, stationToReach + 1, stations[stationToReach][0]);
+        int stops = withFuel == beyondReach? beyondReach : withFuel + 1;
+        // Don't Fuel
+        stops = min(stops, util(target, currentFuel - fuelToReachNearestStation, stations, stationToReach + 1, stations[stationToReach][0]));
+        
+        return dp[{currentPosition, currentFuel}] = stops;
+    }
+public:
+    int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
+        numberOfStations = stations.size();
+        int stops = util(target, startFuel, stations, 0, 0);
+        return stops == beyondReach? -1 : stops;
+    }
+};
