79f498d17184c04eea0b464990051e330613983a
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2023-08-12 06:50:40 -0400

    Attempt: 2023-08-12 06.50.40 - Accepted - runtime 257ms - memory 63.1MB.pd.py

---
+++ 595-big-countries/2023-08-12 06.50.40 - Accepted - runtime 257ms - memory 63.1MB.pd.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def big_countries(world: pd.DataFrame) -> pd.DataFrame:
+    # Filter out by condition
+    bigCountries = world[(world['area'] >= 3000000) | (world['population'] >= 25000000)]
+    # Filter required columns
+    return bigCountries[['name', 'population', 'area']]
