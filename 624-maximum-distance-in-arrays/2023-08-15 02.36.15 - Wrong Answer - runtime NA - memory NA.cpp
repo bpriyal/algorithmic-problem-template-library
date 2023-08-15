91329f2709842d3d75d8a76649b29b3590d7823a
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2023-08-15 02:36:15 -0400

    Attempt: 2023-08-15 02.36.15 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 624-maximum-distance-in-arrays/2023-08-15 02.36.15 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxDistance(vector<vector<int>>& arrays) {
+        int m = arrays.size();
+        sort(arrays.begin(), arrays.end(), [](const auto& array1, const auto& array2) {
+            return array1[0] < array2[0];
+        });
+        int maxEnd = std::numeric_limits<int>::min();
+        for (int i = 1; i < m; ++i) {
+            maxEnd = max(maxEnd, arrays[i].back());
+        }
+
+        return abs(maxEnd - (maxEnd < 0? arrays[0].back() : arrays[0].front()));
+    }
+};
