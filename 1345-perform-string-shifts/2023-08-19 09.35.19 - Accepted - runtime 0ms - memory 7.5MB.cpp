91b7b4e44eb46f5882af9ccf168271f415fe5b1c
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2023-08-19 09:35:19 -0400

    Attempt: 2023-08-19 09.35.19 - Accepted - runtime 0ms - memory 7.5MB.cpp

---
+++ 1345-perform-string-shifts/2023-08-19 09.35.19 - Accepted - runtime 0ms - memory 7.5MB.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string stringShift(string s, vector<vector<int>>& shift) {
+        int size = s.size();
+        int resultantShift = 0;
+        for (const auto& element : shift) resultantShift += element[0]? element[1] : -1 * element[1];
+
+        string prefix, suffix;
+        if (resultantShift == 0) return s;
+        else if (resultantShift > 0) {
+            int shift = size - (abs(resultantShift) % size);
+            prefix = s.substr(shift);
+            suffix = s.substr(0, shift);
+        } else {
+            int shift = abs(resultantShift) % size;
+            prefix = s.substr(shift);
+            suffix = s.substr(0, shift);
+        }
+        return prefix + suffix;
+    }
+};
