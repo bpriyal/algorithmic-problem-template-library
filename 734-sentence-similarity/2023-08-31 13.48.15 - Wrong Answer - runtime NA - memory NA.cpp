3e5b2ff1076ca38d30850c802233e09cebf8530a
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2023-08-31 13:48:15 -0400

    Attempt: 2023-08-31 13.48.15 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 734-sentence-similarity/2023-08-31 13.48.15 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool areSentencesSimilar(vector<string>& sentence1, vector<string>& sentence2, vector<vector<string>>& similarPairs) {
+        int size1 = sentence1.size();
+        int size2 = sentence2.size();
+        if (size1 != size2) return false;
+        unordered_map<string, string> cache;
+        for (const auto& pair : similarPairs) cache.emplace(pair[0], pair[1]);
+
+        for (int i = 0;i < size1; ++i) {
+            if (sentence1[i] == sentence2[i]) continue;
+            if (!(sentence1[i] == cache[sentence2[i]] || cache[sentence1[i]] == sentence2[i])) return false;
+        }
+        return true;
+    }
+};
