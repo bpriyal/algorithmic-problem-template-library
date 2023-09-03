ed34ed21a105271ee6ab5820caa30f6410226b6d
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2023-09-03 15:08:00 -0400

    Attempt: 2023-09-03 15.08.00 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 311-sparse-matrix-multiplication/2023-09-03 15.08.00 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> multiply(vector<vector<int>>& mat1, vector<vector<int>>& mat2) {
+        int row1 = mat1.size();
+        int col1 = mat1[0].size();
+        int row2 = mat2.size();
+        int col2 = mat2[0].size();
+        vector<vector<int>> result;
+
+        for (int i = 0; i < row1; ++i) {
+            vector<int> subResult(col1);
+            for (int k = 0; k < col2; ++k) {
+                for (int j = 0; j < col1; ++j) {
+                    subResult[k] += mat1[i][j] * mat2[j][k];
+                }
+            }
+            result.emplace_back(subResult);
+        }
+        return result;
+    }
+};
+
+// [[7,-7,0],[0,0,0],[0,3,0]]
+// [1, -5] [12]
+//         [-1]
+
+// [12 5]
