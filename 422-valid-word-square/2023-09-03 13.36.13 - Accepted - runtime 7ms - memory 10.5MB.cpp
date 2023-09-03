b0699b6fac36d0fd6ccb8a0a8c7eb30689f5619c
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2023-09-03 13:36:13 -0400

    Attempt: 2023-09-03 13.36.13 - Accepted - runtime 7ms - memory 10.5MB.cpp

---
+++ 422-valid-word-square/2023-09-03 13.36.13 - Accepted - runtime 7ms - memory 10.5MB.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool validWordSquare(vector<string>& words) {
+        int rows = words.size();
+        int cols = words[0].size();
+        if (rows != cols) return false;
+        int k = max(rows, cols);
+        // cout<<k<<endl;
+        for (int i = 0; i < k; ++i) {
+            // cout<<"i: "<<i<<endl;
+            string kColStr;
+            for (int j = 0; j < rows; ++j) {
+                if (i >= words[j].size()) break;
+                // cout<<words[j]<<endl;
+                kColStr += words[j][i];
+            }
+            if (kColStr != words[i]) {
+                // cout<<i<<", "<<kColStr<<", "<<endl;
+                return false;
+            }
+        }
+        return true;
+    }
+};
