820e6043c7e9b9a79a48c0ff2b873e719f253e3c
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2023-09-05 14:35:44 -0400

    Attempt: 2023-09-05 14.35.44 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 252-meeting-rooms/2023-09-05 14.35.44 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canAttendMeetings(vector<vector<int>>& intervals) {
+        int size = intervals.size();
+        if (size <= 1) return true;
+
+        sort(intervals.begin(), intervals.end(), [](const auto& interval1, const auto& interval2) {
+            return interval1[0] < interval2[0];
+        });
+        for (int i = 0; i < size - 1; ++i) {
+            if (intervals[i+1].front() <= intervals[i].back()) return false;
+        }
+        return true;
+    }
+};
