6cda5faf72e80b59d4b4b42eb015dfc3fd3af304
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2023-09-24 11:35:23 -0400

    Attempt: 2023-09-24 11.35.23 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 1200-remove-interval/2023-09-24 11.35.23 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> removeInterval(vector<vector<int>>& intervals, vector<int>& toBeRemoved) {
+        int intervalSize = intervals.size();
+        int toBeRemovedSize = toBeRemoved.size();
+        vector<vector<int>> result;
+        for (int i = 0; i < intervalSize; ++i) {
+             if (intervals[i][1] < toBeRemoved[0] || intervals[i][0] > toBeRemoved[1]) {
+                 cout<<"1"<<endl;
+                result.emplace_back(intervals[i]);
+             }
+             if (intervals[i][0] <= toBeRemoved[0] && intervals[i][1] >= toBeRemoved[1]) {
+                 cout<<"2"<<endl;
+                 if (intervals[i][0] != toBeRemoved[0]) result.emplace_back(vector<int>{intervals[i][0], toBeRemoved[0]});
+                 if (intervals[i][1] >= toBeRemoved[1]) result.emplace_back(vector<int>{toBeRemoved[1], intervals[i][1]});
+                 continue;
+             }
+             if (toBeRemoved[1] >= intervals[i][0] && toBeRemoved[1] < intervals[i][1]) {
+                 cout<<"3"<<endl;
+                 result.emplace_back(vector<int>{toBeRemoved[1], intervals[i][1]});
+             }
+             if (toBeRemoved[0] > intervals[i][0] && toBeRemoved[0] <= intervals[i][1]) {
+                 cout<<"4"<<endl;
+                 result.emplace_back(vector<int>{intervals[i][0], toBeRemoved[0]});
+             }
+        }
+        return result;
+    }
+};
