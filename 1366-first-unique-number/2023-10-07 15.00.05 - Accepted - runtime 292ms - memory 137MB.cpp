5cfe1340297f2c4af701daf3fff349d5ee1962b6
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2023-10-07 15:00:05 -0400

    Attempt: 2023-10-07 15.00.05 - Accepted - runtime 292ms - memory 137MB.cpp

---
+++ 1366-first-unique-number/2023-10-07 15.00.05 - Accepted - runtime 292ms - memory 137MB.cpp
@@ -0,0 +1,31 @@
+class FirstUnique {
+    list<int> queue;
+    unordered_map<int, list<int>::iterator> hashmap;
+public:
+    FirstUnique(vector<int>& nums) {
+        for (const auto& num : nums) add(num);
+    }
+    
+    int showFirstUnique() {
+        return queue.empty() ? -1 : queue.front();
+    }
+    
+    void add(int value) {
+        if (hashmap.find(value) == hashmap.end()) {
+            queue.emplace_back(value);
+            hashmap.try_emplace(value, --queue.end());
+        } else {
+            if (hashmap[value] != queue.end()) {
+                queue.erase(hashmap[value]);
+                hashmap[value] = queue.end();
+            }
+        }
+    }
+};
+
+/**
+ * Your FirstUnique object will be instantiated and called as such:
+ * FirstUnique* obj = new FirstUnique(nums);
+ * int param_1 = obj->showFirstUnique();
+ * obj->add(value);
+ */
