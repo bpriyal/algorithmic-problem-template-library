1f9c6f6a53362208f51870e43c151ca447e794e7
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2023-12-16 11:01:55 -0500

    Attempt: 2023-12-16 11.01.55 - Time Limit Exceeded - runtime NA - memory NA.cpp

---
+++ 850-insert-into-a-sorted-circular-linked-list/2023-12-16 11.01.55 - Time Limit Exceeded - runtime NA - memory NA.cpp
@@ -0,0 +1,59 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+    }
+
+    Node(int _val, Node* _next) {
+        val = _val;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* insert(Node* head, int insertVal) {
+        if (!head) {
+            Node* newNode = new Node(insertVal);
+            newNode->next = newNode;
+            return newNode;
+        }
+        Node* temp = head;
+        if (head == head->next) { // single node circular list
+            Node* newNode = new Node(insertVal, temp->next);
+            temp->next = newNode;
+            return head;
+        }
+        while (true) {
+            cout<<temp->val<<endl;
+            if (insertVal == temp->val) {
+                Node* newNode = new Node(insertVal, temp->next);
+                temp->next = newNode;
+                break;
+            }
+            if (temp->val > temp->next->val) { // already at node with highest value
+                if (insertVal >= temp->val || insertVal <= temp->next->val) {
+                    Node* newNode = new Node(insertVal, temp->next);
+                    temp->next = newNode;
+                    break;
+                }
+            }
+            if (insertVal > temp->val && insertVal < temp->next->val) {
+                Node* newNode = new Node(insertVal, temp->next);
+                temp->next = newNode;
+                break;
+            }
+            temp = temp->next;
+        }
+        return head;
+    }
+};
