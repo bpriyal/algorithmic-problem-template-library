c90916421190cc714b2bb2140563273bb21fcaf8
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2023-12-17 12:47:28 -0500

    Attempt: 2023-12-17 12.47.28 - Accepted - runtime 35ms - memory 32.7MB.cpp

---
+++ 298-binary-tree-longest-consecutive-sequence/2023-12-17 12.47.28 - Accepted - runtime 35ms - memory 32.7MB.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int _result = 0;
+    void _longestConsecutive(TreeNode* root, int lastVal, int sequenceLen) {
+        if (!root) return;
+        // cout<<"val: "<<root->val<<", lastVal: "<<lastVal<<", sequenceLen: "<<sequenceLen<<endl;
+        if (sequenceLen == 0 || lastVal + 1 == root->val) {
+            ++sequenceLen;
+        } else {
+            sequenceLen = 1;
+        }
+        _result = max(_result, sequenceLen);
+        _longestConsecutive(root->left, root->val, sequenceLen);
+        _longestConsecutive(root->right, root->val, sequenceLen);
+    }
+public:
+    int longestConsecutive(TreeNode* root) {
+        _longestConsecutive(root, -1, 0);
+        return _result;
+    }
+};
