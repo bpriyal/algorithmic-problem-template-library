a77aae0c7d61960dcb273921f2228a44a14b10c7
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2023-12-27 01:07:11 -0500

    Attempt: 2023-12-27 01.07.11 - Accepted - runtime 0ms - memory 22MB.cpp

---
+++ 272-closest-binary-search-tree-value-ii/2023-12-27 01.07.11 - Accepted - runtime 0ms - memory 22MB.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+struct CustomComparator {
+    // int _target;
+    // CustomComparator(int target_) : _target(target_) {}
+    bool operator()(pair<int, double> a, pair<int, double> b) const {
+        return a.second < b.second;
+    }
+};
+using PQ = priority_queue<pair<int, double>, vector<pair<int, double>>, CustomComparator>;
+class Solution {
+    void _closestKValues(TreeNode* root, double target, int k, PQ& pq) {
+        if (!root) return;
+        double diff = abs(root->val - target);
+        if (pq.size() >= k) {
+            // cout<<"root->val: "<<root->val<<", pq.top().first: "<<pq.top().first<<", pq.top().second: "<<pq.top().second<<endl;
+            if (diff < pq.top().second) {
+                pq.pop();
+                pq.emplace(root->val, diff);
+            }
+        } else {
+            pq.emplace(root->val, diff);
+        }
+        _closestKValues(root->left, target, k, pq);
+        _closestKValues(root->right, target, k, pq);
+    }
+public:
+    vector<int> closestKValues(TreeNode* root, double target, int k) {
+        vector<int> result;
+        // CustomComparator customComparator(target);
+        priority_queue<pair<int, double>, vector<pair<int, double>>, CustomComparator> pq;
+        _closestKValues(root, target, k, pq);
+        while (!pq.empty()) {
+            result.push_back(pq.top().first);
+            pq.pop();
+        }
+        return result;
+    }
+};
