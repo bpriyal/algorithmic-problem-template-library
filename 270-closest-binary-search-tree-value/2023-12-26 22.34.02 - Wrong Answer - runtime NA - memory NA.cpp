31e2abc222a2fa729d0042a85a9a15d5911dcc9f
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2023-12-26 22:34:02 -0500

    Attempt: 2023-12-26 22.34.02 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 270-closest-binary-search-tree-value/2023-12-26 22.34.02 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    double _leastDiff = std::numeric_limits<double>::max();
+    int _closestVal;
+    void _closestValue(TreeNode* root, double target) {
+        if (!root) return;
+        double diff = abs(target - root->val);
+        if (diff < _leastDiff) {
+            _leastDiff = diff;
+            _closestVal = root->val;
+        }
+        if (root->val < target) _closestValue(root->right, target);
+        else _closestValue(root->left, target);
+    }
+public:
+    int closestValue(TreeNode* root, double target) {
+        _closestValue(root, target);
+        return _closestVal;
+    }
+};
