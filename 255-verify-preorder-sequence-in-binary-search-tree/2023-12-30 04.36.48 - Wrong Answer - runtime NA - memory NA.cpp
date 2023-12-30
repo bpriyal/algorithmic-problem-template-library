3fcef06ec86f6b9e825a5966e7b3ba552e7113cd
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2023-12-30 04:36:48 -0500

    Attempt: 2023-12-30 04.36.48 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 255-verify-preorder-sequence-in-binary-search-tree/2023-12-30 04.36.48 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    int _size;
+    void _verifyPreorder(vector<int>& preorder, int left, int right) {
+        
+    }
+public:
+    bool verifyPreorder(vector<int>& preorder) {
+        _size = preorder.size();
+        stack<int> st;
+        st.emplace(preorder[0]);
+        for (int i = 1; i < _size; ++i) {
+            if (st.empty()) return false;
+            if (st.top() > preorder[i]) {
+                st.emplace(preorder[i]);
+            } else {
+                while (!st.empty() && preorder[i] > st.top()) {
+                    // lastPopped = st.top();
+                    st.pop();
+                }
+            }
+        }
+        return st.empty();
+    }
+};
