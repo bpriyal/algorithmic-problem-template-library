17518853e67560d434bcaa78691227aa42090266
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2023-12-30 09:17:43 -0500

    Attempt: 2023-12-30 09.17.43 - Accepted - runtime 79ms - memory 21.3MB.cpp

---
+++ 333-largest-bst-subtree/2023-12-30 09.17.43 - Accepted - runtime 79ms - memory 21.3MB.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+struct Result {
+    Result(int minVal_, int maxVal_, int maxSize_) : _minVal(minVal_), _maxVal(maxVal_), _maxSize(maxSize_) {}
+    int _minVal;
+    int _maxVal;
+    int _maxSize;
+};
+class Solution {
+    Result _largestBSTSubtree(TreeNode* root) {
+        if (!root) return Result(INT_MAX, INT_MIN, 0);
+        auto left = _largestBSTSubtree(root->left);
+        auto right = _largestBSTSubtree(root->right);
+        if (left._maxVal < root->val && right._minVal > root->val) {
+            cout<<"val: "<<root->val<<", left._maxSize: "<<left._maxSize<<", right._maxSize: "<<right._maxSize<<endl;
+            return Result(min(root->val, left._minVal), max(root->val, right._maxVal), left._maxSize + right._maxSize + 1);
+        }
+        return { INT_MIN, INT_MAX, max(left._maxSize, right._maxSize)};
+    }
+public:
+    int largestBSTSubtree(TreeNode* root) {
+        return _largestBSTSubtree(root)._maxSize;
+    }
+};
