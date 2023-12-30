2ea171fa599a7c50a97b3c54c9a2718f569434f9
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2023-12-30 05:12:30 -0500

    Attempt: 2023-12-30 05.12.30 - Accepted - runtime 58ms - memory 22.8MB.cpp

---
+++ 255-verify-preorder-sequence-in-binary-search-tree/2023-12-30 05.12.30 - Accepted - runtime 58ms - memory 22.8MB.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    int _size;
+    void _verifyPreorder(vector<int>& preorder, int left, int right) {
+        
+    }
+public:
+    bool verifyPreorder(vector<int>& preorder) {
+        _size = preorder.size();
+        stack<int> st;
+        st.emplace(preorder[0]);
+        int minLimit = 0;
+        for (int i = 1; i < _size; ++i) {
+            if (minLimit >= preorder[i]) return false;
+            if (st.top() > preorder[i]) st.emplace(preorder[i]);
+            else {
+                while (!st.empty() && preorder[i] > st.top()) {
+                    minLimit = st.top();
+                    st.pop();
+                }
+                st.emplace(preorder[i]);
+            }
+        }
+        return true;
+    }
+};
