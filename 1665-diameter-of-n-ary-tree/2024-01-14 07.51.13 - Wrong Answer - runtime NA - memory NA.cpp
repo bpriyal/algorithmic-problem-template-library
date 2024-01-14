1e186b50f6488d2b24bc34d23c36116e5bfea483
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2024-01-14 07:51:13 -0500

    Attempt: 2024-01-14 07.51.13 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 1665-diameter-of-n-ary-tree/2024-01-14 07.51.13 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,42 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    int _result = INT_MIN;
+    int _diameter(Node* root) {
+        if (!root) return 0;
+        cout<<root->val<<endl;
+        int nChildren = root->children.size();
+        if (nChildren == 0) return 1;
+        vector<int> subHeights(nChildren);
+        for (int i = 0; i < nChildren; ++i) {
+            subHeights[i] = _diameter(root->children[i]);
+        }
+        sort(subHeights.begin(), subHeights.end());
+        int maxDistance = nChildren < 2? subHeights.back() : subHeights[nChildren - 1] + subHeights[nChildren - 2];
+        _result = max(_result, maxDistance);
+        return subHeights[nChildren - 1] + 1;
+    }
+public:
+    int diameter(Node* root) {
+        _diameter(root);
+        return _result;
+    }
+};
