0df73ef0189ed6a9b0ddd85e27b49033520f339c
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2024-01-16 01:03:02 -0500

    Attempt: 2024-01-16 01.03.02 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 511-all-paths-from-source-lead-to-destination/2024-01-16 01.03.02 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    vector<vector<int>> _graph;
+    bool _leadsToDestination(int path, int destination, vector<bool>& visited) {
+        if (_graph[path].empty()) return path == destination;
+        if (visited[path]) return false;
+        // if (path == destination) return true;
+        visited[path] = true;
+        for (int subPath : _graph[path]) {
+            if (!_leadsToDestination(subPath, destination, visited)) {
+                cout<<subPath<<endl;
+                return false;
+            }
+        }
+        return true;
+    }
+public:
+    bool leadsToDestination(int n, vector<vector<int>>& edges, int source, int destination) {
+        _graph.resize(n, vector<int>());
+        for (const auto& edge : edges) {
+            if (edge[0] == edge[1]) return false;
+            _graph[edge[0]].emplace_back(edge[1]);
+        }
+        if (_graph[source].empty()) return false;
+        for (int subPath : _graph[source]) {
+            vector<bool> visited(n, false);
+            visited[source] = true;
+            if (!_leadsToDestination(subPath, destination, visited)) return false;
+        }
+        return true;
+    }
+};
