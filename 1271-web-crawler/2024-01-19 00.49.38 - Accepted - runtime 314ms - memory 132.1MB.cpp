f788aea0b071a2722b546373d7c73c7b9e86d28c
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2024-01-19 00:49:38 -0500

    Attempt: 2024-01-19 00.49.38 - Accepted - runtime 314ms - memory 132.1MB.cpp

---
+++ 1271-web-crawler/2024-01-19 00.49.38 - Accepted - runtime 314ms - memory 132.1MB.cpp
@@ -0,0 +1,32 @@
+/**
+ * // This is the HtmlParser's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class HtmlParser {
+ *   public:
+ *     vector<string> getUrls(string url);
+ * };
+ */
+
+class Solution {
+    string _getHostName(string startUrl_) {
+        int pos = min(startUrl_.size(), startUrl_.find('/', 7));
+        return startUrl_.substr(7, pos - 7);
+    }
+    void _crawl(string startUrl_, HtmlParser htmlParser_, std::unordered_set<string>& crawledUrls_) {
+        crawledUrls_.emplace(startUrl_);
+        string hostName = _getHostName(startUrl_);
+        cout<<hostName<<endl;
+        for (const auto& url : htmlParser_.getUrls(startUrl_)) {            
+            if (_getHostName(url) == hostName && !crawledUrls_.count(url)) {
+                _crawl(url, htmlParser_, crawledUrls_);
+            }
+        }
+    }
+public:
+    vector<string> crawl(string startUrl, HtmlParser htmlParser) {
+        unordered_set<string> crawledUrls;
+        _crawl(startUrl, htmlParser, crawledUrls);
+        vector<string> vec(crawledUrls.begin(), crawledUrls.end());
+        return vec;
+    }
+};
