6a80be25bee7ec2ff509e285333672381bdb8754
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2024-02-11 05:28:24 -0500

    Attempt: 2024-02-11 05.28.24 - Time Limit Exceeded - runtime NA - memory NA.cpp

---
+++ 1142-minimum-knight-moves/2024-02-11 05.28.24 - Time Limit Exceeded - runtime NA - memory NA.cpp
@@ -0,0 +1,43 @@
+class Solution {
+    // unordered_map<pair<int, int>, int> _visited;
+    // int dfs(int xDest, int yDest, int xCurr, int yCurr) {
+    //     if (_visited.contains({xCurr, yCurr})) return _visited[{xCurr, yCurr}];
+    //     if (xDest == xCurr && yDest == yCurr) return 0;
+    //     vector<vector<int>> possibleMoves = {{2, 1}, {1, 2}, {-1, 2}, {-2, 1},
+    //                                          {-2, -1}, {-1, -2}, {1, -2}, {2, -1}};
+    //     int minMoves = INT_MAX;
+    //     for (const auto& move : possibleMoves) {
+    //         minMoves = min(dfs(xDest, yDest, xCurr + move[0], yCurr + move[1]), minMoves);
+    //     }
+    //     return minMoves 
+    // }
+public:
+    int minKnightMoves(int x, int y) {
+        vector<vector<int>> possibleMoves = {{2, 1}, {1, 2}, {-1, 2}, {-2, 1},
+                                             {-2, -1}, {-1, -2}, {1, -2}, {2, -1}};
+        std::queue<pair<int, int>> bfsQueue;
+        set<pair<int, int>> visited;
+        bfsQueue.emplace(0, 0);
+        visited.emplace(0, 0);
+        int minMoves = -1;
+
+        while (!bfsQueue.empty()) {
+            int size = bfsQueue.size();
+            // cout<<size<<endl;
+            ++minMoves;
+            for (int i = 0; i < size; ++i) {
+                auto location = bfsQueue.front();
+                bfsQueue.pop();
+                if (location.first == x && location.second == y) return minMoves;
+                for (const auto& move : possibleMoves) {
+                    int newX = location.first + move[0];
+                    int newY = location.second + move[1];
+                    if (visited.count({newX, newY})) continue;
+                    bfsQueue.emplace(newX, newY);
+                    visited.emplace(newX, newY);
+                }
+            }
+        }
+        return -1;
+    }
+};
