f95cc9ad49cdca045c2c92f21e9cb18bb895fe18
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2024-02-25 07:19:09 -0500

    Attempt: 2024-02-25 07.19.09 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 358-rearrange-string-k-distance-apart/2024-02-25 07.19.09 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,34 @@
+class Comp {
+  public:
+    bool operator()(const pair<char, int> p1, const pair<char, int> p2) {
+        return p1.second < p2.second;
+    }
+};
+class Solution {
+public:
+    string rearrangeString(string s, int k) {
+        if (k == 0) return s;
+        priority_queue<pair<char, int>, vector<pair<char, int>>, Comp> pq;
+        unordered_map<char, int> cache;
+        for (char c : s) ++cache[c];
+        for (const auto& [ch, frequency] : cache) pq.emplace(ch, frequency);
+
+        string result = "";
+
+        while (!pq.empty()) {
+            vector<pair<char, int>> temp;
+            for (int i = 0; i < k; ++i) {
+                if (pq.empty()) {
+                    // cout<<result<<endl;
+                    return temp.empty()? result : "";
+                }
+                auto [ch, frequency] = pq.top(); pq.pop();
+                result += ch;
+                --frequency;
+                if (frequency != 0) temp.emplace_back(ch, frequency);
+            }
+            for (const auto& [ch, frequency] : temp) pq.emplace(ch, frequency);
+        }
+        return result;
+    }
+};
