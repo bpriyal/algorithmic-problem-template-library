5c9fee8c0a81e16a0e07ae2d55cde06fe52e3487
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2024-04-21 07:17:07 -0400

    Attempt: 2024-04-21 07.17.07 - Accepted - runtime 5ms - memory 9.7MB.cpp

---
+++ 1102-check-if-a-number-is-majority-element-in-a-sorted-array/2024-04-21 07.17.07 - Accepted - runtime 5ms - memory 9.7MB.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isMajorityElement(vector<int>& nums, int target) {
+        int size = nums.size();
+        int firstOccurence = INT_MAX, lastOccurence = INT_MIN;
+        int l = 0, r = size - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) {
+                firstOccurence = min(firstOccurence, mid);
+                r = mid - 1;
+            }
+            else if (nums[mid] < target) l = mid + 1;
+            else r = mid - 1;
+        }
+
+        // l = 0, r = size - 1;
+        // while (l <= r) {
+        //     int mid = l + (r - l) / 2;
+        //     if (nums[mid] == target) {
+        //         lastOccurence = max(lastOccurence, mid);
+        //         l = mid + 1;
+        //     }
+        //     else if (nums[mid] < target) l = mid + 1;
+        //     else r = mid - 1;
+        // }
+        // if (lastOccurence == INT_MIN || firstOccurence == INT_MAX) return false;
+        // return static_cast<double>(lastOccurence - firstOccurence + 1) > (size / 2.0);
+
+        if (firstOccurence == INT_MAX) return false;
+        int minPosition = firstOccurence + (size / 2);
+        if (minPosition >= size) return false;
+        return nums[minPosition] == target;
+    }
+};
