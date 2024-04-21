b18f1d511a9e38e8a4c209e610db06cd4c27280d
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2024-04-21 09:58:05 -0400

    Attempt: 2024-04-21 09.58.05 - Accepted - runtime 11ms - memory 17.5MB.cpp

---
+++ 1192-divide-chocolate/2024-04-21 09.58.05 - Accepted - runtime 11ms - memory 17.5MB.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    int isWorkable(const vector<int>& sweetness, int workableVal, int k) {
+        int runSweetness = 0;
+        int chunks = 0;
+        for (int val : sweetness) {
+            runSweetness += val;
+            if (runSweetness >= workableVal) {
+                ++chunks;
+                runSweetness = 0;
+            }
+        }
+        return chunks;
+    }
+public:
+    int maximizeSweetness(vector<int>& sweetness, int k) {
+        int size = sweetness.size();
+        int totalSweetness = std::accumulate(sweetness.begin(), sweetness.end(), 0);
+        int l = *min_element(sweetness.begin(), sweetness.end());
+        int r = totalSweetness / (k + 1);
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            int chunks = isWorkable(sweetness, mid, k);
+            if (chunks >= k + 1) l = mid + 1;
+            else if (chunks < k + 1) r = mid - 1;
+        }
+        return l - 1;
+    }
+};
+
+// Time = O(log(totalSweetness) * size)
