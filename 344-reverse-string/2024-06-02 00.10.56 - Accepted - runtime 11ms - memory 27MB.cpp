627e9572f359428b2c89432552776c57a5a786f1
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2024-06-02 00:10:56 -0400

    Attempt: 2024-06-02 00.10.56 - Accepted - runtime 11ms - memory 27MB.cpp

---
+++ 344-reverse-string/2024-06-02 00.10.56 - Accepted - runtime 11ms - memory 27MB.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int size = s.size();
+        for (int i = 0; i < size / 2; ++i) swap(s[i], s[size - 1 - i]);
+    }
+};
