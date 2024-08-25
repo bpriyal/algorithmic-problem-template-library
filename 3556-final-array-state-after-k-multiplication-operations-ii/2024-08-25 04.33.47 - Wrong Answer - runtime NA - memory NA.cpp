f1a653f313dbf37a837a2a75901ce7dc9ee07a10
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2024-08-25 04:33:47 -0400

    Attempt: 2024-08-25 04.33.47 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 3556-final-array-state-after-k-multiplication-operations-ii/2024-08-25 04.33.47 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,28 @@
+struct Comp {
+  bool operator()(pair<int, int> p1, pair<int, int> p2) {
+    return p1.first > p2.first;
+  }
+};
+
+class Solution {
+    static constexpr long mod = 1000000007;
+public:
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        int size = nums.size();
+        vector<int> result(size);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, Comp> pq;
+        for (int i = 0; i < size; ++i) pq.emplace(nums[i], i);
+        for (int op = 0; op < k; ++op) {
+          auto [value, index] = pq.top();
+          pq.pop();
+          value = ((value % mod) * multiplier) % mod;
+          pq.emplace(value, index);
+        }
+        while (!pq.empty()) {
+          auto [value, index] = pq.top();
+          pq.pop();
+          result[index] = value % mod;
+        }
+        return result;
+    }
+};
