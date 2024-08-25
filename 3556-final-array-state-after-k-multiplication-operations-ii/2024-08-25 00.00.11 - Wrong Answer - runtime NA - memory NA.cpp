f68f870cc84912a02223b214980f28343971ddd7
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2024-08-25 00:00:11 -0400

    Attempt: 2024-08-25 00.00.11 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 3556-final-array-state-after-k-multiplication-operations-ii/2024-08-25 00.00.11 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,13 @@
+class Solution {
+    static constexpr long mod = 1000000007;
+public:
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        int size = nums.size();
+        for (int op = 0; op < k; ++op) {
+          auto it = min_element(nums.begin(), nums.end());
+          *it = ((*it) % mod) * multiplier;
+        }
+        for (auto& num : nums) num = num % mod;
+        return nums;
+    }
+};
