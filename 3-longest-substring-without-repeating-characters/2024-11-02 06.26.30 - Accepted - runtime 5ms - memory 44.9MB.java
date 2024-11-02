3d8b70456743788cc58f627a39fde7eae5061543
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2024-11-02 06:26:30 -0400

    Attempt: 2024-11-02 06.26.30 - Accepted - runtime 5ms - memory 44.9MB.java

---
+++ 3-longest-substring-without-repeating-characters/2024-11-02 06.26.30 - Accepted - runtime 5ms - memory 44.9MB.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Map currentSub = new HashMap<Integer, Integer>();
+        int longestSubL = 0;
+        int longestSubR = -1;
+        int l = 0;
+        int len = s.length();
+
+        for ( int i = 0 ; i < len; ++i ) {
+            char c = s.charAt(i);
+
+            // check if you wanna close old window
+            if ( currentSub.containsKey(c) && (int)currentSub.get(c) >= l) {
+            // close old window
+            
+                if ( (i - l) > (longestSubR - longestSubL + 1) ) { 
+                    // new longest sub
+                    longestSubR = i - 1;
+                    longestSubL = l;
+                }
+                
+            // start new window
+                l = (int)currentSub.get(c) + 1;
+            }
+            // else don't close old window
+
+            // in all cases
+            currentSub.put(c, i);
+        }
+        if ( (len - l) > (longestSubR - longestSubL + 1) ) { 
+                    // new longest sub
+                    longestSubR = len - 1;
+                    longestSubL = l;
+        }
+        return longestSubR - longestSubL + 1;
+    }
+}
