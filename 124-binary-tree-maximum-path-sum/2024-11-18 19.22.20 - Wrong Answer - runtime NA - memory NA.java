15d606b04799cfbdd5992c4f4dee0895812616f9
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2024-11-18 19:22:20 -0500

    Attempt: 2024-11-18 19.22.20 - Wrong Answer - runtime NA - memory NA.java

---
+++ 124-binary-tree-maximum-path-sum/2024-11-18 19.22.20 - Wrong Answer - runtime NA - memory NA.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    // int maxSum = 0;
+    public int maxPathSum(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return root.val;
+        }
+        if (root.left == null) {
+            return root.val + root.right.val;
+        }
+        if (root.right == null) {
+            return root.val + root.left.val;
+        }
+
+        int left = maxPathSum (root.left);
+        int right = maxPathSum (root.right);
+        int max = Math.max(left,right);
+        int curr = root.val + root.left.val + root.right.val;
+        return Math.max(curr, max);
+    }
+}
