f61659e15098b10ab7c8d4843e3cba656725e3da
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2024-12-01 06:49:07 -0500

    Attempt: 2024-12-01 06.49.07 - Accepted - runtime 50ms - memory 16MB.cpp

---
+++ 1-two-sum/2024-12-01 06.49.07 - Accepted - runtime 50ms - memory 16MB.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> numToIdxMap;
+        vector<int> result;
+        for (int i = 0; i < nums.size(); ++i) numToIdxMap.emplace(nums[i], i);
+        for (int i = 0; i < nums.size(); ++i) {
+            cout << i << ", " << nums[i] <<endl;
+            if (auto it = numToIdxMap.find(target - nums[i]); it != numToIdxMap.end() && i != it->second) {
+                cout << "found " <<endl;
+                result.emplace_back(i);
+                result.emplace_back(it->second);
+                return result;
+            }
+        }
+        return result;
+    }
+};
