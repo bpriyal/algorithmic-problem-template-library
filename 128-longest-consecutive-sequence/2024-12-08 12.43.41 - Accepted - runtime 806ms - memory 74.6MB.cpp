69749cec9b6df19cbf352c3d891f79e316c4035f
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2024-12-08 12:43:41 -0500

    Attempt: 2024-12-08 12.43.41 - Accepted - runtime 806ms - memory 74.6MB.cpp

---
+++ 128-longest-consecutive-sequence/2024-12-08 12.43.41 - Accepted - runtime 806ms - memory 74.6MB.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> cache(nums.begin(), nums.end());
+        int longestSequence = 0;
+        for (const auto& num : nums) {
+            if (!cache.count(num - 1)) {
+                int current = num;
+                int sequence = 1;
+                while (cache.count(current + 1)) {
+                    ++sequence;
+                    ++current;
+                }
+                longestSequence = max(longestSequence, sequence);
+            }
+        }
+        return longestSequence;
+    }
+};
