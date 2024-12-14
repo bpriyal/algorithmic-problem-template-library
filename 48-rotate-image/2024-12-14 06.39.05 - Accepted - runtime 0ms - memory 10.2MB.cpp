a160a1b62bddc4ad3fc9b52fb81fb086d731fa1e
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2024-12-14 06:39:05 -0500

    Attempt: 2024-12-14 06.39.05 - Accepted - runtime 0ms - memory 10.2MB.cpp

---
+++ 48-rotate-image/2024-12-14 06.39.05 - Accepted - runtime 0ms - memory 10.2MB.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+
+        for (int r = 0; r < rows / 2; ++r) {
+            for (int c = 0; c < cols; ++c) {
+                swap(matrix[r][c], matrix[rows - 1 - r][c]);
+            }
+        }
+
+        for (int r = 0; r < rows; ++r) {
+            for (int c = r + 1; c < rows; ++c) {
+                swap(matrix[r][c], matrix[c][r]);
+            }
+        }
+    }
+};
