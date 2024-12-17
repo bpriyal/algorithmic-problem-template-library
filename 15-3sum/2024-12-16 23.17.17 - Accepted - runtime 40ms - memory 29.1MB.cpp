bd840ad41e2445e492756d0ca6cd62a2e59859cb
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2024-12-16 23:17:17 -0500

    Attempt: 2024-12-16 23.17.17 - Accepted - runtime 40ms - memory 29.1MB.cpp

---
+++ 15-3sum/2024-12-16 23.17.17 - Accepted - runtime 40ms - memory 29.1MB.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int size = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> result;
+
+        for (int i = 0; i < size - 2 && nums[i] <= 0; ++i) {
+            if (i == 0 || nums[i] != nums[i - 1]) {
+                int j = i + 1;
+                int k = size - 1;
+                while (j < k) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (sum == 0) {
+                        result.emplace_back(vector<int>{nums[i], nums[j], nums[k]});
+                        ++j;
+                        --k;
+                        while (j < k && nums[j] == nums[j - 1]) ++j;
+                    }
+                    else if (sum < 0) ++j;
+                    else if (sum > 0) --k;
+                }
+            }
+        }
+        return result;
+    }
+};
