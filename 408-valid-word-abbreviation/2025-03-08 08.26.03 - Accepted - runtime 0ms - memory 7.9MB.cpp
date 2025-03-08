112a353d88b5bec4e9703de589d13c8d31b6a7fc
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-03-08 08:26:03 -0500

    Attempt: 2025-03-08 08.26.03 - Accepted - runtime 0ms - memory 7.9MB.cpp

---
+++ 408-valid-word-abbreviation/2025-03-08 08.26.03 - Accepted - runtime 0ms - memory 7.9MB.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    int getNumber(const string& abbr, int& startIdx, int abbrSize) {
+        int n = 0;
+        while (startIdx < abbrSize) {
+            if (isalpha(abbr[startIdx])) return n;
+            n = n * 10 + (abbr[startIdx++] - '0');
+        }
+        return n;
+    }
+public:
+    bool validWordAbbreviation(string word, string abbr) {
+        int wordSize = word.size();
+        int abbrSize = abbr.size();
+        int wordIdx = 0;
+        int abbrIdx = 0;
+
+        while (abbrIdx != abbrSize) {
+            if (isalpha(abbr[abbrIdx])) {
+                if (abbr[abbrIdx] != word[wordIdx]) return false;
+                ++abbrIdx;
+                ++wordIdx;
+            } else {
+                if (abbr[abbrIdx] == '0') return false; // leading zeros are not allowed
+                auto n = getNumber(abbr, abbrIdx, abbrSize);
+                if (wordSize - wordIdx < n) return false;
+                wordIdx += n;
+            }
+        }
+        return wordIdx == wordSize;
+    }
+};
+
+// Time complexity = O(abbrSize)
+// Space complexity = O(1)
