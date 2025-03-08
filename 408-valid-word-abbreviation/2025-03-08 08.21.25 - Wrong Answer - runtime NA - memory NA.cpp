55011013ae39b4ec523007816b334584d9c9db45
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-03-08 08:21:25 -0500

    Attempt: 2025-03-08 08.21.25 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 408-valid-word-abbreviation/2025-03-08 08.21.25 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,39 @@
+class Solution {
+    int getNumber(const string& abbr, int& startIdx, int abbrSize) {
+        int n = 0;
+        while (startIdx < abbrSize) {
+            // cout << "aakashyap: " << abbr[startIdx] << endl;
+            if (isalpha(abbr[startIdx])) return n;
+            n = n * 10 + (abbr[startIdx++] - '0');
+            // cout << "iter: " << n << endl;
+        }
+        return n;
+    }
+public:
+    bool validWordAbbreviation(string word, string abbr) {
+        int wordSize = word.size();
+        int abbrSize = abbr.size();
+        int wordIdx = 0;
+        int abbrIdx = 0;
+
+        while (abbrIdx != abbrSize) {
+            if (isalpha(abbr[abbrIdx])) {
+                if (abbr[abbrIdx] != word[wordIdx]) {
+                    cout << "return from 1 " << abbr[abbrIdx] << ", " << word[wordIdx];
+                    return false;
+                }
+                ++abbrIdx;
+                ++wordIdx;
+            } else {
+                auto n = getNumber(abbr, abbrIdx, abbrSize);
+                cout << n << endl;
+                if (wordSize - wordIdx < n) {
+                    cout << "return from 1" << endl;
+                    return false;
+                }
+                wordIdx += n;
+            }
+        }
+        return true;
+    }
+};
