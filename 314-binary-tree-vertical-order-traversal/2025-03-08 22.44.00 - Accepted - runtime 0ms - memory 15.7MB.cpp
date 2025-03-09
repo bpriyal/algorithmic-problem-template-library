2cd19d5f70cca8cf44b515acfdeadaf0fd991f3a
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-03-08 22:44:00 -0500

    Attempt: 2025-03-08 22.44.00 - Accepted - runtime 0ms - memory 15.7MB.cpp

---
+++ 314-binary-tree-vertical-order-traversal/2025-03-08 22.44.00 - Accepted - runtime 0ms - memory 15.7MB.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> verticalOrder(TreeNode* root) {
+      vector<vector<int>> result;
+      if (!root) return result;
+
+      unordered_map<int, vector<int>> cache;
+      int minCol = 0, maxCol = 0;
+      queue<pair<TreeNode*, int>> q;
+      q.emplace(make_pair(root, 0));
+
+      while (!q.empty()) {
+        auto [node, pos] = q.front();
+        q.pop();
+
+        minCol = min(minCol, pos);
+        maxCol = max(maxCol, pos);
+        cache[pos].emplace_back(node->val);
+
+        if (node->left) q.emplace(make_pair(node->left, pos - 1));
+        if (node->right) q.emplace(make_pair(node->right, pos + 1));
+      }
+
+      for (int col = minCol; col <= maxCol; ++col) result.emplace_back(cache[col]);
+      return result;
+    }
+};
+
+// Time complexity: O(N * logn) + O(key)
+// Space complexity: O(N) + O()
