ccadcc25f4763db783b66d6e5f744ce9f0e09a7e
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-03-11 14:16:05 -0400

    Attempt: 2025-03-11 14.16.05 - Accepted - runtime 4ms - memory 11.8MB.cpp

---
+++ 227-basic-calculator-ii/2025-03-11 14.16.05 - Accepted - runtime 4ms - memory 11.8MB.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int calculate(string s) {
+      int size = s.size();
+      int currentNum = 0;
+      int lastNum = 0;
+      int result = 0;
+      char sign = '+';
+
+      for (int i = 0; i < size; ++i) {
+        auto currentChar = s[i];
+        if (isdigit(currentChar)) {
+          currentNum = currentNum * 10 + (currentChar - '0');
+        }
+        if ((!isdigit(currentChar) && !iswspace(currentChar)) || i == size - 1) {
+          if (sign == '+' || sign == '-') {
+            result += lastNum;
+            lastNum = sign == '+' ? currentNum : -currentNum;
+          } else if (sign == '*' || sign == '/') {
+            lastNum = sign == '*' ? lastNum * currentNum : lastNum / currentNum;
+          }
+          currentNum = 0;
+          sign = currentChar;
+        }
+      }
+
+      result += lastNum;
+      return result;
+    }
+};
