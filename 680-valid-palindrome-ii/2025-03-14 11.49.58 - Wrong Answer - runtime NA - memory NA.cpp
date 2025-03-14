f8183c3c271e4744d7c38d14a82b52b6bc8c998b
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-03-14 11:49:58 -0400

    Attempt: 2025-03-14 11.49.58 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 680-valid-palindrome-ii/2025-03-14 11.49.58 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+      int size = s.size();
+      int i = 0, j = size - 1;
+
+      while (i < j) {
+        if (s[i] == s[j]) {
+          ++i;
+          --j;
+        }
+        else if (s[i + 1] == s[j]) ++i;
+        else if (s[i] == s[j - 1]) --j;
+        else return false;
+      }
+
+      return true;
+    }
+};
