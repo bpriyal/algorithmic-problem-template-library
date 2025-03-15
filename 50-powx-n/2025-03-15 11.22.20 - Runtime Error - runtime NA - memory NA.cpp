6bfc5854019da6f74a2b5f7e1303054d616a109e
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-03-15 11:22:20 -0400

    Attempt: 2025-03-15 11.22.20 - Runtime Error - runtime NA - memory NA.cpp

---
+++ 50-powx-n/2025-03-15 11.22.20 - Runtime Error - runtime NA - memory NA.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+      if (x == 0) return 0;
+      if (n == 0) return 1;
+
+      if (n < 0) {
+        x = 1 / x;
+        n = -n;
+      }
+
+      if (n % 2) return x * myPow(x * x, (n - 1) / 2);
+      else return myPow(x * x, n / 2);
+    }
+};
