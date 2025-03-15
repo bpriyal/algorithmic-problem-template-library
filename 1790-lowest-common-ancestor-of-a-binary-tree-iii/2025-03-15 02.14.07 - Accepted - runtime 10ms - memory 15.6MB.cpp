206fe022a157a927b280e3fc07780b2753dd2069
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-03-15 02:14:07 -0400

    Attempt: 2025-03-15 02.14.07 - Accepted - runtime 10ms - memory 15.6MB.cpp

---
+++ 1790-lowest-common-ancestor-of-a-binary-tree-iii/2025-03-15 02.14.07 - Accepted - runtime 10ms - memory 15.6MB.cpp
@@ -0,0 +1,34 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* parent;
+};
+*/
+
+class Solution {
+public:
+    Node* lowestCommonAncestor(Node* p, Node * q) {
+      unordered_set<Node*> pPathCache;
+      Node* temp = p;
+
+      while (temp) {
+        pPathCache.emplace(temp);
+        temp = temp->parent;
+      }
+
+      temp = q;
+      while (temp) {
+        if (pPathCache.contains(temp)) return temp;
+        temp = temp->parent;
+      }
+
+      return nullptr;
+    }
+};
+
+// Time Compleixty = O(logH) => O(N)
+// Space Complexity = O(logH) + O(logH) => O(N)
