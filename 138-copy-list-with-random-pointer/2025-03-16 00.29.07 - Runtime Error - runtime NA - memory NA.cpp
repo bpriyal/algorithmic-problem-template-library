2c587fcf2b99aa802c29436ea92ecea2d021cf18
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-03-16 00:29:07 -0400

    Attempt: 2025-03-16 00.29.07 - Runtime Error - runtime NA - memory NA.cpp

---
+++ 138-copy-list-with-random-pointer/2025-03-16 00.29.07 - Runtime Error - runtime NA - memory NA.cpp
@@ -0,0 +1,62 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+      unordered_map<int, Node*> idxToNodeMap;
+      unordered_map<Node*, int> nodeToIdxMap;
+      Node *copyTemp = nullptr, *copyPrev = nullptr, *originalTemp = head;
+      Node* copyHead = nullptr;
+      int index = 0;
+
+      // First iterate and allocate all nodes based on next
+      while (originalTemp) {
+        // cout << originalTemp->val << endl;
+        Node* node = new Node(originalTemp->val);
+        idxToNodeMap.try_emplace(index, node);
+        nodeToIdxMap.try_emplace(originalTemp, index);
+        if (index != 0) {
+          copyPrev->next = node;
+          copyPrev = copyPrev->next;
+        } else {
+          copyPrev = node;
+          copyHead = node;
+        }
+        ++index;
+        originalTemp = originalTemp->next;
+      }
+
+      // Now iterate and set random pointers
+      originalTemp = head;
+      copyTemp = copyHead;
+      cout << copyTemp->next->val << endl;
+      while (originalTemp) {
+        // if (!copyTemp) cout << "null copy temp " << endl;
+        // else cout << copyTemp->val << endl;
+        if (originalTemp->random) {
+          auto idx = nodeToIdxMap[originalTemp->random];
+          // cout << "idx: " << idx << ", val: " << copyTemp->val << endl;
+
+          copyTemp->random = idxToNodeMap[idx];
+        }
+        originalTemp = originalTemp->next;
+        copyTemp = copyTemp->next;
+      }
+
+      return copyHead;
+    }
+};
