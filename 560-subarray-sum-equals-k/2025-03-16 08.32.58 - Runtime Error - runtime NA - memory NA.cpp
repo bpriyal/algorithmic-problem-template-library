5e274ca9bc63378895e7f134e5b0c381f2e9bed9
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-03-16 08:32:58 -0400

    Attempt: 2025-03-16 08.32.58 - Runtime Error - runtime NA - memory NA.cpp

---
+++ 560-subarray-sum-equals-k/2025-03-16 08.32.58 - Runtime Error - runtime NA - memory NA.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+      int size = nums.size();
+
+      int subarrays = 0;
+      for (int i = 0; i < size; ++i) {
+        int sum = 0;
+        for (int j = i; j < size + 1; ++j) {
+          sum += nums[j];
+          if (sum == k) ++subarrays;
+        }
+      }
+
+      return subarrays;
+    }
+};
