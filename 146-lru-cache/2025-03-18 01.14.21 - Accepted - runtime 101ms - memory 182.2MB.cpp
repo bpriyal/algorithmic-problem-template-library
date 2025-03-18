b9efd20f363f3422af55415693d3a5c2037cc209
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-03-18 01:14:21 -0400

    Attempt: 2025-03-18 01.14.21 - Accepted - runtime 101ms - memory 182.2MB.cpp

---
+++ 146-lru-cache/2025-03-18 01.14.21 - Accepted - runtime 101ms - memory 182.2MB.cpp
@@ -0,0 +1,51 @@
+class LRUCache {
+    std::list<pair<int, int>> lruCache;
+    unordered_map<int, std::list<pair<int, int>>::iterator> idxMap;
+    int capacity;
+public:
+    LRUCache(int capacity) {
+      this->capacity = capacity;
+    }
+    
+    int get(int key) {
+      auto idxIt = idxMap.find(key);
+      if (idxIt != idxMap.end()) {
+        auto [_, value] = *idxIt->second;
+        lruCache.erase(idxIt->second);
+        lruCache.emplace_back(key, value);
+        idxIt->second = std::prev(lruCache.end());
+        return value;
+      } else return -1;
+    }
+    
+    void put(int key, int value) {
+      // cout << "key: " << keyz << ",value: " << value << ", size: " << lruCache.size() << endl;
+      auto idxIt = idxMap.find(key);
+      if (idxIt != idxMap.end()) {
+        lruCache.erase(idxIt->second);
+        lruCache.emplace_back(key, value);
+        idxIt->second = std::prev(lruCache.end());
+      } else {
+        // cout << "** nOt in map: " << key << endl;
+        if (lruCache.size() == capacity) {
+          // cout << "Capacity reached: " << key << endl;
+          auto [k, v] = lruCache.front();
+          lruCache.pop_front();
+          idxMap.erase(k);
+
+          lruCache.emplace_back(key, value);
+          idxMap.try_emplace(key, std::prev(lruCache.end()));
+        } else {
+          lruCache.emplace_back(key, value);
+          idxMap.try_emplace(key, std::prev(lruCache.end()));
+        }
+      }
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
