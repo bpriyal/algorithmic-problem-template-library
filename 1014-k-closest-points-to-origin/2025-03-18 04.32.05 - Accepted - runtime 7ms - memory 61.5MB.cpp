0d0cf28763aba97dc9cc8b04bc7be1d2d1aa2052
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-03-18 04:32:05 -0400

    Attempt: 2025-03-18 04.32.05 - Accepted - runtime 7ms - memory 61.5MB.cpp

---
+++ 1014-k-closest-points-to-origin/2025-03-18 04.32.05 - Accepted - runtime 7ms - memory 61.5MB.cpp
@@ -0,0 +1,59 @@
+class Solution {
+    int dist(const vector<int>& point) {
+      return point[0] * point[0] + point[1] * point[1];
+    }
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+      int size = points.size();
+      // priority_queue<pair<int, int>> pq;
+
+      // for (int i = 0; i < size; ++i) {
+      //   pair<int, int> p = { dist(points[i]), i } ;
+      //   if (pq.size() == k) {
+      //     if (p.first < pq.top().first) {
+      //       pq.pop();
+      //       pq.emplace(p);
+      //     }
+      //   } else {
+      //     pq.emplace(p);
+      //   }
+      // }
+
+      // vector<vector<int>> result;
+      // while (!pq.empty()) {
+      //   result.emplace_back(points[pq.top().second]);
+      //   pq.pop();
+      // }
+      // return result;
+
+      if (size == k) return points;
+      int left = 0;
+      int right = size - 1;
+
+      while (left <= right) {
+        int pivotIdx = left + rand() % (right - left + 1);
+
+        // swap pivot with last index
+        swap(points[pivotIdx], points[right]);
+
+        // Put pivotIdx element in correct place
+        int infPoint = left;
+        int pivotDist = dist(points[right]);
+        for (int i = left; i <= right; ++i) {
+          if (dist(points[i]) < pivotDist) {
+            swap(points[infPoint], points[i]);
+            ++infPoint;
+          }
+        }
+
+        // Swap back
+        swap(points[infPoint], points[right]);
+
+        if (infPoint == k) return vector<vector<int>>(points.begin(), points.begin() + k);
+        else if (infPoint > k) right = infPoint - 1;
+        else left = infPoint + 1;
+      }
+
+      return vector<vector<int>>();
+    }
+};
