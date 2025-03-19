5ff0f525fbbd03ca7e1fedffafaecc96286f580a
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-03-19 02:51:11 -0400

    Attempt: 2025-03-19 02.51.11 - Accepted - runtime 0ms - memory 14.9MB.cpp

---
+++ 1-two-sum/2025-03-19 02.51.11 - Accepted - runtime 0ms - memory 14.9MB.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+      unordered_map<int, int> valToIdxMap;
+
+      for (int i = 0; i < nums.size(); ++i) {
+        auto it = valToIdxMap.find(target - nums[i]);
+        if (it != valToIdxMap.end()) return {i, it->second};
+        valToIdxMap.try_emplace(nums[i], i);
+      }
+
+      // for (int i = 0; i < nums.size(); ++i) {
+      //   auto it = valToIdxMap.find(target - nums[i]);
+      //   if (it != valToIdxMap.end() && it->second != i) return {i, it->second};
+      // }
+      return vector<int>();
+    }
+};
