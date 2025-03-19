82899d4a072b9c95164c2cbf0712d14d25f19167
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-03-19 01:47:55 -0400

    Attempt: 2025-03-19 01.47.55 - Accepted - runtime 2ms - memory 8MB.cpp

---
+++ 670-maximum-swap/2025-03-19 01.47.55 - Accepted - runtime 2ms - memory 8MB.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maximumSwap(int num) {
+      string numStr = to_string(num);
+      int size = numStr.size();
+
+      vector<int> maxNumIdx(size);
+      maxNumIdx[size - 1] = size - 1;
+      for (int i = size - 2; i >= 0; --i) {
+        maxNumIdx[i] = (numStr[maxNumIdx[i + 1]] >= numStr[i]) ? maxNumIdx[i + 1] : i; 
+      }
+
+      for (int i = 0; i < size; ++i) cout << maxNumIdx[i] << endl;
+
+      for (int i = 0; i < size; ++i) {
+        if (numStr[i] != numStr[maxNumIdx[i]]) {
+          swap(numStr[i], numStr[maxNumIdx[i]]);
+          break;
+        }
+      }
+
+      return stoi(numStr);
+    }
+};
