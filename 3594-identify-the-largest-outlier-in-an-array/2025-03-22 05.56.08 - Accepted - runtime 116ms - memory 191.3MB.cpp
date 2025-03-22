f889f6f136410cc4d8df29d574e8bf1866ab1992
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-03-22 05:56:08 -0400

    Attempt: 2025-03-22 05.56.08 - Accepted - runtime 116ms - memory 191.3MB.cpp

---
+++ 3594-identify-the-largest-outlier-in-an-array/2025-03-22 05.56.08 - Accepted - runtime 116ms - memory 191.3MB.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int getLargestOutlier(vector<int>& nums) {
+      int size = nums.size();
+      unordered_map<int, int> numToFreqMap;
+      int sum = 0;
+      for (auto num : nums) {
+        ++numToFreqMap[num];
+        sum += num;
+      }
+
+      int outlier = INT_MIN;
+      for (auto num : nums) {
+        auto potentialOutlier = sum - 2 * num;
+        if (numToFreqMap[potentialOutlier] > (num == potentialOutlier)) outlier = max(outlier, potentialOutlier);
+      }
+      
+      return outlier;
+    }
+};
