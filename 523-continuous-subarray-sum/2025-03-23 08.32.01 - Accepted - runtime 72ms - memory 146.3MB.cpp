07a599e5e4af0fafb0342f7356b08c3006eaa8cf
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-03-23 08:32:01 -0400

    Attempt: 2025-03-23 08.32.01 - Accepted - runtime 72ms - memory 146.3MB.cpp

---
+++ 523-continuous-subarray-sum/2025-03-23 08.32.01 - Accepted - runtime 72ms - memory 146.3MB.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+      int size = nums.size();
+      unordered_map<int, int> modToIdxMap;
+      int sum = 0;
+
+      for (int i = 0; i < size; ++i) {
+        sum += nums[i];
+        int mod = sum % k;
+        if (mod == 0 && i >= 1) return true;
+        auto it = modToIdxMap.find(mod);
+        if (it != modToIdxMap.end()) {
+          if (i - it->second > 1) return true;
+        } else {
+          modToIdxMap.try_emplace(mod, i);
+        }
+      }
+      return false;
+    }
+};
