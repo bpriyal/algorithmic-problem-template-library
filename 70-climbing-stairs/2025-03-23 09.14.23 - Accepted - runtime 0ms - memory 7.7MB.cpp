8415cd8a6e082abd5469c601019dd886fb7f8801
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-03-23 09:14:23 -0400

    Attempt: 2025-03-23 09.14.23 - Accepted - runtime 0ms - memory 7.7MB.cpp

---
+++ 70-climbing-stairs/2025-03-23 09.14.23 - Accepted - runtime 0ms - memory 7.7MB.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    vector<int> dp;
+    int util(int n) {
+      if (n < 0) return 0;
+      if (n == 0) return 1;
+      if (dp[n] != -1) return dp[n];
+      dp[n] = util(n - 1) + util(n - 2);
+      return dp[n];
+    }
+public:
+    int climbStairs(int n) {
+      if (n <= 1) return n;
+      // return util(n);
+
+      // Iterative
+      int first = 1;
+      int second = 2;
+      int third = 2;
+
+      for (int i = 3; i <= n; ++i) {
+        third = first + second;
+        first = second;
+        second = third;
+      }
+
+      return third;
+    }
+};
