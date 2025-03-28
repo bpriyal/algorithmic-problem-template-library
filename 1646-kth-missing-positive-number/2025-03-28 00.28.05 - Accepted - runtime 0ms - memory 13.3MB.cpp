17283707ea31cea69f62cb4187f836da9bf321b6
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-03-28 00:28:05 -0400

    Attempt: 2025-03-28 00.28.05 - Accepted - runtime 0ms - memory 13.3MB.cpp

---
+++ 1646-kth-missing-positive-number/2025-03-28 00.28.05 - Accepted - runtime 0ms - memory 13.3MB.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+      int size = arr.size();
+      int left = 0, right = size - 1;
+      int mid;
+
+      while (left <= right) {
+        mid = left + (right - left) / 2;
+        int missingNumbersTillMid = arr[mid] - 1 - mid;
+        if (missingNumbersTillMid < k) left = mid + 1;
+        else right = mid - 1;
+      }
+
+      return left + k;
+    }
+};
