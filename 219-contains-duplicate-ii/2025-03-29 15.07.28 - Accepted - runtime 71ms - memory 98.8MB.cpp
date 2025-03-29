e1b40533e30fca3b2392a51cdaf4ba98a7179a7a
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-03-29 15:07:28 -0400

    Attempt: 2025-03-29 15.07.28 - Accepted - runtime 71ms - memory 98.8MB.cpp

---
+++ 219-contains-duplicate-ii/2025-03-29 15.07.28 - Accepted - runtime 71ms - memory 98.8MB.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+      unordered_map<int, int> numToIdxMap;
+
+      for (int i = 0; i < nums.size(); ++i) {
+        auto it = numToIdxMap.find(nums[i]);
+        if (it != numToIdxMap.end()) {
+          if (i - it->second <= k) return true;
+          it->second = i;
+        } else numToIdxMap.try_emplace(nums[i], i);
+      }
+      return false;
+    }
+};
