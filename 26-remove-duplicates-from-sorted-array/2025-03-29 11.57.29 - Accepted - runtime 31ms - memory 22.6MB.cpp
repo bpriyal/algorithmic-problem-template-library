1bce5f3511d04b4fa40e525876993dc1a27a202e
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-03-29 11:57:29 -0400

    Attempt: 2025-03-29 11.57.29 - Accepted - runtime 31ms - memory 22.6MB.cpp

---
+++ 26-remove-duplicates-from-sorted-array/2025-03-29 11.57.29 - Accepted - runtime 31ms - memory 22.6MB.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+      int size = nums.size();
+      int idx = -1;
+      nums.emplace_back(INT_MAX);
+      for (int i = 0; i < size; ++i) {
+        if (nums[i] != nums[i + 1]) {
+          ++idx;
+          nums[idx] = nums[i];
+          cout << "idx: " << idx << ", nums[i]: " << nums[i] << endl;
+        }
+      }
+      return idx + 1;
+    }
+};
