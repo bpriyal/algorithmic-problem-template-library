580c5187ac3aa5d9e40a125f41f17849e4bf643d
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-03-30 04:37:20 -0400

    Attempt: 2025-03-30 04.37.20 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 14-longest-common-prefix/2025-03-30 04.37.20 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+      int size = strs.size();
+
+      // string prefix = strs[0];
+
+      // for (int i = 1; i < size; ++i) {
+      //   int j = 0, k = 0;
+      //   string newPrefix;
+      //   while (j < prefix.size() && k < strs[i].size() && prefix[j] == strs[i][k]) {
+      //     newPrefix.append(1, prefix[j]);
+      //     ++j;
+      //     ++k;
+      //   }
+      //   prefix = newPrefix;
+      // }
+      // return prefix;
+
+      for (int col = 0; col < strs[0].size(); ++col) {
+        char c = strs[0][col];
+        for (int i = 1; i < size; ++i) {
+          if (col == strs[i].size() || c != strs[i][col]) return strs[i].substr(0, col);
+        }
+      }
+      return "";
+    }
+};
