1136dd6c6384e07db9941d7a3c26f591831a117c
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-03-30 04:18:44 -0400

    Attempt: 2025-03-30 04.18.44 - Accepted - runtime 3ms - memory 11.9MB.cpp

---
+++ 14-longest-common-prefix/2025-03-30 04.18.44 - Accepted - runtime 3ms - memory 11.9MB.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+      int size = strs.size();
+      if (size == 1) return strs[0];
+
+      string prefix = strs[0];
+
+      for (int i = 1; i < size; ++i) {
+        int j = 0, k = 0;
+        string newPrefix;
+        while (j < prefix.size() && k < strs[i].size() && prefix[j] == strs[i][k]) {
+          newPrefix.append(1, prefix[j]);
+          ++j;
+          ++k;
+        }
+        prefix = newPrefix;
+      }
+      return prefix;
+    }
+};
