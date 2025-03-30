fe069dbc3ba9c33ce52b2fb03f9440b88763c801
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-03-30 07:06:10 -0400

    Attempt: 2025-03-30 07.06.10 - Accepted - runtime 2ms - memory 74.5MB.cpp

---
+++ 53-maximum-subarray/2025-03-30 07.06.10 - Accepted - runtime 2ms - memory 74.5MB.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+      int size = nums.size();
+      vector<int> prefixSum(size, 0);
+      for (int i = 0; i < size; ++i) prefixSum[i] = i == 0? nums[i] : prefixSum[i - 1] + nums[i];
+      int minSum = 0;
+      int maxSum = nums[0];
+
+      for (int i = 0; i < size; ++i) {
+        maxSum = max(maxSum, prefixSum[i] - minSum);
+        minSum = min(minSum, prefixSum[i]);
+      }
+
+      return maxSum;
+    }
+};
