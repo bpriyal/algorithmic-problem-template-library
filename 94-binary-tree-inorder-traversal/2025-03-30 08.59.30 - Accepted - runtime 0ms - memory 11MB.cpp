dd98410ae8e3cc98c831e4320211e4f436403114
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-03-30 08:59:30 -0400

    Attempt: 2025-03-30 08.59.30 - Accepted - runtime 0ms - memory 11MB.cpp

---
+++ 94-binary-tree-inorder-traversal/2025-03-30 08.59.30 - Accepted - runtime 0ms - memory 11MB.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    void inorder(TreeNode* root, vector<int>& result) {
+      if (!root) return;
+      inorder(root->left, result);
+      result.emplace_back(root->val);
+      inorder(root->right, result);
+    }
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+      // vector<int> result;
+      // inorder(root, result);
+      // return result;
+
+      vector<int> result;
+      stack<TreeNode*> st;
+      TreeNode* curr = root;
+      while (curr != nullptr || !st.empty()) {
+        while (curr) {
+          st.emplace(curr);
+          curr = curr->left;
+        }
+        auto* node = st.top();
+        st.pop();
+        result.emplace_back(node->val);
+        curr = node->right;
+      } 
+      return result;
+    }
+};
