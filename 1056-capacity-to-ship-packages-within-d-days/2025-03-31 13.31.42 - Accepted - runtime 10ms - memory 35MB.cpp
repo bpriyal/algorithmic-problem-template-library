80bb598aad3132a69c6825c8eb69e6d40a7d681a
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-03-31 13:31:42 -0400

    Attempt: 2025-03-31 13.31.42 - Accepted - runtime 10ms - memory 35MB.cpp

---
+++ 1056-capacity-to-ship-packages-within-d-days/2025-03-31 13.31.42 - Accepted - runtime 10ms - memory 35MB.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    bool checkPossibility(const vector<int>& weights, int weight, int days) {
+      int run = 0;
+      int expectedDays = 1;
+      for (int i = 0; i < weights.size(); ++i) {
+        run += weights[i];
+        if (weight < run) {
+          run = weights[i];
+          ++expectedDays;
+        }
+      }
+      return expectedDays <= days;
+    }
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+      int sum = 0;
+      int maxWeight = INT_MIN;
+      for (int weight : weights) {
+        sum += weight;
+        maxWeight = max(maxWeight, weight);
+      }
+      int left = maxWeight;
+      int right = sum;
+
+      while (left < right) {
+        int mid = left + (right - left) / 2;
+        auto possible = checkPossibility(weights, mid, days);
+        if (!possible) left = mid + 1;
+        else right = mid;
+      }
+      return left;
+    }
+};
+
+// Time complexity = O(N) + O(Nlog500 * N)
