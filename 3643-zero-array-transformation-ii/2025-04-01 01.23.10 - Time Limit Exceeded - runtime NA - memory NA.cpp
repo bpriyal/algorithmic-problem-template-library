060a6ce524b94b4dc31f01afba539f93afdb56a9
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-01 01:23:10 -0400

    Attempt: 2025-04-01 01.23.10 - Time Limit Exceeded - runtime NA - memory NA.cpp

---
+++ 3643-zero-array-transformation-ii/2025-04-01 01.23.10 - Time Limit Exceeded - runtime NA - memory NA.cpp
@@ -0,0 +1,42 @@
+class Solution {
+    int numSize, queriesSize;
+    bool isZero(const vector<int>& nums) {
+      int sum = 0;
+      for (const int& num : nums) {
+        // cout << num << ",";
+        sum += num;
+      }
+      cout << endl;
+      return sum == 0;
+    }
+    bool possible(vector<int> nums, const vector<vector<int>>& queries, int K) {
+      for (int k = 0; k < K; ++k) {
+        for (int i = queries[k][0]; i <= queries[k][1]; ++i) {
+          nums[i] = max(0, nums[i] - queries[k][2]);
+        }
+        if (isZero(nums)) return true;
+      }
+      // cout << "returning K" << K<<  endl;
+      return false;
+    }
+public:
+    int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+      if (isZero(nums)) return 0;
+      numSize = nums.size();
+      queriesSize = queries.size();
+
+      int left = 0, right = queriesSize;
+      bool isZero = false;
+
+      while (left <= right) {
+        // cout<<left<<","<<right<<endl;
+        int mid = left + (right - left) / 2;
+        if (possible(nums, queries, mid)) {
+          right = mid - 1;
+          isZero = true;
+        }
+        else left = mid + 1;
+      }
+      return isZero ? left : -1;
+    }
+};
