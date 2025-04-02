3d0a27bcceb0c03ca5eee163578429c0b3c7dede
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-02 01:58:20 -0400

    Attempt: 2025-04-02 01.58.20 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 8-string-to-integer-atoi/2025-04-02 01.58.20 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+      int size = s.size();
+      bool positive = true;
+      int result = 0;
+      int index = 0;
+      int sign = 1;
+      
+      while (index < size && s[index] == ' ') ++index;
+      if (index < size && s[index] == '+') {
+        sign = 1;
+        ++index;
+      } else if (index < size && s[index] == '-') {
+        sign = -1;
+        ++index;
+      }
+
+      while (index < size && isdigit(s[index])) {
+        if (result > INT_MAX / 10 || (result == INT_MAX / 10 && s[index] > INT_MAX % 10)) {
+          return sign == 1 ? INT_MAX : INT_MIN;
+        }
+        cout << s[index] - '0' << endl;
+        result = result * 10 + s[index] - '0';
+        ++index;
+      }
+
+      return sign * result;
+    }
+};
