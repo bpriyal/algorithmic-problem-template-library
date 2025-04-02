b9c9b6e9cee5686297420a62de37f2b9dab360d7
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-02 01:14:54 -0400

    Attempt: 2025-04-02 01.14.54 - Accepted - runtime 122ms - memory 32.6MB.cpp

---
+++ 636-exclusive-time-of-functions/2025-04-02 01.14.54 - Accepted - runtime 122ms - memory 32.6MB.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> exclusiveTime(int n, vector<string>& logs) {
+      int size = logs.size();
+
+      vector<vector<string>> logSplit;
+      for (int i = 0; i < size; ++i) {
+        std::stringstream ss(logs[i]);
+        string token;
+        vector<string> tokens;
+        while (std::getline(ss, token, ':')) {
+          tokens.emplace_back(token);
+        }
+        logSplit.emplace_back(tokens);
+      }
+      cout << "here"<< endl;
+      vector<int> time(n, 0);
+      stack<int> st;
+      st.emplace(stoi(logSplit[0][0]));
+      int i = 1;
+      int prevTime = stoi(logSplit[0][2]);
+      while (i < size) {
+        cout << i << endl;
+        auto localTime = stoi(logSplit[i][2]);
+        if (logSplit[i][1] == "start") {
+          if (!st.empty()) time[st.top()] += localTime - prevTime;
+          st.emplace(stoi(logSplit[i][0]));
+          prevTime = localTime;
+        } else {
+          if (st.empty()) cout << "stack empty" << endl;
+          else cout << "stack not empty" << endl;
+          time[st.top()] += localTime + 1 - prevTime;
+          cout << "here4" << endl;
+          st.pop();
+          prevTime = localTime + 1;
+        }
+        ++i;
+      }
+
+      return time;
+    }
+};
