11a1d47b4d9eab7cc15701771e527918d8aa9272
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-02 14:24:26 -0400

    Attempt: 2025-04-02 14.24.26 - Time Limit Exceeded - runtime NA - memory NA.cpp

---
+++ 70-climbing-stairs/2025-04-02 14.24.26 - Time Limit Exceeded - runtime NA - memory NA.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    vector<int> dp;
+    int util(int n) {
+      if (n < 0) return 0;
+      if (n == 0) return 1;
+      if (dp[n] != -1) return dp[n];
+      dp[n] = climbStairs(n - 1) + climbStairs(n - 2);
+      return dp[n];
+    }
+public:
+    int climbStairs(int n) {
+      dp.resize(n + 1, -1);
+      return util(n);
+    }
+};
