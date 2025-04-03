3e4979cbe74e282081d49de57455ae216a4475ca
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-03 03:38:18 -0400

    Attempt: 2025-04-03 03.38.18 - Accepted - runtime 0ms - memory 21.2MB.cpp

---
+++ 270-closest-binary-search-tree-value/2025-04-03 03.38.18 - Accepted - runtime 0ms - memory 21.2MB.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int closestVal;
+    void util(TreeNode* root, double target) {
+      if (!root) return;
+      double candidate = abs(root->val - target);
+      double existingDiff = abs(closestVal - target);
+      
+      if (candidate == existingDiff) closestVal = min(closestVal, root->val);
+      else if (candidate < existingDiff) closestVal = root->val;
+
+      if (root->val == target) return;
+      else if (root->val < target) util(root->right, target);
+      else util(root->left, target);
+    }
+public:
+    int closestValue(TreeNode* root, double target) {
+      closestVal = root->val;
+      util(root, target);
+      return closestVal;
+    }
+};
