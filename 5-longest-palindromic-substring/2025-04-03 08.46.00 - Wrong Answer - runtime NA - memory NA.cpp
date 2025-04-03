03bfec31be3a050d3787be1aee9fa77a38ec91f2
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-03 08:46:00 -0400

    Attempt: 2025-04-03 08.46.00 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 5-longest-palindromic-substring/2025-04-03 08.46.00 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,40 @@
+class Solution {
+    int size;
+    int maxPal = 0;
+    void util(const string& s, int i, int j) {
+      while (i >= 0 && j < size) {
+        if (s[i] != s[j]) break;
+        --i;
+        ++j;
+      }
+      maxPal = max(maxPal, j - i -1);
+    }
+public:
+    string longestPalindrome(string s) {
+      size = s.size();
+      int resultStartIdx = 0, resultEndIdx = 0;
+
+      vector<vector<bool>> dp(size, vector<bool>(size, false));
+
+      for (int i = 0; i < size; ++i) {
+        dp[i][i] = true;
+        if (i != size - 1 && s[i] == s[i + 1]) {
+          resultStartIdx = i;
+          resultEndIdx = i + 1;
+          dp[i][i + 1] = true;
+        }
+      }
+
+      for (int depth = 2; depth < size; ++depth) {
+        for (int i = 0; i < size - depth; ++i) {
+          dp[i][i + depth] = (s[i] == s[i + depth]) && dp[i + 1][i + depth -1];
+          if (dp[i][i + depth]) {
+            resultStartIdx = i;
+            resultEndIdx = i + depth;
+          }
+        }
+      }
+
+      return s.substr(resultStartIdx, resultEndIdx);
+    }
+};
