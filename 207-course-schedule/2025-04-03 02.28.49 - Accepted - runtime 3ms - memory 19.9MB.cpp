b7818155152bc9eeaaed9b16366ddb3baed13c70
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-03 02:28:49 -0400

    Attempt: 2025-04-03 02.28.49 - Accepted - runtime 3ms - memory 19.9MB.cpp

---
+++ 207-course-schedule/2025-04-03 02.28.49 - Accepted - runtime 3ms - memory 19.9MB.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    unordered_map<int, vector<int>> adjList;
+    bool util(int course, vector<bool>& visited, vector<bool>& inStack) {
+      if (inStack[course]) return false;
+      if (visited[course]) return true;
+      visited[course] = true;
+      inStack[course] = true;
+      
+      for (const auto& depCourse : adjList[course]) {
+        if (!util(depCourse, visited, inStack)) {
+          return false;
+        }
+      }
+
+      inStack[course] = false;
+      return true;
+    }
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+      for (const auto& dep : prerequisites) {
+        auto [it, success] = adjList.try_emplace(dep[0], vector<int>());
+        it->second.emplace_back(dep[1]);
+      }
+
+      vector<bool> visited(numCourses, false);
+      vector<bool> inStack(numCourses, false);
+      for (int i = 0; i < numCourses; ++i) {
+        if (!util(i, visited, inStack)) return false;
+      }
+      return true;
+    }
+};
