bc92d1b3d7f06bbb5d3cd2a76fc3a2a5768f0f62
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-03 01:10:46 -0400

    Attempt: 2025-04-03 01.10.46 - Accepted - runtime 6ms - memory 15.6MB.cpp

---
+++ 1128-remove-all-adjacent-duplicates-in-string/2025-04-03 01.10.46 - Accepted - runtime 6ms - memory 15.6MB.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+       stack<char> st;
+
+       for (const auto& c : s) {
+        if (!st.empty() && st.top() == c) st.pop();
+        else st.emplace(c);
+       }
+      
+       string result;
+       
+       while (!st.empty()) {
+        result += (st.top());
+        st.pop();
+       }
+       reverse(result.begin(), result.end());
+       return result;
+    }
+};
