d1bdc303c0c7ee2098a17b376cf02e05b601b8cd
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-03 02:04:41 -0400

    Attempt: 2025-04-03 02.04.41 - Time Limit Exceeded - runtime NA - memory NA.cpp

---
+++ 207-course-schedule/2025-04-03 02.04.41 - Time Limit Exceeded - runtime NA - memory NA.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    unordered_map<int, vector<int>> adjList;
+    bool util(int course, vector<bool>& visited) {
+      if (visited[course]) return false;
+      visited[course] = true;
+      
+      for (const auto& depCourse : adjList[course]) {
+        if (!util(depCourse, visited)) {
+          cout << "original course: " << course << ", dependent course: " << depCourse << endl;
+          return false;
+        }
+      }
+
+      visited[course] = false;
+      return true;
+    }
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+      for (const auto& dep : prerequisites) {
+        auto [it, success] = adjList.try_emplace(dep[0], vector<int>());
+        it->second.emplace_back(dep[1]);
+        cout << dep[0] << "->" << dep[1] << endl;
+      }
+
+      for (int i = 0; i < numCourses; ++i) {
+        vector<bool> visited(numCourses);
+        cout << i << endl;
+        if (!util(i, visited)) return false;
+      }
+      return true;
+    }
+};
