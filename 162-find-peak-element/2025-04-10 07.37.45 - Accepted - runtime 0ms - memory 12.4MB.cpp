37f9a24b4d8132dd631c862219fbcf112b8bdc55
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-10 07:37:45 -0400

    Attempt: 2025-04-10 07.37.45 - Accepted - runtime 0ms - memory 12.4MB.cpp

---
+++ 162-find-peak-element/2025-04-10 07.37.45 - Accepted - runtime 0ms - memory 12.4MB.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+      int size = nums.size();
+      int left = 0, right = size - 1;
+
+      while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        auto isLeftSmaller = mid == 0 || nums[mid - 1] < nums[mid];
+        auto isRightSmaller = mid == size - 1 || nums[mid + 1] < nums[mid];
+
+        if (isLeftSmaller && isRightSmaller) return mid;
+        else if (!isLeftSmaller) right = mid - 1; // go left
+        else if (!isRightSmaller) left = mid + 1; // go right
+      }
+
+      return -1;
+    }
+};
