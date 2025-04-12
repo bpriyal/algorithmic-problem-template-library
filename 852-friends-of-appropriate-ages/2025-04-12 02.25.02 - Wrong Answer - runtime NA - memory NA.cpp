00325135c8f6284fdb55c47b3a8537c80b4072d0
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-12 02:25:02 -0400

    Attempt: 2025-04-12 02.25.02 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 852-friends-of-appropriate-ages/2025-04-12 02.25.02 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numFriendRequests(vector<int>& ages) {
+      vector<int> count(121, 0);
+      for (const auto& age : ages) ++count[age];
+      int result = 0;
+
+      for (int ageA = 0; ageA < 121; ++ageA) {
+        for (int ageB = 0; ageB < 121; ++ageB) {
+          if (ageA == ageB) {
+            result += count[ageA] * (count[ageA] - 1);
+          } else {
+            if (ageB <= 0.5 * ageA + 7) continue;
+            if (ageB > ageA) continue;
+            if (ageB > 100 && ageA < 100) continue;
+            result += count[ageA] * count[ageB];
+          }
+        }
+      }
+
+      return result;
+    }
+};
