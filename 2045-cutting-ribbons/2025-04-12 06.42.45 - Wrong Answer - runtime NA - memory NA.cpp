04fe63c5b6bf1ec323920d8f3e97064c0b354bc8
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-12 06:42:45 -0400

    Attempt: 2025-04-12 06.42.45 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 2045-cutting-ribbons/2025-04-12 06.42.45 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    int size;
+    bool isPossible(const vector<int>& ribbons, int k, int x) {
+      int ribbonsOfSizeX = 0;
+      for (const int& ribbon : ribbons) {
+        ribbonsOfSizeX += ribbon / x;
+      }
+      return ribbonsOfSizeX >= k;
+    }
+public:
+    int maxLength(vector<int>& ribbons, int k) {
+      size = ribbons.size();
+      int left = 0, right = *max_element(ribbons.begin(), ribbons.end());
+
+      while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (mid == 0) return 0;
+        if (isPossible(ribbons, k, mid)) left = mid + 1;
+        else right = mid - 1;
+      }
+      return left - 1;
+    }
+};
