80fcf3a2d4e4da0f4e969ffa23bcdb97d675a6fc
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-12 14:46:57 -0400

    Attempt: 2025-04-12 14.46.57 - Accepted - runtime 15ms - memory 61.5MB.cpp

---
+++ 215-kth-largest-element-in-an-array/2025-04-12 14.46.57 - Accepted - runtime 15ms - memory 61.5MB.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+      int size = nums.size();
+      priority_queue<int, vector<int>, greater<int>> pq;
+
+      for (int num : nums) {
+        if (pq.size() < k) pq.emplace(num);
+        else {
+          if (pq.top() < num) {
+            pq.pop();
+            pq.emplace(num);
+          }
+        }
+      }
+      return pq.top();
+    }
+};
