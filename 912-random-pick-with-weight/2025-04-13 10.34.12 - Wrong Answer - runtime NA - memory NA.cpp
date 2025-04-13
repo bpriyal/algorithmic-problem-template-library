d6963d826ea77c6105fa561f9c8f10ba01861879
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-13 10:34:12 -0400

    Attempt: 2025-04-13 10.34.12 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 912-random-pick-with-weight/2025-04-13 10.34.12 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    vector<int> prefixSum;
+public:
+    Solution(vector<int>& w) {
+      for (const int& num : w) {
+        if (prefixSum.empty()) prefixSum.emplace_back(num);
+        else prefixSum.emplace_back(prefixSum.back() + num);
+      }
+    }
+    
+    int pickIndex() {
+       int randomNumber = (rand() % prefixSum.back());
+        cout << randomNumber << endl;
+       for (int i = prefixSum.size() - 1; i >= 0; --i) {
+        if (randomNumber <= prefixSum[i]) return i;
+       }
+       return -1;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(w);
+ * int param_1 = obj->pickIndex();
+ */
