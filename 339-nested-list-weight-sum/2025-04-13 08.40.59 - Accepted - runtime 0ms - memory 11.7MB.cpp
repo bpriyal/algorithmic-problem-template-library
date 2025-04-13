49ae78d69d75a7fb9e7cb2855520f85e96f205af
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-13 08:40:59 -0400

    Attempt: 2025-04-13 08.40.59 - Accepted - runtime 0ms - memory 11.7MB.cpp

---
+++ 339-nested-list-weight-sum/2025-04-13 08.40.59 - Accepted - runtime 0ms - memory 11.7MB.cpp
@@ -0,0 +1,48 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Constructor initializes an empty nested list.
+ *     NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     NestedInteger(int value);
+ *
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     void add(const NestedInteger &ni);
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+class Solution {
+    void util(const NestedInteger& nestedInteger, int depth, int& sum) {
+      if (nestedInteger.isInteger()) sum += depth * nestedInteger.getInteger();
+      else {
+        for (const auto& nestedInt : nestedInteger.getList()) {
+          util(nestedInt, depth + 1, sum);
+        }
+      }
+    }
+public:
+    int depthSum(vector<NestedInteger>& nestedList) {
+      int size = nestedList.size();
+      int sum = 0;
+      for (const auto& nestedInt : nestedList) {
+        util(nestedInt, 1, sum);
+      }
+      return sum;
+    }
+};
