b6464e94890ca5bc9df1fa7997c09ec9cf00150c
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-16 04:05:18 -0400

    Attempt: 2025-04-16 04.05.18 - Accepted - runtime 0ms - memory 17.9MB.cpp

---
+++ 347-top-k-frequent-elements/2025-04-16 04.05.18 - Accepted - runtime 0ms - memory 17.9MB.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+      int size = nums.size();
+      unordered_map<int, int> numToFreqMap;
+      for (int num : nums) ++numToFreqMap[num];
+
+    //   auto comp = [&](int num1, int num2) -> bool {
+    //     return numToFreqMap[num1] > numToFreqMap[num2];
+    //   };
+    //   priority_queue<int, vector<int>, decltype(comp)> pq(comp);
+
+    //   for (const auto& [num, freq] : numToFreqMap) {
+    //     if (pq.size() < k) pq.emplace(num);
+    //     else {
+    //       if (numToFreqMap[pq.top()] < freq) {
+    //         pq.pop();
+    //         pq.emplace(num);
+    //       }
+    //     }
+    //   }
+
+    //   vector<int> result;
+    //   while (!pq.empty()) {
+    //     result.emplace_back(pq.top());
+    //     pq.pop();
+    //   }
+
+    //   return result;
+    // }
+
+      vector<int> uniqueNums;
+      vector<int> result;
+      for (const auto& [num, _] : numToFreqMap) uniqueNums.emplace_back(num);
+
+      int l = 0, r = uniqueNums.size() - 1;
+
+      while (l <= r) {
+        int random = l + (rand() % (r - l + 1));
+        swap(uniqueNums[random], uniqueNums[r]);
+        int inflection = l;
+        for (int i = l; i < r; ++i) {
+          if (numToFreqMap[uniqueNums[i]] >= numToFreqMap[uniqueNums[r]]) {
+            swap(uniqueNums[i], uniqueNums[inflection]);
+            ++inflection;
+          }
+        }
+        swap(uniqueNums[inflection], uniqueNums[r]);
+        if (inflection == k - 1) return vector<int>(uniqueNums.begin(), uniqueNums.begin() + k);
+        else if (inflection < k) l = inflection + 1;
+        else r = inflection - 1;
+      }
+      return vector<int>();
+    }
+
+};
+
+// O(N) + O(NLogK)
