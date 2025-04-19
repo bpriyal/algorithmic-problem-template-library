8fd14eea1d7c1040d87e1ddcb6fb79be648db04b
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-19 08:58:37 -0400

    Attempt: 2025-04-19 08.58.37 - Accepted - runtime 0ms - memory 65.1MB.cpp

---
+++ 975-range-sum-of-bst/2025-04-19 08.58.37 - Accepted - runtime 0ms - memory 65.1MB.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int sum = 0;
+    void util(TreeNode* root, int low, int high) {
+      if (!root) return;
+      if (root->val >= low && root->val <= high) sum += root->val;
+      if (root->val >= low) util(root->left, low, high);
+      if (root->val <= high) util(root->right, low, high);
+    }
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+      util(root, low, high);
+      return sum;
+    }
+};
