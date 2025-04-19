0b885d1f190493d347e7b0685e599aa94cb710e4
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-19 05:59:25 -0400

    Attempt: 2025-04-19 05.59.25 - Accepted - runtime 0ms - memory 8.1MB.cpp

---
+++ 807-custom-sort-string/2025-04-19 05.59.25 - Accepted - runtime 0ms - memory 8.1MB.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string customSortString(string order, string s) {
+      string result;
+      vector<int> cache(26, 0);
+      for (char c : s) ++cache[c - 'a'];
+
+      for (char c : order) {
+        for (int i = 0; i < cache[c - 'a']; ++i) result += c;
+        cache[c - 'a'] = 0;
+      }
+
+      for (int i = 0; i < 26; ++i) {
+        for (int j = 0; j < cache[i]; ++j) result += ('a' + i);
+        cache[i] = 0;
+      }
+      return result;
+;    }
+};
