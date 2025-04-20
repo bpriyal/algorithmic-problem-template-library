187de35835d226ef92aee02a13d6e6795380461a
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-20 09:08:58 -0400

    Attempt: 2025-04-20 09.08.58 - Accepted - runtime 0ms - memory 19.3MB.cpp

---
+++ 207-course-schedule/2025-04-20 09.08.58 - Accepted - runtime 0ms - memory 19.3MB.cpp
@@ -0,0 +1,38 @@
+class Solution {
+    vector<vector<int>> adjMap;
+    vector<int> visited;
+    bool dfs(int i) {
+      visited[i] = 1;
+      for (int course : adjMap[i]) {
+        if (visited[course] == 2) continue;
+        else if (visited[course] == 1) return false;// in current stack
+        if (!dfs(course)) return false;
+      }
+      visited[i] = 2;
+      return true;
+    }
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+      adjMap.resize(numCourses);
+      visited.resize(numCourses, 0);
+      for (const auto& prereq : prerequisites) {
+        adjMap[prereq[1]].emplace_back(prereq[0]);
+      }
+
+      for (int i = 0; i < numCourses; ++i) {
+        if (!visited[i]) {
+          if (!dfs(i)) return false;
+        }
+      }
+      return true;
+    }
+};
+
+// 0 -> 1
+// 1 -> 0
+
+// [1, 2] [0, 1]
+
+// 0
+// 1 -> 0
+// 2 -> 1
