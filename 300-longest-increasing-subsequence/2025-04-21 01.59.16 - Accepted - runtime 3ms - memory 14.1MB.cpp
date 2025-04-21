3389804801c35a4f913494a4f9a90246eb625c77
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-21 01:59:16 -0400

    Attempt: 2025-04-21 01.59.16 - Accepted - runtime 3ms - memory 14.1MB.cpp

---
+++ 300-longest-increasing-subsequence/2025-04-21 01.59.16 - Accepted - runtime 3ms - memory 14.1MB.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+      int size = nums.size();
+      // vector<int> dp(size, 1);
+      // int result = dp[0];
+
+      // for (int i = 1; i < size; ++i) {
+      //   for (int j = 0; j < i; ++j) {
+      //     if (nums[i] > nums[j]) dp[i] = max(dp[i], dp[j] + 1);
+      //   }
+      //   result = max(result, dp[i]);
+      // }
+      // return result;
+
+      vector<int> subsequence;
+      subsequence.emplace_back(nums[0]);
+
+      for (int i = 1; i < size; ++i) {
+        if (nums[i] > subsequence.back()) {
+          subsequence.emplace_back(nums[i]);
+        } else {
+          auto it = lower_bound(subsequence.begin(), subsequence.end(), nums[i]);
+          if (it != subsequence.end()) *it = nums[i];
+          // for (int j = 0; j < subsequence.size(); ++j) {
+          //   if (subsequence[j] >= nums[i]) {
+          //     subsequence[j] = nums[i];
+          //     break;
+          //   }
+          // }
+        }
+      }
+      return subsequence.size();
+    }
+};
