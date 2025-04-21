2574cc6f3def41c7eec3b4f4feddc1d533b71e07
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-21 14:25:15 -0400

    Attempt: 2025-04-21 14.25.15 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 354-russian-doll-envelopes/2025-04-21 14.25.15 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    int binarySearch(const vector<vector<int>>& envelopes, const vector<int>& target) {
+      int size = envelopes.size();
+      int left = 0, right = size - 1;
+
+      while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (envelopes[mid][1] >= target[1]) right = mid - 1;
+        else left = right + 1;
+      }
+      return left;
+    }
+public:
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+      int size = envelopes.size();
+      vector<vector<int>> sub;
+      sort(envelopes.begin(), envelopes.end());
+      sub.emplace_back(envelopes[0]);
+
+      for (int i = 1; i < size; ++i) {
+        if (envelopes[i][1] > sub.back()[1]) {
+          sub.emplace_back(envelopes[i]);
+        } else {
+          auto idx = binarySearch(envelopes, envelopes[i]);
+          sub[idx] = envelopes[i];
+        }
+      }
+      return sub.size();
+    }
+};
+
+// [[5,4],[6,4],[6,7],[2,3]]
+
+// 1 1 2 2 3 3
+
