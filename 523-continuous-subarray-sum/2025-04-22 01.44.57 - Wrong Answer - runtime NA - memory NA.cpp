4f3b13c9c0f293ff6637c2eb50b34cb2a8359a06
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-22 01:44:57 -0400

    Attempt: 2025-04-22 01.44.57 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 523-continuous-subarray-sum/2025-04-22 01.44.57 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+      unordered_map<int, int> modToIdxMap;
+      modToIdxMap.emplace(0, -1);
+      int runModSum = 0;
+      for (int i = 0; i < nums.size(); ++i) {
+        runModSum = (runModSum + nums[i]) % k;
+        auto it = modToIdxMap.find(runModSum);
+        if (it != modToIdxMap.end() && i - it->second > 2) return true;
+        modToIdxMap.emplace(runModSum, i); 
+      }
+      return false;
+    }
+};
