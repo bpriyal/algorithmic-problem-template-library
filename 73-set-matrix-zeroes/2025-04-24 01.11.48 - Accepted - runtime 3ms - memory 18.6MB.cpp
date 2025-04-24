f23b1d9117b881e4f586e17144879988fd69c8de
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-24 01:11:48 -0400

    Attempt: 2025-04-24 01.11.48 - Accepted - runtime 3ms - memory 18.6MB.cpp

---
+++ 73-set-matrix-zeroes/2025-04-24 01.11.48 - Accepted - runtime 3ms - memory 18.6MB.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+      int rows = matrix.size();
+      int cols = matrix[0].size();
+
+      bool isFirstRowZero = false;
+      bool isFirstColZero = false;
+      for (int i = 0; i < cols; ++i) if (matrix[0][i] == 0) isFirstRowZero = true;
+      for (int i = 0; i < rows; ++i) if (matrix[i][0] == 0) isFirstColZero = true;
+
+      for (int r = 1; r < rows; ++r) {
+        for (int c = 1; c < cols; ++c) {
+          if (matrix[r][c] == 0) {
+            matrix[r][0] = 0;
+            matrix[0][c] = 0;
+          }
+        }
+      }
+      cout << "here1" << endl;
+
+      for (int i = 1; i < cols; ++i) {
+        if (matrix[0][i] == 0) {
+          for (int j = 1; j < rows; ++j) matrix[j][i] = 0;
+        }
+      }
+      cout << "here2" << endl;
+
+      for (int i = 1; i < rows; ++i) {
+        if (matrix[i][0] == 0) {
+          for (int j = 1; j < cols; ++j) matrix[i][j] = 0;
+        }
+      }
+      cout << "here3" << endl;
+
+      if (isFirstRowZero) {
+        for (int i = 0; i < cols; ++i) matrix[0][i] = 0;
+      }
+      cout << "here4" << endl;
+
+      if (isFirstColZero) {
+        for (int i = 0; i < rows; ++i) matrix[i][0] = 0;
+      }
+      cout << "here5" << endl;
+    }
+};
