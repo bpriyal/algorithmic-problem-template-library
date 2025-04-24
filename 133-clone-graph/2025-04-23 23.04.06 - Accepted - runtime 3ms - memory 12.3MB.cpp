bea300908e6442d992c1aa2eda76cca7bbc29f12
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-23 23:04:06 -0400

    Attempt: 2025-04-23 23.04.06 - Accepted - runtime 3ms - memory 12.3MB.cpp

---
+++ 133-clone-graph/2025-04-23 23.04.06 - Accepted - runtime 3ms - memory 12.3MB.cpp
@@ -0,0 +1,37 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+    unordered_map<Node*, Node*> oldToNewNodeMap;
+    Node* util(Node* node) {
+      if (!node) return nullptr;
+      if (auto it = oldToNewNodeMap.find(node); it != oldToNewNodeMap.end()) return it->second;
+
+      auto newNode = new Node(node->val);
+      oldToNewNodeMap.emplace(node, newNode);
+      for (const auto neighbor : node->neighbors) newNode->neighbors.emplace_back(util(neighbor));
+      return newNode;
+    }
+public:
+    Node* cloneGraph(Node* node) {
+      return util(node);
+    }
+};
