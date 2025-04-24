7dc6d6bddc818df3da632f62d2abe7a4ba76f8ef
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-23 23:29:26 -0400

    Attempt: 2025-04-23 23.29.26 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 415-add-strings/2025-04-23 23.29.26 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+      int size1 = num1.size();
+      int size2 = num2.size();
+      int i = size1 - 1, j = size2 - 1;
+      int carry = 0;
+      int result = 0;
+      int factor = 1;
+
+      while (i >= 0 && j >= 0) {
+        int sum =  (num1[i] - '0') + (num2[j] - '0') + carry;
+        result = (sum % 10) * factor + result;
+        factor *= 10;
+        carry = sum / 10;
+        --i;
+        --j;
+      }
+
+      while (i >= 0) {
+        int sum =  (num1[i] - '0') + carry;
+        result = (sum % 10) * factor + result;
+        factor *= 10;
+        carry = sum / 10;
+        --i;
+      }
+
+      while (j >= 0) {
+        int sum =  (num2[j] - '0') + carry;
+        result = (sum % 10) * factor + result;
+        factor *= 10;
+        carry = sum / 10;
+        --j;
+      }
+
+      return to_string(result);
+    }
+};
