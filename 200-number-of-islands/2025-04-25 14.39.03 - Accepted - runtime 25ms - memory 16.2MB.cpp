a72fc7597e08052e9380e6d3ebe57568c0f46564
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-25 14:39:03 -0400

    Attempt: 2025-04-25 14.39.03 - Accepted - runtime 25ms - memory 16.2MB.cpp

---
+++ 200-number-of-islands/2025-04-25 14.39.03 - Accepted - runtime 25ms - memory 16.2MB.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    int m, n;
+    void dfs(vector<vector<char>>& grid, int row, int col) {
+      if (row < 0 || row == m || col < 0 || col == n || grid[row][col] != '1') return;
+      grid[row][col] = '2';
+      dfs(grid, row + 1, col);
+      dfs(grid, row - 1, col);
+      dfs(grid, row, col + 1);
+      dfs(grid, row, col - 1);
+    }
+public:
+    int numIslands(vector<vector<char>>& grid) {
+      int count = 0;
+      m = grid.size();
+      n = grid[0].size();
+      for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+          if (grid[i][j] == '1') {
+            dfs(grid, i, j);
+            ++count;
+          }
+        }
+      }
+      return count;
+    }
+};
