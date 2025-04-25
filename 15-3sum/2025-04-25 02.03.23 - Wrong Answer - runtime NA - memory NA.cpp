3d7a8622c39c5c32ae3745b818b0d247167b1729
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-25 02:03:23 -0400

    Attempt: 2025-04-25 02.03.23 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 15-3sum/2025-04-25 02.03.23 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+      int size = nums.size();
+      sort(nums.begin(), nums.end());
+      vector<vector<int>> result;
+
+      for (int i = 0; i < size - 2; ++i) {
+        int j = i + 1;
+        int k = size - 1;
+        if (nums[i] <= 0 && (i == 0 || nums[i - 1] != nums[i])) {
+          while (j < k) {
+            int sum = nums[i] + nums[j] + nums[k];
+            if (sum == 0) {
+              result.emplace_back(vector<int>{nums[i], nums[j], nums[k]});
+              ++j;
+              --k;
+            }
+            else if (sum < 0) ++j;
+            else --k;
+          }
+        }
+      }
+      return result;
+    }
+};
