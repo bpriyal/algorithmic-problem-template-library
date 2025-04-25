578df4d4af47a048497db22ce985dd59ff64dc62
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-25 00:52:42 -0400

    Attempt: 2025-04-25 00.52.42 - Accepted - runtime 11ms - memory 13.2MB.cpp

---
+++ 850-insert-into-a-sorted-circular-linked-list/2025-04-25 00.52.42 - Accepted - runtime 11ms - memory 13.2MB.cpp
@@ -0,0 +1,64 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+    }
+
+    Node(int _val, Node* _next) {
+        val = _val;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* insert(Node* head, int insertVal) {
+      Node* temp = head;
+      // Empty list
+      if (!temp) {
+        auto node = new Node(insertVal);
+        node->next = node;
+        return node;
+      }
+      // single node list
+      if (temp->next == temp) {
+        auto node = new Node(insertVal);
+        node->next = temp;
+        temp->next = node;
+        return temp;
+      }
+      // 2 or more
+      temp = head->next;
+      while (temp != head) {
+        if (temp->next->val >= temp->val) {
+          if (temp->val <= insertVal && insertVal <= temp->next->val) {
+            auto node = new Node(insertVal);
+            node->next = temp->next;
+            temp->next = node;
+            return head;
+          }
+        } else {
+          if (temp->val <= insertVal || insertVal <= temp->next->val) {
+            auto node = new Node(insertVal);
+            node->next = temp->next;
+            temp->next = node;
+            return head;
+          }
+        }
+        temp = temp->next;
+      }
+      auto node = new Node(insertVal);
+      node->next = temp->next;
+      temp->next = node;
+      return head;
+    }
+};
