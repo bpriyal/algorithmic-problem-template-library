8b4bfcc3cdbd42d42d34cb2d28405bfaec2e7b18
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-26 14:12:39 -0400

    Attempt: 2025-04-26 14.12.39 - Accepted - runtime 0ms - memory 13.3MB.cpp

---
+++ 74-search-a-2d-matrix/2025-04-26 14.12.39 - Accepted - runtime 0ms - memory 13.3MB.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    int rows, cols;
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+      rows = matrix.size();
+      cols = matrix[0].size();
+      // int top = 0, bottom = rows - 1;
+      // while (top <= bottom) {
+      //   int mid = top + (bottom - top) / 2;
+      //   if (target >= matrix[mid][0] && target <= matrix[mid][matrix[mid].size() - 1]) {
+      //     return binary_search(matrix[mid].begin(), matrix[mid].end(), target);
+      //   } else if (target < matrix[mid][0]) bottom = mid - 1;
+      //   else top = mid + 1;
+      // }
+      // return false;
+
+      int left = 0, right = rows * cols - 1;
+      while (left <= right) {
+        int mid = left + (right - left) / 2;
+        int row = mid / cols;
+        int col = mid % cols;
+        if (matrix[row][col] == target) return true;
+        else if (matrix[row][col] < target) left = mid + 1;
+        else right = mid - 1;
+      }
+      return false;
+    }
+};
