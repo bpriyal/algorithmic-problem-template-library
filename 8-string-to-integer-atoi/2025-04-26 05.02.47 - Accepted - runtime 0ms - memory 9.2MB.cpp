37e96ceb601a2a70d4d8301c908f49ecf8f667ef
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-26 05:02:47 -0400

    Attempt: 2025-04-26 05.02.47 - Accepted - runtime 0ms - memory 9.2MB.cpp

---
+++ 8-string-to-integer-atoi/2025-04-26 05.02.47 - Accepted - runtime 0ms - memory 9.2MB.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+      int size = s.size();
+      int number = 0;
+      bool isDigitFound = false;
+      bool isPositive = true;
+      bool isSignFound = false;
+      
+      for (int i = 0; i < size; ++i) {
+        if (iswspace(s[i])) {
+          if (isDigitFound || isSignFound) break;
+        } else if (s[i] == '+' || s[i] == '-') {
+          if (isDigitFound || isSignFound) break;
+          else isPositive = s[i] == '+';
+          isSignFound = true;
+        } else if (isdigit(s[i])) {
+          int incomingNumber = s[i] - '0';
+          if (number > INT_MAX / 10 || (number == INT_MAX / 10 && incomingNumber > INT_MAX % 10)) {
+            return isPositive ? INT_MAX : INT_MIN;
+          }
+          // if (isPositive) {
+          //   if (number > INT_MAX / 10 || (number == INT_MAX / 10 && incomingNumber > INT_MAX % 10)) return INT_MAX;
+          // } else {
+          //   cout << incomingNumber << ", " << INT_MIN % 10 << endl;
+          //   if (-number < INT_MIN / 10 || (number == INT_MIN / 10 && incomingNumber > abs(INT_MIN % 10))) return INT_MIN;
+          // }
+          
+          number = number * 10 + (s[i] - '0');
+          isDigitFound = true;
+        } else break;
+      }
+
+      return number * (isPositive ? 1 : -1);
+    }
+};
