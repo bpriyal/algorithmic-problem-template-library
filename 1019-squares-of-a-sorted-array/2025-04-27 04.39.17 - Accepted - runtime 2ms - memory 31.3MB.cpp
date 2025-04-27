f8aacbfaf4e1cab568e4dd63010d0846322a18d5
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-27 04:39:17 -0400

    Attempt: 2025-04-27 04.39.17 - Accepted - runtime 2ms - memory 31.3MB.cpp

---
+++ 1019-squares-of-a-sorted-array/2025-04-27 04.39.17 - Accepted - runtime 2ms - memory 31.3MB.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+      int size = nums.size();
+      vector<int> result;
+      int firstZeroOrPositiveIndex;
+      for (int i = 0; i < size; ++i) {
+        if (nums[i] >= 0) {
+          firstZeroOrPositiveIndex = i;
+          break;
+        }
+      }
+      int negPtr = firstZeroOrPositiveIndex - 1;
+      while (negPtr >= 0 || firstZeroOrPositiveIndex < size) {
+        int candidate1 = negPtr >= 0 ? nums[negPtr] * nums[negPtr] : INT_MAX;
+        int candidate2 = firstZeroOrPositiveIndex < size ? nums[firstZeroOrPositiveIndex] * nums[firstZeroOrPositiveIndex] : INT_MAX;
+        if (candidate1 <= candidate2) {
+          --negPtr;
+          result.emplace_back(candidate1);
+        } else {
+          ++firstZeroOrPositiveIndex;
+          result.emplace_back(candidate2);
+        }
+      }
+      return result;
+    }
+};
+
+// negPtr = 1, first = 2
