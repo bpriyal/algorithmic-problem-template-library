d8d86ba7e7e87d7c5cad3c9c02e806b73ee3b72d
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-28 23:18:12 -0400

    Attempt: 2025-04-28 23.18.12 - Accepted - runtime 0ms - memory 23.8MB.cpp

---
+++ 283-move-zeroes/2025-04-28 23.18.12 - Accepted - runtime 0ms - memory 23.8MB.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+      int size = nums.size();
+      int zeroIdx = 0;
+      for (int i = 0; i < size; ++i) {
+        if (nums[i] != 0) {
+          swap(nums[i], nums[zeroIdx]);
+          ++zeroIdx;
+        }
+      }
+    }
+};
+
+// 0 1 0 3 12
+// 1 0 0 3 12
+// 1 3 0 0 12
+// 1 3 12 
+
+//  1 2 3
