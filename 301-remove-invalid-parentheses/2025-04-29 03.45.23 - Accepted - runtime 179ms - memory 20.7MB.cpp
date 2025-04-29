7d1395ca716a1d0294f9f893588f67ed1462b1c4
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-29 03:45:23 -0400

    Attempt: 2025-04-29 03.45.23 - Accepted - runtime 179ms - memory 20.7MB.cpp

---
+++ 301-remove-invalid-parentheses/2025-04-29 03.45.23 - Accepted - runtime 179ms - memory 20.7MB.cpp
@@ -0,0 +1,43 @@
+class Solution {
+    unordered_set<string> result;
+    int minRemoved = INT_MAX;
+    int size;
+    void recurse(const string& s, int i, int leftCount, int rightCount, int remCount, string& runStr) {
+      if (i == size) {
+        if (leftCount == rightCount) {
+          if (remCount < minRemoved) {
+            // cout << remCount << ", minRemoved: " << minRemoved << endl;
+            minRemoved = remCount;
+            result.clear();
+            result.emplace(runStr);
+          } else if (remCount == minRemoved) result.emplace(runStr);
+        }
+      } else {
+          if (s[i] == '(') {
+            recurse(s, i + 1, leftCount, rightCount, remCount + 1, runStr);
+            runStr += s[i];
+            recurse(s, i + 1, leftCount + 1, rightCount, remCount, runStr);
+            runStr.pop_back();
+          } else if (s[i] == ')') {
+            recurse(s, i + 1, leftCount, rightCount, remCount + 1, runStr);
+            if (leftCount > rightCount) {
+              runStr += s[i];
+              recurse(s, i + 1, leftCount, rightCount + 1, remCount, runStr);
+              runStr.pop_back();
+            }
+          } else {
+            runStr += s[i];
+            recurse(s, i + 1, leftCount, rightCount, remCount, runStr);
+            runStr.pop_back();
+          }
+      }
+    }
+public:
+    vector<string> removeInvalidParentheses(string s) {
+      size = s.size();
+      string runStr;
+      recurse(s, 0, 0, 0, 0, runStr);
+      vector<string> res(result.begin(), result.end());
+      return res;
+    }
+};
