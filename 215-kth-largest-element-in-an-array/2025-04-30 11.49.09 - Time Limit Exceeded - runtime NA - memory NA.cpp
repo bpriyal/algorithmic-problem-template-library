dd2e4a4f9560a5418ddbd07047a65f0adbe04693
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-30 11:49:09 -0400

    Attempt: 2025-04-30 11.49.09 - Time Limit Exceeded - runtime NA - memory NA.cpp

---
+++ 215-kth-largest-element-in-an-array/2025-04-30 11.49.09 - Time Limit Exceeded - runtime NA - memory NA.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+      // priority_queue<int, vector<int>, greater<int>> pq;
+      // for (int num : nums) {
+      //   if (pq.size() < k) pq.emplace(num);
+      //   else {
+      //     if (pq.top() < num) {
+      //       pq.pop();
+      //       pq.emplace(num);
+      //     }
+      //   }
+      // }
+      // return pq.top();
+
+      int left = 0, right = nums.size() - 1;
+      while (left <= right) {
+        int pivotIdx = left + (rand() % (right - left + 1));
+        swap(nums[pivotIdx], nums[right]);
+        int inflectionIdx = left;
+        for (int i = left; i < right; ++i) {
+          if (nums[i] <= nums[right]) {
+            swap(nums[inflectionIdx], nums[i]);
+            ++inflectionIdx;
+          }
+        }
+        swap(nums[inflectionIdx], nums[right]);
+        cout << "pivotIdx: " << pivotIdx << "inflection: " << inflectionIdx << endl;
+        for (int num : nums) cout << num << "\t";
+        cout << endl;
+        if (inflectionIdx == nums.size() - k) return nums[inflectionIdx];
+        else if (inflectionIdx < nums.size() - k) left = inflectionIdx + 1;
+        else right = inflectionIdx - 1;
+      }
+      return -1;
+    }
+};
+
+// [3,2,1,5,6,4]
+// [1,2,3,4,5,6]
+// 6 - 2 -1 = 3
