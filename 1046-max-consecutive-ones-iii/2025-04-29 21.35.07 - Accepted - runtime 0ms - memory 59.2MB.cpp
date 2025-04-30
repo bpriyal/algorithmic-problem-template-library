25359f048fc8d723aef46e72e380a2ae892175dd
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-04-29 21:35:07 -0400

    Attempt: 2025-04-29 21.35.07 - Accepted - runtime 0ms - memory 59.2MB.cpp

---
+++ 1046-max-consecutive-ones-iii/2025-04-29 21.35.07 - Accepted - runtime 0ms - memory 59.2MB.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+      int size = nums.size();
+      int left = 0, right = 0;
+      int result = INT_MIN;
+
+      for (right = 0; right < size; ++right) {
+        if (nums[right] == 0) {
+          --k;
+        }
+        while (k < 0) {
+          k += 1 - nums[left];
+          ++left;
+        }
+        result = max(result, right - left + 1);
+      }
+      return result;
+    }
+};
