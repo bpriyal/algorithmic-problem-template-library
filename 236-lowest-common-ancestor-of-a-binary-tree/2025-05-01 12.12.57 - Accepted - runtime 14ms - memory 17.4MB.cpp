099faa06ad60d05712844bcdb3c9e18fc30b6b27
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-05-01 12:12:57 -0400

    Attempt: 2025-05-01 12.12.57 - Accepted - runtime 14ms - memory 17.4MB.cpp

---
+++ 236-lowest-common-ancestor-of-a-binary-tree/2025-05-01 12.12.57 - Accepted - runtime 14ms - memory 17.4MB.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    TreeNode* lca = nullptr;
+    int util(TreeNode* root, TreeNode* p, TreeNode* q) {
+      if (!root) return 0;
+
+      int left = util(root->left, p, q);
+      int right = util(root->right, p, q);
+      int mid = root == p || root == q;
+
+      if (left + right + mid >= 2 && !lca) lca = root;
+
+      return left + right + mid > 0;
+    }
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+      util(root, p, q);
+      return lca;
+    }
+};
