0768b4f73b8b9b80c3b5742c1692997c2a1c926f
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-05-02 05:57:21 -0400

    Attempt: 2025-05-02 05.57.21 - Wrong Answer - runtime NA - memory NA.cpp

---
+++ 65-valid-number/2025-05-02 05.57.21 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isNumber(string s) {
+      int size = s.size();
+      bool seenDigit = false, seenDot = false, seenExp = false, seenSign = false;
+
+      for (int i = 0; i < size; ++i) {
+        char c = s[i];
+        if (isdigit(c)) {
+          seenDigit = true;
+        } else if (c == '-' || c == '+') {
+          if (seenDigit || seenSign) return false;
+          seenSign = true;
+        } else if (c == '.') {
+          if (seenExp) return false;
+          seenDot = true;
+        } else if (isalpha(c)) {
+          if (c != 'e' || c != 'E' || !seenDigit) return false;
+          seenExp = true;
+          seenDigit = false; // reset, there should be a new digit
+          seenDot = false;
+          seenSign = false;
+        }
+      }
+      return seenDigit;
+    }
+};
