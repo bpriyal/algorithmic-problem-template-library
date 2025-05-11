9e9c5f582d2139d01e979d069adfc7484d369aca
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-05-11 05:41:31 -0400

    Attempt: 2025-05-11 05.41.31 - Wrong Answer - runtime NA - memory NA.java

---
+++ 3-longest-substring-without-repeating-characters/2025-05-11 05.41.31 - Wrong Answer - runtime NA - memory NA.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> charToLastInd = new HashMap<>();
+        // s = s.trim();
+        if (s == null) {
+            return 0;
+        }
+        int max = 1;
+        int start = 0;
+        // int end = -1;
+        for (int i = 0; i < s.length(); ++i) {
+             char c = s.charAt(i);
+             if(!charToLastInd.containsKey(c)) {
+                charToLastInd.put(c,i);
+             } else {
+                int window = i - start;
+                if (max < window) {
+                    max = window;
+                }
+                start = charToLastInd.get(c) + 1;
+                charToLastInd.replace(c,i);
+                // charToLastInd.remove(c);
+             }
+        }
+        return max;
+    }
+}
