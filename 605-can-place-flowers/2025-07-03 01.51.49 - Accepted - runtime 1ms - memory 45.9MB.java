e2ae5a71227bb0378b7ae89f12d231f4c765a737
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-07-03 01:51:49 -0400

    Attempt: 2025-07-03 01.51.49 - Accepted - runtime 1ms - memory 45.9MB.java

---
+++ 605-can-place-flowers/2025-07-03 01.51.49 - Accepted - runtime 1ms - memory 45.9MB.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        boolean isContArr = false;
+        int nz = -1;
+        if (flowerbed.length == 0 && n > 0) return false;
+        if (flowerbed[0] == 0) {
+            nz = 0;
+            isContArr = true;
+        }
+        int total = 0;
+        int i;
+        for (i = 0; i < flowerbed.length - 1; ++i) {
+            if (flowerbed[i] == 0 && flowerbed[i + 1] == 0) {
+                isContArr = true;
+                nz++;
+            }
+            else if (isContArr) {
+                total += (nz + 1)/2;
+                nz = -1;
+                isContArr = false;
+            }
+            if (total >= n) return true;
+        }
+        if (isContArr) {
+            if (flowerbed[i] == 0) {
+                nz++;
+            }
+            total += (nz + 1)/2;
+        } 
+        return total >= n;
+    }
+}
+
+/*
+0 0 0 0 0 1 1 0 0 1 0 0 0 1 0 0 0 0 0 1 1 1 1
+*/
+// remove 1st and last in contiguous azeroes -- except edge cases
+// find max that can be placed in remaining
+// check edge cases later
