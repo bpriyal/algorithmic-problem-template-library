8ce974e932a7cf808a26bf131d8819820eae5977
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-07-05 07:53:35 -0400

    Attempt: 2025-07-05 07.53.35 - Runtime Error - runtime NA - memory NA.java

---
+++ 392-is-subsequence/2025-07-05 07.53.35 - Runtime Error - runtime NA - memory NA.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0;
+        if (s == null || t == null) return false;
+        if (s.length() == 0) return true;
+        if (t.length() == 0) return false;
+        for (char c : t.toCharArray()) {
+            if (s.charAt(i) == c) {
+                i++;
+            }
+        }
+        return (i == s.length());
+    }
+}
