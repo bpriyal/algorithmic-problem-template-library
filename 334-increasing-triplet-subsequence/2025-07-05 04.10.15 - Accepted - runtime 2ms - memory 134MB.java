8433eb883f338e4866ba397551941d3428638453
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-07-05 04:10:15 -0400

    Attempt: 2025-07-05 04.10.15 - Accepted - runtime 2ms - memory 134MB.java

---
+++ 334-increasing-triplet-subsequence/2025-07-05 04.10.15 - Accepted - runtime 2ms - memory 134MB.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int size = nums.length;
+        int min = nums[0];
+        int max = Integer.MIN_VALUE;
+        boolean maxSet = false;
+        
+        for (int i = 1; i < size; ++i) {
+            if (maxSet && nums[i] > max) {
+                return true;
+            }
+
+            if (nums[i] <= min) {
+                min = nums[i];
+            } else if (!maxSet) { // && nums[i] > max
+                max = nums[i];
+                maxSet = true;
+            } else if (nums[i] < max) {
+                max = nums[i];
+            }
+        }
+        return false;
+    }
+}
