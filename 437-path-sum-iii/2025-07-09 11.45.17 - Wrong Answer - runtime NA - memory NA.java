3d6187658e469d52c50df522cbc690163ea2ba95
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-07-09 11:45:17 -0400

    Attempt: 2025-07-09 11.45.17 - Wrong Answer - runtime NA - memory NA.java

---
+++ 437-path-sum-iii/2025-07-09 11.45.17 - Wrong Answer - runtime NA - memory NA.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int count = 0;
+    public int pathSum(TreeNode root, int targetSum) {
+        helper(root, targetSum, new ArrayDeque<Integer>(), 0);
+        return count;
+    }
+    void helper (TreeNode root, int target, ArrayDeque<Integer> q, int sum) {
+        int c = count;
+        if (root == null) return;
+        q.add(root.val);
+        sum += root.val;
+        if (sum == target) count++;
+        int s = sum;
+        Queue<Integer> copy = new ArrayDeque<Integer>(q);
+        while (copy.size() > 1) {
+            s -= copy.poll();
+            if (s == target) {
+                count++;
+            }
+        }
+        helper(root.left, target, q, sum);
+        if (root.left != null) q.pollLast();
+        helper(root.right, target, q, sum);
+        // q.pollLast();
+    }
+}
