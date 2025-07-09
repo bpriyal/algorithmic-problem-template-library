c6a6895681902116d85ca599d2eff07fc459de5e
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-07-09 02:53:03 -0400

    Attempt: 2025-07-09 02.53.03 - Wrong Answer - runtime NA - memory NA.java

---
+++ 2216-delete-the-middle-node-of-a-linked-list/2025-07-09 02.53.03 - Wrong Answer - runtime NA - memory NA.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = head;
+        while (fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            if (fast.next.next != null) {     
+                fast = fast.next.next;
+            } else {
+                break;
+            }
+        }
+        prev.next = slow.next;
+        slow = null;
+        return head;
+    }
+}
