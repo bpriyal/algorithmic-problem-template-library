da5e95567506e647bca887e22dc97a2b02fe6751
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-07-09 02:14:50 -0400

    Attempt: 2025-07-09 02.14.50 - Time Limit Exceeded - runtime NA - memory NA.java

---
+++ 649-dota2-senate/2025-07-09 02.14.50 - Time Limit Exceeded - runtime NA - memory NA.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String predictPartyVictory(String senate) {
+        Queue<Character> status = new ArrayDeque();
+        if (senate == null || senate.length() == 0) return "";
+        for (char c : senate.toCharArray()) status.add(c);
+        status.add('*');
+        int r = 0;
+        int d = 0;
+        while (status.size() > 1) {
+            char c = status.poll();
+            if (c == 'R') {
+                if (d > 0) d--;
+                else {
+                    r++;
+                    status.add('R');
+                }
+            } else if (c == 'D') {
+                if (r > 0) r--;
+                else {
+                    d++;
+                    status.add('D');
+                }
+            } else { //*
+                if (r == 0 && d > 0 && d == status.size()) return "Dire";
+                else if (d == 0 && r > 0 && r == status.size()) return "Radiant";
+                status.add('*');
+            }
+        }
+        if (status.size() == 1 && status.poll() == 'R') return "Radiant";
+        return "Dire";
+    }
+}
