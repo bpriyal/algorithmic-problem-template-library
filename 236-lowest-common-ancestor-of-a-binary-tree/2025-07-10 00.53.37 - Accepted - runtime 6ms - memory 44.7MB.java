d4d1a7e73e106d5c617d93d85da9b54bf7f581a7
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-07-10 00:53:37 -0400

    Attempt: 2025-07-10 00.53.37 - Accepted - runtime 6ms - memory 44.7MB.java

---
+++ 236-lowest-common-ancestor-of-a-binary-tree/2025-07-10 00.53.37 - Accepted - runtime 6ms - memory 44.7MB.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    TreeNode anc;
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        helper (root, p, q);
+        return anc;
+    }
+
+    boolean helper (TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return false;
+        boolean l = helper (root.left, p, q);
+        boolean r = helper (root.right, p , q);
+        if (l == r && l == true) {
+            anc = root;
+            return false;
+        }
+        if (root == p || root == q) {
+            if (l == true || r == true) {
+                anc = root;
+                return false;
+            }
+            return true;
+        }
+        return l || r;
+    }
+    
+    
+    
+}
