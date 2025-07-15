5c57bd5f57bd4813c249f5317a43f9933e6b5ff8
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-07-15 00:46:08 -0400

    Attempt: 2025-07-15 00.46.08 - Time Limit Exceeded - runtime NA - memory NA.java

---
+++ 747-min-cost-climbing-stairs/2025-07-15 00.46.08 - Time Limit Exceeded - runtime NA - memory NA.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        if (cost.length == 0) return 0;
+        if (cost.length == 1) return cost[0];
+        int s0 = helper (cost, 0, cost[0]);
+        int s1 = helper (cost, 1, cost[1]);
+        return Math.min(s0, s1);
+    }
+    int helper (int[] cost, int ind, int sumAtInd) {
+        if (ind > cost.length - 1) return sumAtInd;
+        int n1 = sumAtInd, n2 = sumAtInd;
+        n1 = (ind + 1 < cost.length) ? helper (cost, ind + 1, sumAtInd + cost[ind + 1]) : sumAtInd;
+        n2 = (ind + 2 < cost.length) ? helper (cost, ind + 2, sumAtInd + cost[ind + 2]) : sumAtInd;
+        return Math.min(n1, n2);
+    }
+}
