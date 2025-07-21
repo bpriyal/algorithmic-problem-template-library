eeca7ce7eff3a58a0c9580466111820fc4f0828f
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-07-21 10:51:33 -0400

    Attempt: 2025-07-21 10.51.33 - Wrong Answer - runtime NA - memory NA.java

---
+++ 408-valid-word-abbreviation/2025-07-21 10.51.33 - Wrong Answer - runtime NA - memory NA.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean validWordAbbreviation(String word, String abbr) {
+        // Character[] wordarr = new Character[word.length()](word.toCharArray());
+        // Character[] abbarr = new Character[abbr.length()];
+        int a = 0;
+        int num = 0;
+        for (int w = 0; w < word.length() && a < abbr.length();) {
+            if (Character.isLetter(abbr.charAt(a))) {
+                if (num != 0) {
+                     w += num/10;
+                    //  System.out.println(word.charAt(w));
+                     num = 0;
+                     continue;
+                }
+                if (abbr.charAt(a) != word.charAt(w)) return false;
+                else {
+                    ++a;
+                    ++w;
+                    continue;
+                }
+            }
+            num += Character.getNumericValue(abbr.charAt(a));
+            num *= 10;
+            ++a;
+            // ++w;
+        }
+        return true;
+    }
+}
