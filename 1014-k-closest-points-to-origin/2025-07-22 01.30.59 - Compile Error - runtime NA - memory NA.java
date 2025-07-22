c1d00faa94331deb1c2ad8efa658a898cfc1e5e7
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-07-22 01:30:59 -0400

    Attempt: 2025-07-22 01.30.59 - Compile Error - runtime NA - memory NA.java

---
+++ 1014-k-closest-points-to-origin/2025-07-22 01.30.59 - Compile Error - runtime NA - memory NA.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        Random random = new Random();
+        for (int i = 0 i < 10; ++i) {
+            int pivotInd = random.nextInt(point.length) - 1;
+            System.out.println(pivtoInd);
+        }
+        return null;
+
+    }
+}
