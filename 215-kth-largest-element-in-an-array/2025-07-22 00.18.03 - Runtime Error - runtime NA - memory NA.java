f7639b396dde2a6680683365be553523be9b818a
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-07-22 00:18:03 -0400

    Attempt: 2025-07-22 00.18.03 - Runtime Error - runtime NA - memory NA.java

---
+++ 215-kth-largest-element-in-an-array/2025-07-22 00.18.03 - Runtime Error - runtime NA - memory NA.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int i : nums) {
+            max = Math.max(max, i);
+            min = Math.min(min, i);
+        } 
+        if (max < 0) max = 0;
+        int n = min < 0 ? max - min + 1 : max;
+        if (min > 0) min = 1;
+        int count[] = new int[n];
+
+        for (int i : nums) {
+            count[i - min]++;
+        }
+        int i = n - 1;
+        for (i = n - 1; i >= 0; --i) {
+            k -= count[i];
+            if (k <= 0) break;
+        }
+        return i + min;
+    }
+}
