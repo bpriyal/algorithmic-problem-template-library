955147602bd55d06493c6d3e697495d4c9475214
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-07-22 09:08:53 -0400

    Attempt: 2025-07-22 09.08.53 - Wrong Answer - runtime NA - memory NA.java

---
+++ 1171-shortest-path-in-binary-matrix/2025-07-22 09.08.53 - Wrong Answer - runtime NA - memory NA.java
@@ -0,0 +1,42 @@
+class Solution {
+    int MAX = Integer.MAX_VALUE;
+    int MIN = Integer.MAX_VALUE;
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (grid[i][j] == 1) grid[i][j] = MAX;
+            }
+        }
+        dfs (grid, n - 1, m - 1, new boolean[n][m], 1);
+        MIN = MIN == MAX ? -1 : MIN;
+        return MIN;
+    }
+
+    void dfs (int[][] grid, int i, int j, boolean[][] visited, int count) {
+        int n = grid.length;
+        int m = grid[0].length;
+        if (i < 0 || i >= n || j < 0 || j >= m) return;
+        if (grid[i][j] == MAX) return;
+        if (i == 0 && j == 0) {
+            MIN = Math.min(MIN, count);
+            return;
+        }
+        if (visited[i][j]) return;
+        int[][] copy = new int[n][m];
+        copy = Arrays.stream(grid).map(int[]::clone).toArray(int[][]::new);
+        visited[i][j] = true;
+        // copy[i][j] = Math.min(copy[i][j], count);
+        dfs (copy, i - 1, j - 1, visited, count + 1);
+        dfs (copy, i, j - 1, visited, count + 1);
+        dfs (copy, i + 1, j - 1, visited, count + 1);
+        dfs (copy, i + 1, j, visited, count + 1);
+        dfs (copy, i + 1, j + 1, visited, count + 1);
+        dfs (copy, i, j + 1, visited, count + 1);
+        dfs (copy, i - 1, j + 1, visited, count + 1);
+        dfs (copy, i - 1, j, visited, count + 1);
+        // visited[i][j] = false;
+
+    }
+}
