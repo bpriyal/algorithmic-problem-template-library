2b4e6eecb4bf1ef9fe75419735e202e58b37238f
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-07-24 02:58:27 -0400

    Attempt: 2025-07-24 02.58.27 - Accepted - runtime 3ms - memory 42.7MB.java

---
+++ 807-custom-sort-string/2025-07-24 02.58.27 - Accepted - runtime 3ms - memory 42.7MB.java
@@ -0,0 +1,58 @@
+class Solution {
+    public String customSortString(String order, String s) {
+        StringBuilder sb = new StringBuilder(s);
+
+        int early = 0;
+        int later = order.length() - 1;
+        char earlyPivot = order.charAt(early);
+        char laterPivot = order.charAt(later);
+
+        while (early < order.length() - 1) {
+            while (early < later) {
+                int loe = sb.lastIndexOf(String.valueOf(earlyPivot));
+                int eol = sb.indexOf(String.valueOf(laterPivot));
+                if (loe == -1) {
+                    early++;
+                    earlyPivot = order.charAt(early);
+                    continue;
+                }
+                if (eol == -1) {
+                    later--;
+                    laterPivot = order.charAt(later);
+                    continue;
+                }
+
+                if (loe < eol) {
+                   later--;
+                   laterPivot = order.charAt(later);
+                }
+                else {
+                    swap(sb,loe,eol);
+                }
+            }
+            early++;
+            if (early < order.length()) earlyPivot = order.charAt(early);
+            later = order.length() - 1;
+            laterPivot = order.charAt(later);
+        }
+        return sb.toString();
+    }
+
+        void swap(StringBuilder sb, int l, int e) {
+            char temp = sb.charAt(l);
+            sb.setCharAt(l, sb.charAt(e));
+            sb.setCharAt(e, temp);
+        }
+
+}
+
+// s = atdijferisdf
+
+// order = rtsagfi
+
+// sb = adijferisdf
+
+
+
+
+
