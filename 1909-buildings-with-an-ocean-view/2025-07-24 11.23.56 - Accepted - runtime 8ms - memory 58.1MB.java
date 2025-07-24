c1b78cc28aa64229f79c25365f9394da3691e97e
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-07-24 11:23:56 -0400

    Attempt: 2025-07-24 11.23.56 - Accepted - runtime 8ms - memory 58.1MB.java

---
+++ 1909-buildings-with-an-ocean-view/2025-07-24 11.23.56 - Accepted - runtime 8ms - memory 58.1MB.java
@@ -0,0 +1,19 @@
+class Solution {
+    int maxInd;
+    public int[] findBuildings(int[] heights) {
+        if (heights == null) return null;
+        int n = heights.length;
+        if (n < 2) return new int[]{0};
+        int count = 1;
+        maxInd = n - 1;
+        List<Integer> arr = new LinkedList();
+        arr.add(n - 1);
+        for (int i = n - 2; i >= 0; --i) {
+            if (heights[i] > heights[maxInd]) {
+                maxInd = i;
+                arr.addFirst(i);
+            }
+        }
+        return arr.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
