5acfecf348545ad3a7ccf7fe8fde3e7a637e246d
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-07-25 08:59:57 -0400

    Attempt: 2025-07-25 08.59.57 - Wrong Answer - runtime NA - memory NA.java

---
+++ 850-insert-into-a-sorted-circular-linked-list/2025-07-25 08.59.57 - Wrong Answer - runtime NA - memory NA.java
@@ -0,0 +1,58 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _next) {
+        val = _val;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node insert(Node head, int insertVal) {
+        Node newNode = new Node (insertVal);
+        Node root = head;
+        if (head == null) {
+            newNode.next = newNode;
+            return newNode;
+        }
+        if (root.next == root) {
+            root.next = newNode;
+            newNode.next = root;
+            return root;
+        }
+        // if (insertVal < root.val)
+        Node prev = root;
+        
+        do {
+            prev = root;
+            root = root.next;
+            if (prev.val > root.val) {
+                if (insertVal >= prev.val || insertVal <= root.val) 
+                    root = insert (prev, root, newNode);
+            }
+            else if (insertVal >= prev.val && insertVal < root.val) {
+                root = insert(prev,root,newNode);
+                break;
+            }
+        } while (root != head); 
+        if (newNode.next == null) {
+            return insert (root, root.next, newNode);
+        }
+        return head;
+    }
+    Node insert (Node prev, Node curr, Node between) {
+        prev.next = between;
+        between.next = curr;
+        return curr;
+    }
+}
