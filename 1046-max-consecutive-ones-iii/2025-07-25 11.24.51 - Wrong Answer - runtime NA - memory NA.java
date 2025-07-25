286b61fb9c5cdfda2c4f2287fbef358a6bc1294f
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-07-25 11:24:51 -0400

    Attempt: 2025-07-25 11.24.51 - Wrong Answer - runtime NA - memory NA.java

---
+++ 1046-max-consecutive-ones-iii/2025-07-25 11.24.51 - Wrong Answer - runtime NA - memory NA.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int hold = k;
+        int first0ind = -1;
+        int windowLen = 0;
+        int max = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] == 0 && first0ind == -1) first0ind = i;
+            if (k > 0) {
+                if (nums[i] == 0) {
+                    windowLen++;
+                    k--;
+                } else {
+                    windowLen++;
+                }
+            } else { //k is now 0
+                if (nums[i] == 0) {
+                    i = first0ind++;
+                    k = hold;
+                    max = Math.max(max, windowLen);
+                    windowLen = 0;
+                } else {
+                    windowLen++;
+                }
+            }
+        }
+        return max;
+    }
+}
