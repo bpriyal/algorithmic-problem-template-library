22d23e03e2a2b32b5ac9ac89907d38df7bbd9dc5
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-08-06 11:01:55 -0400

    Attempt: 2025-08-06 11.01.55 - Wrong Answer - runtime NA - memory NA.java

---
+++ 348-design-tic-tac-toe/2025-08-06 11.01.55 - Wrong Answer - runtime NA - memory NA.java
@@ -0,0 +1,56 @@
+class TicTacToe {
+
+    int[] row1;
+    int[] col1;
+    int[] row2;
+    int[] col2;
+    int dia1;
+    int adia1;
+    int dia2;
+    int adia2;
+    int totalMoves = 0;
+    int n = 0;
+    public TicTacToe(int N) {
+            n = N;
+            row1 = new int[n];
+            row2 = new int[n];
+            col1 = new int[n];
+            col2 = new int[n];
+            dia1 = 0;
+            dia2 = 0;
+            adia1 = 0;
+            adia2 = 0;
+            totalMoves = n * n;
+    }
+    
+    public int move(int row, int col, int player) {
+        if(--totalMoves == 0) return 0;
+        if (player == 1) {
+            row1[row]++;
+            col1[col]++;
+            if (row == col) dia1++;
+            else if (row + col == n - 1) adia1++;
+            if (row1[row] == n || col1[col] == n || dia1 == n || adia1 == n) return 1;
+        } else {
+            row2[row]++;
+            col2[col]++;
+            if (row == col) dia2++;
+            else if (row + col == n - 1) adia2++;
+            if (row2[row] == n || col2[col] == n || dia2 == n || adia2 == n) return 2;
+        }
+        return 0;
+    }
+}
+
+/**
+ * Your TicTacToe object will be instantiated and called as such:
+ * TicTacToe obj = new TicTacToe(n);
+ * int param_1 = obj.move(row,col,player);
+ */
+
+// 00 01 02 03 04 05
+// 10 11 12 13 14 15
+// 20 21 22 23 24 25
+// 30 31 32 33 34 35
+// 40 41 42 43 44 45
+// 50 51 52 53 54 55
