e1dde4ab81ef6ecfcfba72f6b83ccf714753f495
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-08-13 05:24:04 -0400

    Attempt: 2025-08-13 05.24.04 - Wrong Answer - runtime NA - memory NA.java

---
+++ 1221-element-appearing-more-than-25-in-sorted-array/2025-08-13 05.24.04 - Wrong Answer - runtime NA - memory NA.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int findSpecialInteger(int[] arr) {
+        if (arr == null || arr.length == 0) return 0;
+        if (arr.length == 1) return arr[0];
+        int max = 0;
+        int size = arr.length;
+        int n = size - 1;
+        int ans = 0;
+        int[] check = {arr[n/4], arr[n/2], arr[3*n/4], arr[n]};
+        for (int num : check) {
+            int len = binSearch(num, arr, 0, arr.length - 1, false) - binSearch(num, arr, 0, arr.length - 1, true);
+            if (len > max) ans = num;
+        }
+        return ans;
+    }
+
+    int binSearch (int target, int[] arr, int left, int right, boolean isLeft) {
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                if (isLeft) {
+                    if (mid-1 >= 0 && arr[mid - 1] != target) return mid;
+                    else right = mid - 1;
+                } else {
+                    if (mid+1 < arr.length && arr[mid + 1] != target) return mid;
+                    else left = mid + 1;
+                }
+            } else if (target > arr[mid]) left = mid + 1;
+            else right = mid;
+        }
+        return left;
+    }
+}
+
