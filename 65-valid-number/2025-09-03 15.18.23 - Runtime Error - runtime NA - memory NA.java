5a49c44352f0f64357716e8aa9bf63c35c047bd3
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-09-03 15:18:23 -0400

    Attempt: 2025-09-03 15.18.23 - Runtime Error - runtime NA - memory NA.java

---
+++ 65-valid-number/2025-09-03 15.18.23 - Runtime Error - runtime NA - memory NA.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean isNumber(String s) {
+        String[] splits = s.split("[eE]");
+        if(splits.length > 2 || splits.length < 1) return false;
+        boolean ans0 = false;
+        if (s.indexOf(".") == -1) 
+            ans0 = checkInt(splits[0]);
+        else ans0 = checkDeci(splits[0]);
+        if (splits.length == 1) return ans0;
+        return ans0 && checkInt(splits[1]);
+    }
+
+    boolean checkInt (String s) {
+        int startInd = 0;
+        if (s.charAt(0) == '+' || s.charAt(0) == '-') {
+            startInd = 1;
+        }
+        for (int i = startInd; i < s.length(); ++i) {
+            if (!Character.isDigit(s.charAt(i))) return false;
+        }
+        return true;
+    }
+
+    boolean checkDeci(String s) {
+        String splits[] = s.split("[.]");
+        if (splits.length == 2) {
+            if(!Character.isDigit(splits[1].charAt(0))) return false;
+            if (splits[0].length() == 0) return checkInt(splits[1]);
+            return checkInt(splits[0]) && checkInt(splits[1]);
+        }
+        if (splits.length == 1) {
+            if(!Character.isDigit(splits[0].charAt(0))) return false;
+            return checkInt(splits[0]);
+        }
+        return false;
+    }
+}
