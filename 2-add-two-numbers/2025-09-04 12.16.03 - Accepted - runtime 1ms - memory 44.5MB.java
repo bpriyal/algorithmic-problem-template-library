d679d107af2910d989aa28f61307812201d5337a
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-09-04 12:16:03 -0400

    Attempt: 2025-09-04 12.16.03 - Accepted - runtime 1ms - memory 44.5MB.java

---
+++ 2-add-two-numbers/2025-09-04 12.16.03 - Accepted - runtime 1ms - memory 44.5MB.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = l1;
+        ListNode prev = l1;
+        // ListNode prev = l1;
+        int rem = 0;
+        while (l1 != null || l2 != null) {
+            if (l1 == null) {l1 = new ListNode(rem); rem = rem/10; prev.next = l1;}
+            else if (l2 == null) {l2 = new ListNode(rem); rem = rem/10;}
+            rem = addGiveRem(l1, l2, rem);
+            prev = l1;
+            l1 = l1.next;
+            l2 = l2.next; 
+        }
+        if (rem != 0) {
+            prev.next = new ListNode(rem);
+        }
+        return head;
+    }
+
+    int addGiveRem(ListNode l1, ListNode l2, int rem) {
+        int n = l1.val + l2.val + rem;
+        l1.val = n % 10;
+        return (n / 10);
+    }
+}
