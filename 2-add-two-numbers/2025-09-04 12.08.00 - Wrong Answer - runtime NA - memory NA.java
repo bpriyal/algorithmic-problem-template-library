96e26d5884dc5c4f61de97eac8ab24d30f251693
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-09-04 12:08:00 -0400

    Attempt: 2025-09-04 12.08.00 - Wrong Answer - runtime NA - memory NA.java

---
+++ 2-add-two-numbers/2025-09-04 12.08.00 - Wrong Answer - runtime NA - memory NA.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = l1;
+        ListNode prev = l1;
+        int rem = 0;
+        while (l1 != null || l2 != null) {
+            if (l1 == null && rem != 0) {l1 = new ListNode(rem); rem = 0;}
+            else if (l2 == null && rem != 0) {l2 = new ListNode(rem); rem = 0;}
+            rem = addGiveRem(l1, l2, rem);
+            prev = l1;
+            l1 = l1.next;
+            l2 = l2.next; 
+        }
+        if (rem != 0) {
+            prev.next = new ListNode(rem);
+        }
+        return head;
+    }
+
+    int addGiveRem(ListNode l1, ListNode l2, int rem) {
+        int n = l1.val + l2.val + rem;
+        l1.val = n % 10;
+        return (n / 10);
+    }
+}
