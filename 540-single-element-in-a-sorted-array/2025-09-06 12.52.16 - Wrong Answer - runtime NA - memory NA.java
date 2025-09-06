e2986ba9e0c11b141d6ca7c780455e3806e9bb78
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-09-06 12:52:16 -0400

    Attempt: 2025-09-06 12.52.16 - Wrong Answer - runtime NA - memory NA.java

---
+++ 540-single-element-in-a-sorted-array/2025-09-06 12.52.16 - Wrong Answer - runtime NA - memory NA.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
+        int max = n/2 + 1;
+        int left = 1;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (mid - 2 >= 0) {
+                if (nums[mid - 1] > nums[mid - 2] && nums[mid - 1] < nums[mid])  return nums[mid - 1];
+            }
+            else if (nums[mid - 1] < nums[mid]) return nums[mid - 1];
+            if (nums[mid - 1] > right / 2) left = mid;
+            else right = mid;
+        }
+        return left;
+    }
+}
+
