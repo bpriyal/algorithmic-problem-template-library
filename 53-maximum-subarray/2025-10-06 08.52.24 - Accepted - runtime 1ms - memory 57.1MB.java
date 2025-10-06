ce3c0cd0c4618e77ff69ad051b406553a8a6f312
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-10-06 08:52:24 -0400

    Attempt: 2025-10-06 08.52.24 - Accepted - runtime 1ms - memory 57.1MB.java

---
+++ 53-maximum-subarray/2025-10-06 08.52.24 - Accepted - runtime 1ms - memory 57.1MB.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        // int[] lastMax = new int[n];
+        int currSum = 0;
+        int MAX = nums[0];
+        for (int i = 0; i < n; ++i) {
+            if (currSum + nums[i] > 0) {
+                currSum += nums[i];
+                if (MAX < currSum) MAX = currSum;
+            }
+            else {
+                if (MAX < nums[i]) MAX = nums[i];
+                currSum = 0;
+            }
+        }
+        return MAX;
+    }
+}
