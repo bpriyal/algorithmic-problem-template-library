bbd50251c964a3a93c13d79fa65af460f2e4d602
Priyal Bhagwanani <priyalbhagwanani@gmail.com>
2025-10-09 11:32:18 -0400

    Attempt: 2025-10-09 11.32.18 - Wrong Answer - runtime NA - memory NA.java

---
+++ 74-search-a-2d-matrix/2025-10-09 11.32.18 - Wrong Answer - runtime NA - memory NA.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int r = -1;
+        int i = 0;
+        int n = matrix.length;
+        for (i = 0; i < n; ++i) {
+            if (target < matrix[i][0]) {
+                r = i - 1;
+                break;
+            }
+        }
+        if (r == -1) {
+            if (i == 0) return false;
+            r = n - 1;
+        }
+        int m = matrix[0].length - 1;
+        if (target > matrix[r][m]) return false;
+        for (int j = 0; j < m; ++j) {
+            if (target == matrix[r][j]) return true;
+        } 
+        return false; 
+    }
+}
